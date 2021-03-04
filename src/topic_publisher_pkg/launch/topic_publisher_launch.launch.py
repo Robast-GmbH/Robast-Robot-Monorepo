d66290952a2d69a7856fc7fb10c239ba31a2ff67
TAlscher <t.alscher@tiplu.de>
2021-03-04 15:18:54 +0100

    initial commit

---
+++ src/topic_publisher_pkg/launch/topic_publisher_launch.launch.py
@@ -0,0 +1,8 @@
+from launch import LaunchDescription
+import launch_ros.actions
+
+def generate_launch_description():
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package='topic_publisher_pkg', executable = 'simple_publisher_node', output = 'screen'),
+    ])
