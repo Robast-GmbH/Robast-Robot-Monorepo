8a3116a429aad94b75998e7f636be7f773d2c33a
TipluJacob <j.ritterbach@tiplu.de>
2021-07-27 08:33:42 +0200

    Improved default view for rviz and gazebo

--- src/map_server/launch/full_nav_launch.py
+++ src/map_server/launch/full_nav_launch.py
@@ -89,7 +89,7 @@ def generate_launch_description():
 
     declare_rviz_config_file_cmd = DeclareLaunchArgument(
         'rviz_config_file',
-        default_value=os.path.join(bringup_dir, 'rviz', 'nav2_default_view.rviz'),
+        default_value=os.path.join(config_dir, 'config', 'nav2_default_view.rviz'),
         description='Full path to the RVIZ config file to use')
 
     declare_use_simulator_cmd = DeclareLaunchArgument(
