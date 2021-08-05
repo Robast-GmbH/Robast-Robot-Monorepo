fc3ee044afdfb9710096cdb50e612994c13c7e20
TipluJacob <j.ritterbach@tiplu.de>
2021-08-05 15:20:10 +0200

    added plugins to enable collision of actor

--- shell_setup.sh
+++ shell_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Setup der Variablen
-export GAZEBO_MODEL_PATH=/workspaces/foxy_ws/src/map_server/models:${GAZEBO_MODEL_PATH}
+export GAZEBO_MODEL_PATH=/workspaces/Robast_RosTheron/src/map_server/models:${GAZEBO_MODEL_PATH}
+export GAZEBO_PLUGIN_PATH=workspaces/Robast_RosTheron/gaz/plugins:${GAZEBO_PLUGIN_PATH}
 export WORLD_MODEL=5OG
 export TURTLEBOT3_MODEL=waffle
 source install/setup.bash
