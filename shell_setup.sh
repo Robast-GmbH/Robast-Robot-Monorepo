29e24348ada4152d7d52af51d44a3b1edb9046b7
TipluJacob <j.ritterbach@tiplu.de>
2021-08-31 12:58:02 +0200

    fixed capitalization in shell_setup

--- shell_setup.sh
+++ shell_setup.sh
@@ -3,6 +3,6 @@
 export GAZEBO_MODEL_PATH=/workspaces/Robast_RosTheron/src/map_server/models:${GAZEBO_MODEL_PATH}
 export GAZEBO_PLUGIN_PATH=workspaces/Robast_RosTheron/gaz/plugins:${GAZEBO_PLUGIN_PATH}
 export WORLD_MODEL=5OG
-export ROBOT_MODEL=RB_THERON
+export ROBOT_MODEL=rb_theron
 source install/setup.bash
 source /usr/share/gazebo/setup.sh
