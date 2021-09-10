d41751a7a0c654dc8587e47340add08f98409c10
TipluJacob <j.ritterbach@tiplu.de>
2021-09-10 17:46:03 +0200

    Minor fix in entrypoint

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -4,6 +4,6 @@ cd ~/rviz2_ws/src
 git clone https://github.com/ros2/rviz.git
 cp -r ./resource_retriever ~/rviz2_ws/src/rviz/rviz_rendering/include/
 colcon build --merge-install
-cd /workspaces/foxy_ws
+cd /workspaces/Robast_RosTheron
 
 #sudo /startup.sh
