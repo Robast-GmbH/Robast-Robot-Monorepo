605ba98a4fd28e7d88513054172e970d480c79cb
TipluJacob <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>
2021-10-29 17:50:12 +0200

    navigation with amcl works now and can be used in waypoint test

--- src/navigation/launch/amcl_launch.py
+++ src/navigation/launch/amcl_launch.py
@@ -15,10 +15,7 @@ def generate_launch_description():
 
     nav2_params_yaml = os.path.join(get_package_share_directory('navigation'), 'config', 'nav2_params.yaml')
     nav2_localization_params_yaml = os.path.join(get_package_share_directory('navigation'), 'config', 'localization_params.yaml')
-    default_bt_xml_filename = os.path.join(get_package_share_directory('navigation'), 'behavior_trees', os.environ['ROS_DISTRO'], 'navigate_w_replanning_and_recovery.xml')
     map_file = os.path.join(get_package_share_directory('navigation'), 'maps', '5OG_map.yaml')
-    rviz_config_dir = os.path.join(get_package_share_directory('navigation'), 'config', 'nav2_default_view.rviz')
-
 
     print('map_file: {}'.format(map_file))
 
@@ -29,11 +26,6 @@ def generate_launch_description():
     lifecycle_nodes = [
                         'map_server',
                         'amcl',
-                        'controller_server',
-                        'planner_server',
-                        'recoveries_server',
-                        'bt_navigator',
-                        'waypoint_follower',
                        ]
 
     # Map fully qualified names to relative ones so the node's namespace can be prepended.
@@ -43,7 +35,8 @@ def generate_launch_description():
     # TODO(orduno) Substitute with `PushNodeRemapping`
     #              https://github.com/ros2/launch_ros/issues/56
     remappings = [('/tf', 'tf'),
-                  ('/tf_static', 'tf_static')]
+                  ('/tf_static', 'tf_static'),
+                  ('/map', 'map')]
 
 
     return LaunchDescription([
@@ -75,68 +68,17 @@ def generate_launch_description():
             output='screen',
             parameters=[
                 nav2_localization_params_yaml,
-                {"initial_pose": {"x": 8.59, "y": -13.45, "yaw": 3.14}},
+                {"initial_pose": {"x": float(POSE_INIT_X), "y": float(POSE_INIT_Y), "yaw": 3.14}},
                 {"set_initial_pose": True},
             ],
             remappings=remappings),
-
-        Node(
-            package='nav2_controller',
-            executable='controller_server',
-            output='screen',
-            parameters=[nav2_params_yaml],
-            remappings=remappings),
-
-        Node(
-            package='nav2_planner',
-            executable='planner_server',
-            name='planner_server',
-            output='screen',
-            parameters=[nav2_params_yaml],
-            remappings=remappings),
-
-        Node(
-            package='nav2_recoveries',
-            executable='recoveries_server',
-            name='recoveries_server',
-            output='screen',
-            parameters=[nav2_params_yaml],
-            remappings=remappings),
-
-        Node(
-            package='nav2_bt_navigator',
-            executable='bt_navigator',
-            name='bt_navigator',
-            output='screen',
-            parameters=[
-                nav2_params_yaml,
-                {'default_bt_xml_filename': default_bt_xml_filename},
-            ],
-            remappings=remappings),
-
-        Node(
-            package='nav2_waypoint_follower',
-            executable='waypoint_follower',
-            name='waypoint_follower',
-            output='screen',
-            parameters=[nav2_params_yaml],
-            remappings=remappings),
-
-        
+    
         Node(
             package='nav2_lifecycle_manager',
             executable='lifecycle_manager',
-            name='lifecycle_manager_navigation',
+            name='lifecycle_manager_localization',
             output='screen',
             parameters=[{'use_sim_time': use_sim_time},
                         {'autostart': autostart},
-                        {'node_names': lifecycle_nodes}]),
-
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            arguments=['-d', rviz_config_dir],
-            parameters=[{'use_sim_time': use_sim_time}],
-            output='screen'),
+                        {'node_names': lifecycle_nodes}])    
     ])
