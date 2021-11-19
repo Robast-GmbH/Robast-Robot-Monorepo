3b3e2670021ea978afebaf0ea8f5a12b871ee54c
TipluJacob <j.ritterbach@tiplu.de>
2021-11-19 10:29:38 +0100

    Adding first draft for interim_goal_selector cpp

---
+++ src/navigation/ra_nav_interim_goal/include/ra_nav_interim_goal/interim_goal_selector.hpp
@@ -0,0 +1,144 @@
+// Copyright (c) 2019 Samsung Research America
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef RA_NAV_INTERIM_GOAL__INTERIM_GOAL_HPP_
+#define RA_NAV_INTERIM_GOAL__INTERIM_GOAL_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "nav2_util/lifecycle_node.hpp"
+#include "nav2_msgs/action/navigate_to_pose.hpp"
+#include "nav2_msgs/action/follow_waypoints.hpp"
+#include "nav_msgs/msg/path.hpp"
+#include "nav2_util/simple_action_server.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+namespace ra_nav_interim_goal
+{
+
+struct interim_goal
+{
+  double x;
+  double y;
+  double yaw;
+};
+
+enum class ActionStatus
+{
+  UNKNOWN = 0,
+  PROCESSING = 1,
+  FAILED = 2,
+  SUCCEEDED = 3
+};
+
+/**
+ * @class ra_nav_interim_goal::InterimGoalSelector
+ * @brief A server that gets a path to a goal position and selects from a list of poses the one
+ * that's closest to the path and returns this one as interim goal pose.
+ */
+class InterimGoalSelector : public nav2_util::LifecycleNode
+{
+public:
+  using ActionT = ra_nav_interim_goal::action::ComputeInterimGoal;
+  using ActionServer = nav2_util::SimpleActionServer<ActionT>;
+
+  /**
+   * @brief A constructor for ra_nav_interim_goal::InterimGoalSelector class
+   */
+  InterimGoalSelector();
+  /**
+   * @brief A destructor for ra_nav_interim_goal::InterimGoalSelector class
+   */
+  ~InterimGoalSelector();
+
+protected:
+  /**
+   * @brief Configures member variables
+   *
+   * Initializes action server for "FollowWaypoints"
+   * @param state Reference to LifeCycle node state
+   * @return SUCCESS or FAILURE
+   */
+  nav2_util::CallbackReturn on_configure(const rclcpp_lifecycle::State & state) override;
+  /**
+   * @brief Activates action server
+   * @param state Reference to LifeCycle node state
+   * @return SUCCESS or FAILURE
+   */
+  nav2_util::CallbackReturn on_activate(const rclcpp_lifecycle::State & state) override;
+  /**
+   * @brief Deactivates action server
+   * @param state Reference to LifeCycle node state
+   * @return SUCCESS or FAILURE
+   */
+  nav2_util::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & state) override;
+  /**
+   * @brief Resets member variables
+   * @param state Reference to LifeCycle node state
+   * @return SUCCESS or FAILURE
+   */
+  nav2_util::CallbackReturn on_cleanup(const rclcpp_lifecycle::State & state) override;
+  /**
+   * @brief Called when in shutdown state
+   * @param state Reference to LifeCycle node state
+   * @return SUCCESS or FAILURE
+   */
+  nav2_util::CallbackReturn on_shutdown(const rclcpp_lifecycle::State & state) override;
+
+  /**
+   * @brief Helper function to load the interim goals from a yaml file
+   * @param state Reference to LifeCycle node state
+   */
+  void load_interim_goals_from_yaml(const std::string interim_goals_yaml_filename);
+
+  /**
+   * @brief Action server execution callback
+   */
+  void select_interim_goal();
+
+  /**
+   * @brief Action client result callback
+   * @param result Result of action server updated asynchronously
+   */
+  void resultCallback(const rclcpp_action::ClientGoalHandle<ClientT>::WrappedResult & result);
+
+  /**
+   * @brief Action client goal response callback
+   * @param future Shared future to goalhandle
+   */
+  void goalResponseCallback(
+    std::shared_future<rclcpp_action::ClientGoalHandle<ClientT>::SharedPtr> future);
+
+  // TODO: Remove these variables. Only here for comparison
+  // Our action server
+  std::unique_ptr<ActionServer> action_server_;
+  ActionClient::SharedPtr nav_to_pose_client_;
+  rclcpp::Node::SharedPtr client_node_;
+  std::shared_future<rclcpp_action::ClientGoalHandle<ClientT>::SharedPtr> future_goal_handle_;
+  bool stop_on_failure_;
+  ActionStatus current_goal_status_;
+  int loop_rate_;
+  std::vector<int> failed_ids_;
+
+  // TODO: Remove this comment
+  // NEUE VON MIR HINZUGEFÜGTE:
+  std::vector<interim_goal> interim_goals_;
+};
+
+}  // namespace ra_nav_interim_goal
+
+#endif  // RA_NAV_INTERIM_GOAL__INTERIM_GOAL_HPP_
--- src/navigation/ra_nav_interim_goal/src/interim_goal_selector.cpp
+++ src/navigation/ra_nav_interim_goal/src/interim_goal_selector.cpp
@@ -1,23 +1,117 @@
+#include "ra_nav_interim_goal/interim_goal_selector.hpp"
+
+#include "yaml-cpp/yaml.h"
 #include "rclcpp/rclcpp.hpp"
-#include "ra_nav_interim_goal/srv/compute_interim_goal.hpp"
+#include "ra_nav_interim_goal/action/compute_interim_goal.hpp"
 #include <inttypes.h>
 #include <memory>
 
-namespace ra_nav_door_bell
+#define param_interim_goals_yaml "interim_goals"
+
+namespace ra_nav_interim_goal
 {
 
 InterimGoalSelector::InterimGoalSelector()
 : nav2_util::LifecycleNode("ra_nav_interim_goal", "", true),
 {
   RCLCPP_INFO(get_logger(), "Creating");
+  
+  // Declare this node's parameters
+  declare_parameter(param_interim_goals_yaml);
+}
 
+InterimGoalSelector::~InterimGoalSelector()
+{
+  RCLCPP_INFO(get_logger(), "Destroying");
+}
+
+nav2_util::CallbackReturn
+InterimGoalSelector::on_configure(const rclcpp_lifecycle::State & /*state*/)
+{
+  RCLCPP_INFO(get_logger(), "Configuring");
+
+  std::string interim_goals_yaml_filename = get_parameter(param_interim_goals_yaml).as_string();
+
+  load_interim_goals_from_yaml(interim_goals_yaml_filename);
+
+  action_server_ = std::make_unique<ActionServer>(
+    get_node_base_interface(),
+    get_node_clock_interface(),
+    get_node_logging_interface(),
+    get_node_waitables_interface(),
+    "ComputeInterimGoal", std::bind(&InterimGoalSelector::select_interim_goal, this), false);
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+InterimGoalSelector::on_activate(const rclcpp_lifecycle::State & /*state*/)
+{
+  RCLCPP_INFO(get_logger(), "Activating");
+
+  action_server_->activate();
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+InterimGoalSelector::on_deactivate(const rclcpp_lifecycle::State & /*state*/)
+{
+  RCLCPP_INFO(get_logger(), "Deactivating");
+
+  action_server_->deactivate();
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+InterimGoalSelector::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
+{
+  RCLCPP_INFO(get_logger(), "Cleaning up");
+
+  action_server_.reset();
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+InterimGoalSelector::on_shutdown(const rclcpp_lifecycle::State & /*state*/)
+{
+  RCLCPP_INFO(get_logger(), "Shutting down");
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+void InterimGoalSelector::select_interim_goal() {
+  auto goal = action_server_->get_current_goal();
+  auto feedback = std::make_shared<ActionT::Feedback>();
+  auto result = std::make_shared<ActionT::Result>();
+
+  // Check if request is valid
+  if (!action_server_ || !action_server_->is_server_active()) {
+    RCLCPP_DEBUG(get_logger(), "Action server inactive. Stopping.");
+    return;
+  }
+
+  RCLCPP_INFO(
+    get_logger(), "Received a interim goal computation request for a path with %i poses.",
+    static_cast<int>(goal->goal_path.poses.size()));
 
 }
 
-//TODO: on_configure
-//TODO: on_activate
-//TODO: on_deactivate
-//TODO: on_cleanup
-//TODO: on_shutdown
+void InterimGoalSelector::load_interim_goals_from_yaml(const std::string interim_goals_yaml_filename)
+{
+  //TODO: get full path for interim_goals_yaml_filename
+
+  YAML::Node doc = YAML::LoadFile(interim_goals_yaml_filename);
+
+  for (std::size_t i=1; i<doc.size()+1; i++) {
+    interim_goal interim_goal;
+    interim_goal.x = doc[i]["x"].as<double>();
+    interim_goal.y = doc[i]["y"].as<double>();
+    interim_goal.yaw = doc[i]["yaw"].as<double>();
+
+    interim_goals_.push_back(interim_goal);
+  }
+}
 
 } // namespace ra_nav_door_bell
