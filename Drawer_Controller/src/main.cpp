fd9fcaecd497efbfde856a0c03327c757ec6e8bb
TipluJacob <j.ritterbach@tiplu.de>
2022-03-14 15:07:45 +0100

    Proof of Concept - LED Strip ansteuern klappt

--- Drawer_Controller/src/main.cpp
+++ Drawer_Controller/src/main.cpp
@@ -1,9 +1,201 @@
 #include <Arduino.h>
+#include <ESP32CAN.h>
+#include <CAN_config.h>
+#include <NeoPixelBus.h>
+#include <NeoPixelAnimator.h>
+
+#define LED_POWER_PIN GPIO_NUM_18
+#define LED_PIXEL_PIN GPIO_NUM_2
+#define CAN_TX_PIN GPIO_NUM_5
+#define CAN_RX_PIN GPIO_NUM_4
+
+const uint16_t PixelCount = 19; // make sure to set this to the number of pixels in your strip
+const uint8_t AnimationChannels = 1; // we only need one as all the pixels are animated at once
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(PixelCount, LED_PIXEL_PIN);
+NeoPixelAnimator animations(AnimationChannels); // NeoPixel animation management object
+
+boolean fadeToColor = true;  // general purpose variable used to store effect state
+
+// what is stored for state is specific to the need, in this case, the colors.
+// basically what ever you need inside the animation update function
+struct MyAnimationState
+{
+    RgbColor StartingColor;
+    RgbColor EndingColor;
+};
+
+// one entry per pixel to match the animation timing manager
+MyAnimationState animationState[AnimationChannels];
+
+void SetRandomSeed()
+{
+    uint32_t seed;
+
+    // random works best with a seed that can use 31 bits
+    // analogRead on a unconnected pin tends toward less than four bits
+    seed = analogRead(0);
+    delay(1);
+
+    for (int shifts = 3; shifts < 31; shifts += 3)
+    {
+        seed ^= analogRead(0) << shifts;
+        delay(1);
+    }
+
+    randomSeed(seed);
+}
+
+// simple blend function
+void BlendAnimUpdate(const AnimationParam& param)
+{
+    // this gets called for each animation on every time step
+    // progress will start at 0.0 and end at 1.0
+    // we use the blend function on the RgbColor to mix
+    // color based on the progress given to us in the animation
+    RgbColor updatedColor = RgbColor::LinearBlend(
+        animationState[param.index].StartingColor,
+        animationState[param.index].EndingColor,
+        param.progress);
+
+    // apply the color to the strip
+    for (uint16_t pixel = 0; pixel < PixelCount; pixel++)
+    {
+        strip.SetPixelColor(pixel, updatedColor);
+    }
+}
+
+void FadeInFadeOutRinseRepeat(float luminance)
+{
+    if (fadeToColor)
+    {
+        // Fade upto a random color
+        // we use HslColor object as it allows us to easily pick a hue
+        // with the same saturation and luminance so the colors picked
+        // will have similiar overall brightness
+        RgbColor target = HslColor(random(360) / 360.0f, 1.0f, luminance);
+        uint16_t time = random(800, 2000);
+
+        animationState[0].StartingColor = strip.GetPixelColor(0);
+        animationState[0].EndingColor = target;
+
+        animations.StartAnimation(0, time, BlendAnimUpdate);
+    }
+    else 
+    {
+        // fade to black
+        uint16_t time = random(600, 700);
+
+        animationState[0].StartingColor = strip.GetPixelColor(0);
+        animationState[0].EndingColor = RgbColor(0);
+
+        animations.StartAnimation(0, time, BlendAnimUpdate);
+    }
+
+    // toggle to the next effect state
+    fadeToColor = !fadeToColor;
+}
+
+CAN_device_t CAN_cfg;               // CAN Config
+unsigned long previousMillis = 0;   // will store last time a CAN Message was send
+const int interval = 1000;          // interval at which send CAN Messages (milliseconds)
+const int rx_queue_size = 10;       // Receive Queue size
+
+int debug_counter = 0;
+bool toggle_LED = true;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Basic Demo - ESP32 Drawer Controller");
+
+  pinMode(LED_POWER_PIN, OUTPUT);
+
+  strip.Begin();
+  strip.Show();
+
+  SetRandomSeed();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  debug_counter++;
+  if (debug_counter>10000000) {
+    debug_counter = 0;
+    if (toggle_LED) {
+      digitalWrite(LED_POWER_PIN, HIGH);
+      Serial.println("Setting LED POWER PIN HIGH!");
+      toggle_LED = false;
+    } else {
+      digitalWrite(LED_POWER_PIN, LOW);
+      Serial.println("Setting LED POWER PIN LOW!");
+      toggle_LED = true;
+    }
+  }
+
+  if (animations.IsAnimating())
+    {
+        // the normal loop just needs these two to run the active animations
+        animations.UpdateAnimations();
+        strip.Show();
+    }
+    else
+    {
+        // no animation runnning, start some 
+        //
+        FadeInFadeOutRinseRepeat(0.2f); // 0.0 = black, 0.25 is normal, 0.5 is bright
+    }
+}
+
+
+void can_setup() {
+  CAN_cfg.speed = CAN_SPEED_125KBPS;
+  CAN_cfg.tx_pin_id = CAN_TX_PIN;
+  CAN_cfg.rx_pin_id = CAN_RX_PIN;
+  CAN_cfg.rx_queue = xQueueCreate(rx_queue_size, sizeof(CAN_frame_t));
+  // Init CAN Module
+  ESP32Can.CANInit();
+}
+
+
+void can_loop() {
+  CAN_frame_t rx_frame;
+
+  unsigned long currentMillis = millis();
+
+  // Receive next CAN frame from queue
+  if (xQueueReceive(CAN_cfg.rx_queue, &rx_frame, 3 * portTICK_PERIOD_MS) == pdTRUE) {
+
+    if (rx_frame.FIR.B.FF == CAN_frame_std) {
+      printf("New standard frame");
+    }
+    else {
+      printf("New extended frame");
+    }
+
+    if (rx_frame.FIR.B.RTR == CAN_RTR) {
+      printf(" RTR from 0x%08X, DLC %d\r\n", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
+    }
+    else {
+      printf(" from 0x%08X, DLC %d, Data ", rx_frame.MsgID,  rx_frame.FIR.B.DLC);
+      for (int i = 0; i < rx_frame.FIR.B.DLC; i++) {
+        printf("0x%02X ", rx_frame.data.u8[i]);
+      }
+      printf("\n");
+    }
+  }
+  // Send CAN Message
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    CAN_frame_t tx_frame;
+    tx_frame.FIR.B.FF = CAN_frame_std;
+    tx_frame.MsgID = 0x001;
+    tx_frame.FIR.B.DLC = 8;
+    tx_frame.data.u8[0] = 0x00;
+    tx_frame.data.u8[1] = 0x01;
+    tx_frame.data.u8[2] = 0x02;
+    tx_frame.data.u8[3] = 0x03;
+    tx_frame.data.u8[4] = 0x04;
+    tx_frame.data.u8[5] = 0x05;
+    tx_frame.data.u8[6] = 0x06;
+    tx_frame.data.u8[7] = 0x07;
+    ESP32Can.CANWriteFrame(&tx_frame);
+  }
 }
