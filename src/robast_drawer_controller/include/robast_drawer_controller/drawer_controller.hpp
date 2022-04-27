bcf9243318a3fa08f7b4785c14814f43727c9f69
JacobRitterbach <j.ritterbach@tiplu.de>
2022-04-27 11:20:05 +0200

    First example for serial communication that works

--- src/robast_drawer_controller/include/robast_drawer_controller/drawer_controller.hpp
+++ src/robast_drawer_controller/include/robast_drawer_controller/drawer_controller.hpp
@@ -5,6 +5,14 @@
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
+// C library headers
+#include <stdio.h>
+#include <string.h>
+// Linux headers for serial communication
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <errno.h> // Error integer and strerror() function
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <unistd.h> // write(), read(), close()
 
 #include "robast_hardware_nodes_msgs/action/control_drawer.hpp"
 
