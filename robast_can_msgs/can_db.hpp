3555991c3fb6fa434fbde526f8be044bf01d63bb
TipluJacob <j.ritterbach@tiplu.de>
2022-04-29 17:58:39 +0200

    first draft for decode_can_message (untested)

--- robast_can_msgs/can_db.hpp
+++ robast_can_msgs/can_db.hpp
@@ -7,11 +7,11 @@ namespace robast_can_msgs
 {
     std::vector<can_message> can_db = {
         {0x01, "drawer_user_access", {
-            {"drawer_id", 0, 24},
-            {"open_drawer", 24, 1},
-            {"LED_red", 25, 8},
-            {"LED_green", 33, 8},
-            {"LED_blue", 42, 8},
+            {"drawer_id", 0, 24, 0},
+            {"open_drawer", 24, 1, 0},
+            {"LED_red", 25, 8, 0},
+            {"LED_green", 33, 8, 0},
+            {"LED_blue", 42, 8, 0},
         }},
         {0x02, "drawer_feedback", {
             {"is_endstop_switch_pushed", 0, 1},
--- robast_can_msgs/can_message.hpp
+++ robast_can_msgs/can_message.hpp
@@ -9,8 +9,9 @@ namespace robast_can_msgs
     struct can_signal
     {
         std::string name;
-        uint8_t bit_start;
+        uint8_t bit_start_LSB;
         uint8_t bit_length; // number of bits for this can_signal
+        uint64_t data;
     };
 
     struct can_message
@@ -19,6 +20,14 @@ namespace robast_can_msgs
         std::string name;
         std::vector<can_signal> can_signals;
     };
+
+    can_message decode_can_message(can_message can_message, uint64_t can_data) {
+        // TODO: Test this!
+        for (int i = 0; i < can_message.can_signals.size(); i++) {
+            can_message.can_signals[i].data = (can_data >> can_message.can_signals[i].bit_start_LSB) << (64 - can_message.can_signals[i].bit_length);
+        }
+        return can_message;
+    }
 
 }
 
