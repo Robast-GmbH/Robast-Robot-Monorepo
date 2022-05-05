747ce5b8e31de93547ffad5d8135619dbaa346d3
TorbenZurhelle <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>
2022-05-05 08:31:31 +0200

    drawermanagement

--- src/robast_drawer_manager/include/robast_drawer_manager/drawer_manager.hpp
+++ src/robast_drawer_manager/include/robast_drawer_manager/drawer_manager.hpp
@@ -7,15 +7,14 @@
 #include <stdio.h>
 #include <string.h>
 
-
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-#include "robast_hardware_nodes_msgs/srv/request_drawer.hpp"
+#include "robast_msgs/srv/get_all_attached_drawers.hpp"
+#include "robast_msgs/action/drawer_user_access.hpp"
 
 
 namespace robast_drawer_manager
 {
-
 
 
 class DrawerManager : public rclcpp::Node
@@ -27,6 +26,17 @@ public:
  
   ~DrawerManager();
 
+private:
+
+   rclcpp_action::Client<DrawerUserAccess>::SharedPtr open_drawers_client;
+   rclcpp::Service<GetAllAttachedDrawers>::SharedPtr get_all_drawers_server;
+   rclcpp::Service<GetAllAttachedDrawers>::SharedPtr open_drawer_server;
+ 
+   void get_all_drawer(const std::shared_ptr<robast_msgs::srv::GetAllAttachedDrawers::Request> request,
+                      std::shared_ptr<robast_msgs::srv::GetAllAttachedDrawers::Response>      response);
+
+  void open_drawer(const std::shared_ptr<robast_msgs::srv::HandleDrawer::Request> request,
+                        std::shared_ptr<robast_msgs::srv::HandleDrawer::Response>      response)
 
 };
 
--- src/robast_drawer_manager/src/drawer_manager.cpp
+++ src/robast_drawer_manager/src/drawer_manager.cpp
@@ -1,15 +1,19 @@
-#include <inttypes.h>
 #include <memory>
 
 #include "robast_drawer_manager/drawer_manager.hpp"
 
 namespace robast_drawer_manager
 {
-
+  
 
   DrawerManager::DrawerManager(): Node("drawer_manager_Node")
   {
     RCLCPP_INFO(get_logger(), "Creating");
+    this->get_all_drawers_server = this->create_service<robast_msgs::srv::GetAllAttachedDrawers>("Get_all_attached_drawer_drawer_management", &get_all_drawer);
+    this->handle_drawer_server = this->create_service<robast_msgs::srv::HandleDrawer>("open_drawer_drawer_management", &open_drawer);
+    this->open_drawers_client = rclcpp_action::create_client<Fibonacci>(
+      this,
+      "fibonacci");
   }
 
   DrawerManager::~DrawerManager()
@@ -17,5 +21,27 @@ namespace robast_drawer_manager
     RCLCPP_INFO(get_logger(), "Destroying");
   }
 
+  void get_all_drawer(const std::shared_ptr<robast_msgs::srv::GetAllAttachedDrawers::Request> request,
+                      std::shared_ptr<robast_msgs::srv::GetAllAttachedDrawers::Response>      response)
+{
+    rclcpp::Client<robast_msgs::srv::GetAllAttachedDrawers>::SharedPtr stateMaschineRequest =  node->create_client<robast_msgs::srv::GetAllAttachedDrawers>("Get_all_attached_drawer");
+    response->drawers;
+}
+
+void open_drawer(const std::shared_ptr<robast_msgs::srv::HandleDrawer::Request> request,
+                      std::shared_ptr<robast_msgs::srv::HandleDrawer::Response>      response)
+{
+    //start NFC Reader 
+      //im Frontend anzeigen das der Reader  aktive ist
+       RCLCPP_INFO(get_logger(), "The reader is active please authinticate yourselfe at the reader.");
+    
+    // open drawer
+      // Schublade offen im Frontend anzeigen
+      RCLCPP_INFO(get_logger(), "The drawer is open please close the drawer after you are done.");
+      
+      // nach zeit intervall //bitte schließen imfrontend anzeigen
+
+    //  wenn die Schublade  geschlossen ist action beenden 
+}
 
 } // namespace robast_drawer_manager
