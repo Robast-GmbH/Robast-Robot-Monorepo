583d43de31ae667259f2b145efba6d3a8ed08d15
TipluJacob <j.ritterbach@tiplu.de>
2022-06-13 11:32:09 +0200

    Adding pins and digitalwrite for open and closing lock

--- Drawer_Controller/src/main.cpp
+++ Drawer_Controller/src/main.cpp
@@ -2,7 +2,8 @@
 #include <CAN_config.h>
 #include <FastLED.h>
 
-#define LOCK_POWER_PIN GPIO_NUM_21
+#define OPEN_LOCK1_PIN GPIO_NUM_22
+#define CLOSE_LOCK1_PIN GPIO_NUM_21
 #define LOCK_SENSOR_PIN GPIO_NUM_39
 #define LED_PIXEL_PIN GPIO_NUM_13
 #define CAN_TX_PIN GPIO_NUM_5
@@ -21,54 +22,42 @@ uint8_t brightness_maximum = 200;
 void setup() {
   Serial.begin(115200);
 
-  pinMode(LOCK_POWER_PIN, OUTPUT);
+  pinMode(OPEN_LOCK1_PIN, OUTPUT);
+  pinMode(CLOSE_LOCK1_PIN, OUTPUT);
   pinMode(LOCK_SENSOR_PIN, INPUT);
 
   FastLED.addLeds<NEOPIXEL,LED_PIXEL_PIN>(leds, NUM_LEDS);
-  // FastLED.setBrightness(191);
+  FastLED.setBrightness(100);
 
   lock_is_closed_last_loop = false;
   fade_up = true;
   brightness = 0;
+  digitalWrite(OPEN_LOCK1_PIN, LOW);
+  digitalWrite(CLOSE_LOCK1_PIN, LOW);
 }
 
 void loop() {
 
   bool lock_is_closed = !digitalRead(LOCK_SENSOR_PIN);
 
-  if (lock_is_closed != lock_is_closed_last_loop) {
-    fade_up = true;
-    brightness = 0;
-  }
-
-  lock_is_closed_last_loop = lock_is_closed;
-
-  if (fade_up) {
-    if (brightness == brightness_maximum)
-		{
-			fade_up = false;
-		}
-    else {
-      brightness = brightness + 1;
-    }
-    FastLED.setBrightness(brightness);
-
-    delay(20);
-  }
-  
   if (lock_is_closed) {
-
-    Serial.println("Lock is closed!");
+    // Serial.println("Lock is closed!");
     for(int i = 0; i < NUM_LEDS; i++) {   
-      leds[i] = CRGB::SeaGreen;
+      leds[i] = CRGB::Green;
     }
     FastLED.show();
+
+    digitalWrite(OPEN_LOCK1_PIN, LOW);
+    digitalWrite(CLOSE_LOCK1_PIN, HIGH);
   }
   else {
-    Serial.println("Lock is open!");
+    // Serial.println("Lock is open!");
     for(int i = 0; i < NUM_LEDS; i++) {   
-      leds[i] = CRGB::White;
+      leds[i] = CRGB::Red;
     }
     FastLED.show();
+
+    digitalWrite(CLOSE_LOCK1_PIN, LOW);
+    digitalWrite(OPEN_LOCK1_PIN, HIGH);
   }
 }
