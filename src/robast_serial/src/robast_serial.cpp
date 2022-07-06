97a701130b0c13fcb2dcd993381cd91f2605620a
TipluJacob <j.ritterbach@tiplu.de>
2022-07-06 11:20:07 +0200

    Created robast_serial library

---
+++ src/robast_serial/src/robast_serial.cpp
@@ -0,0 +1,106 @@
+#include "../include/robast_serial.h"
+
+namespace robast_serial
+{
+
+    SerialHelper::SerialHelper(string serial_path)
+    {
+        this->serial_path = serial_path;
+    }
+    
+    SerialHelper::~SerialHelper()
+    {
+    }
+
+    string SerialHelper::open_serial()
+    {
+        this->serial_port = open(this->serial_path.c_str() , O_RDWR);
+        // Check for errors
+        if (this->serial_port < 0) {
+            string errno_string(errno, sizeof(errno));
+            return "Error " + errno_string + " from open: " + strerror(errno) + "\n";
+        }
+
+        // Create new termios struct, we call it 'tty' for convention
+        struct termios tty;
+
+        // Read in existing settings, and handle any error
+        if(tcgetattr(this->serial_port, &tty) != 0) {
+            string errno_string(errno, sizeof(errno));
+            return "Error " + errno_string + " from tcgetattr: " + strerror(errno) + "\n";
+        }
+
+        tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+        tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+        tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
+        tty.c_cflag |= CS8; // 8 bits per byte (most common)
+        tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+        tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+        tty.c_lflag &= ~ICANON;
+        tty.c_lflag &= ~ECHO; // Disable echo
+        tty.c_lflag &= ~ECHOE; // Disable erasure
+        tty.c_lflag &= ~ECHONL; // Disable new-line echo
+        tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+        tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+
+        tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+        tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+        // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
+        // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
+
+        tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+        tty.c_cc[VMIN] = 0;
+
+        // Set in/out baud rate to be 9600
+        cfsetispeed(&tty, B9600);
+        cfsetospeed(&tty, B9600);
+
+        // Save tty settings, also checking for error
+        if (tcsetattr(this->serial_port, TCSANOW, &tty) != 0) {
+            string errno_string(errno, sizeof(errno));
+            return "Error " + errno_string + " from tcgetattr: " + strerror(errno) + "\n";
+        }
+        return "";
+    }
+
+    void SerialHelper::close_serial()
+    {
+        close(this->serial_port);
+        this->serial_port = -1;
+    }
+
+    uint16_t SerialHelper::read_serial(string* result, uint16_t max_num_bytes)
+    {
+        char read_buf [256];
+        memset(&read_buf, '\0', sizeof(read_buf));
+        int num_bytes;
+        num_bytes = read(this->serial_port, &read_buf, sizeof(read_buf));
+        if( num_bytes > max_num_bytes)
+        {
+            *result = "Error reading: ";
+            result->append(strerror(errno));
+            result->append("/n");
+            return 0;
+        }
+        *result = string(read_buf, num_bytes);
+        return num_bytes;
+    }
+
+    string SerialHelper::write_serial(string msg)
+    {
+        if (this->serial_port < 0)
+        {
+            string errno_string(errno, sizeof(errno));
+            return "Error " + errno_string + " from open: " + strerror(errno) + "\n";
+        }
+        write(this->serial_port, &msg[0], msg.length());
+        return "";
+    }
+
+    string SerialHelper::send__ascii_cmd(string cmd)
+    {
+        return this->write_serial(cmd + "\r");
+    }
+}
