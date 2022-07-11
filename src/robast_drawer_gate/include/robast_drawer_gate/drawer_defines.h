d46620ba4e9320e34dbafa5a2393e6d2ad642a5b
TipluJacob <j.ritterbach@tiplu.de>
2022-07-11 12:42:40 +0200

    Adding drawer_defines.h

---
+++ src/robast_drawer_gate/include/robast_drawer_gate/drawer_defines.h
@@ -0,0 +1,25 @@
+#ifndef DRAWER_DEFINES_HPP_
+#define DRAWER_DEFINES_HPP_
+
+#include "robast_ros2_msgs/msg/Box.hpp"
+
+
+namespace robast_drawer_gate
+{
+    // Drawer Module with 10cm Height, 40cm Depth, 1 Drawer
+    #define DRAWER_INSIDE_WIDTH_10x40x1 310
+    #define DRAWER_INSIDE_DEPTH_10x40x1 400
+    #define DRAWER_INSIDE_HEIGHT_10x40x1 74
+
+    // Drawer Module with 20cm Height, 40cm Depth, 1 Drawer
+    #define DRAWER_INSIDE_WIDTH_20x40x1 310
+    #define DRAWER_INSIDE_DEPTH_20x40x1 400
+    #define DRAWER_INSIDE_HEIGHT_20x40x1 177
+
+    // Drawer Module with 30cm Height, 40cm Depth, 1 Drawer
+    #define DRAWER_INSIDE_WIDTH_30x40x1 310
+    #define DRAWER_INSIDE_DEPTH_30x40x1 400
+    #define DRAWER_INSIDE_HEIGHT_30x40x1 282
+}
+
+#endif /* DRAWER_DEFINES_HPP_ */
