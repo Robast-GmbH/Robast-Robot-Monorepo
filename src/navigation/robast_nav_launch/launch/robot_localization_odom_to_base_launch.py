1e4d8c37566bd4ebeb1c5816089ecedda36f770e
TipluJacob <j.ritterbach@tiplu.de>
2022-08-24 15:51:03 +0200

    small refactor for robot_localization launch

--- src/navigation/robast_nav_launch/launch/robot_localization_odom_to_base_launch.py
+++ src/navigation/robast_nav_launch/launch/robot_localization_odom_to_base_launch.py
@@ -5,33 +5,51 @@ from launch import LaunchDescription
 from launch.actions import SetEnvironmentVariable, DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
+from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
 
-    use_sim_time = LaunchConfiguration('use_sim_time')
-
     robot_localization_params_yaml = os.path.join(get_package_share_directory(
         'robast_nav_launch'), 'config', 'robot_localization_ekf_odom_to_base.yaml')
 
+    namespace = LaunchConfiguration('namespace')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace'
+    )
+
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
-        default_value='false',
+        default_value='true',
         description='Use simulation (Gazebo) clock if true')
+
+    param_substitutions = {
+        'use_sim_time': use_sim_time
+    }
+
+    configured_params = RewrittenYaml(
+        source_file=robot_localization_params_yaml,
+        root_key=namespace,
+        param_rewrites=param_substitutions,
+        convert_types=True)
 
     start_robot_localization_cmd = Node(
         package='robot_localization',
         executable='ekf_node',
         name='ekf_filter_node',
         output='screen',
-        parameters=[robot_localization_params_yaml,
-                    {'use_sim_time': use_sim_time}])
+        parameters=[configured_params])
 
     ld = LaunchDescription()
 
     # Set env var to print messages to stdout immediately
     ld.add_action(SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'))
 
+    ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_sim_time_cmd)
 
     ld.add_action(start_robot_localization_cmd)
