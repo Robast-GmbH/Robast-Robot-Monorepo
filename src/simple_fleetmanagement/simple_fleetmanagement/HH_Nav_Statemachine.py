1add87d8937bf0ff4a733382c6878607f72260c3
Tobi <t.alscher@robast.de>
2022-09-10 22:49:57 +0200

    alle states für HH definiert

---
+++ src/simple_fleetmanagement/simple_fleetmanagement/HH_Nav_Statemachine.py
@@ -0,0 +1,49 @@
+class HHStateMachine:
+    def __init__(self) -> None:
+
+        pass
+
+    def start(self):
+        pass
+
+    def changeState_running_to_pause(self):
+        pass
+
+    def changeState_pause_to_drawer_open(self):
+        pass
+
+    def changeState_pause_to_homing(self):
+        pass
+
+    def changeState_pause_to_running(self):
+        pass
+
+    def changeState_drawer_open_to_pause(self):
+        pass
+
+    def checkCondition_running_to_pause(self):
+        pass
+
+    def checkContition_pause_to_drawer_open(self):
+        pass
+
+    def checkCondition_pause_to_drawer_open(self):
+        pass
+
+    def checkCondition_pause_to_running(self):
+        pass
+
+    def checkCondition_drawer_open_to_pause(self):
+        pass
+
+    def stateRunning(self):
+        pass
+
+    def statePause(self):
+        pass
+
+    def stateDrawerOpen(self):
+        pass
+
+    def stateHoming(self):
+        pass
