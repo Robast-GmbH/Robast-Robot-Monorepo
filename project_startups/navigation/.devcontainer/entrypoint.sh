3849f6ab35ca0538e1e60267c7dc4bb5179d6193
Tobi <t.alscher@robast.de>
2022-10-04 09:06:29 +0200

    compose für webstack um ports erweitert

--- project_startups/navigation/.devcontainer/entrypoint.sh
+++ project_startups/navigation/.devcontainer/entrypoint.sh
@@ -2,4 +2,5 @@
 sudo apt-get update && sudo apt-get upgrade -y
 rosdep update
 rosdep install --from-paths /workspace/src --ignore-src -r -y
-
+ros2 param set /robot/front_laser_node angle_start -1.95
+ros2 param set /robot/front_laser_node angle_end 1.95
