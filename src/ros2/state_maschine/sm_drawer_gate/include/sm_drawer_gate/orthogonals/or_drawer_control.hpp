ed839f07b5fd8e67e96a66f3d6997d56256c5475
TipluJacob <j.ritterbach@tiplu.de>
2022-10-18 11:08:06 +0200

    Adding or_drawer_control

---
+++ src/ros2/state_maschine/sm_drawer_gate/include/sm_drawer_gate/orthogonals/or_drawer_control.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <smacc2/smacc_orthogonal.hpp>
+#include <sm_drawer_gate/clients/cl_drawer_control/cl_drawer_control.hpp>
+
+using namespace std::chrono_literals;
+
+namespace sm_drawer_gate
+{
+class OrDrawerControl : public smacc2::Orthogonal<OrDrawerControl>
+{
+public:
+  void onInitialize() override
+  {
+    auto clDrawerControl = this->createClient<cl_drawer_control::ClDrawerControl>("open_drawer");
+  }
+};
+}  // namespace sm_drawer_gate
