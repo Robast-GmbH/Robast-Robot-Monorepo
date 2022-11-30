76af86543809f706fcd15b080b1c3ae170b2fbae
Torben Zurhelle <t.zurhelle@tiplu.de>
2022-11-30 12:17:38 +0100

    run for ever test

--- src/web/frontend/react_frontend/deploy-entrypoint.sh
+++ src/web/frontend/react_frontend/deploy-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-cd  /workspace/src
-npm install -g serve
-npm ci
-npm run build&
-serve -s build&
+# cd  /workspace/src
+# npm install -g serve
+# npm ci
+# npm run build&
+# serve -s build&
 
-tail -f /dev/null
+# tail -f /dev/null
+x=1
+while [ 1=1 ]
+do
+  echo "Welcome $x times"
+  x=$(( $x + 1 ))
+done
