70ce3995c9eb0deb5a724e076379d8b085b73117
TipluJacob <j.ritterbach@tiplu.de>
2022-11-30 17:28:32 +0100

    First pitch for getting gazebo ignition running - docker not building at the moment

---
+++ project_startups/simulation_ignition/simulation/.devcontainer/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#mkdir -p ~/rviz2_ws/src
+#cd ~/rviz2_ws/src
+#git clone https://github.com/ros2/rviz.git
+#cp -r ./resource_retriever ~/rviz2_ws/src/rviz/rviz_rendering/include/
+#colcon build --merge-install
+
+cd /workspace
+sudo apt-get update && sudo apt-get upgrade -y
+rosdep update
+rosdep install --from-paths src --ignore-src -r -y
+#sudo dos2unix shell_setup.sh
+#sudo /startup.sh
+
+sudo echo 'export GAZEBO_MODEL_PATH=/workspace/src/simulation/tiplu_world/models:${GAZEBO_MODEL_PATH}' >> /home/$USER/.bashrc
+sudo echo 'export GAZEBO_MODEL_PATH=/workspace/src/simulation/tiplu_world/models/furniture:${GAZEBO_MODEL_PATH}' >> /home/$USER/.bashrc
+sudo echo 'export GAZEBO_PLUGIN_PATH=/workspace/install/gazebo_plugins:${GAZEBO_PLUGIN_PATH}' >> /home/$USER/.bashrc
