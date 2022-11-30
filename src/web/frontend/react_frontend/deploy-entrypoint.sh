48c74a8a3f16b747c558a1dfa307ded6bd657fc2
Torben Zurhelle <t.zurhelle@tiplu.de>
2022-11-30 16:52:59 +0100

    Enviroment variable Deployment
    Enviroment variable Deployment 2
    
    Enviroment variable Deployment 2.1
    
    Enviroment variable Deployment 2.2
    
    Enviroment variable Deployment 2.3
    
    Enviroment variable Deployment 2.4
    
    Enviroment variable Deployment 2.5
    
    Enviroment variable Deployment 2.6
    
    Enviroment variable Deployment 2.7 stabile test version
    
    Enviroment variable Deployment 3

--- src/web/frontend/react_frontend/deploy-entrypoint.sh
+++ src/web/frontend/react_frontend/deploy-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
  cd  /workspace/src
  npm install -g serve
- npm ci
- npm run build&
- serve -s build&
-if $DOCKER_Deployment != "True"
-then
-  tail -f /dev/null
-fi
+ 
+ 
+ if [ -z ${DOCKER_MODE+x} ]; then
+   npm ci 
+   npm run build&
+   serve -s build&
+   tail -f /dev/null
+ fi
