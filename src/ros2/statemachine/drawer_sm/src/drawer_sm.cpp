27408fe564f42a991ceecdc65d1b0371c80ea12d
Tobi <t.alscher@robast.de>
2022-12-06 12:18:42 +0100

    drawer main cpp debugging (funktioniert nicht)

--- src/ros2/statemachine/drawer_sm/src/drawer_sm.cpp
+++ src/ros2/statemachine/drawer_sm/src/drawer_sm.cpp
@@ -22,9 +22,9 @@ namespace drawer_statemachine
       : Node("bt_ticker")
     {
       std::cout << "BTTicker" << std::endl;
-      
-      std::cout << "hinzugefügt is" << std::endl;      
-      
+
+      std::cout << "hinzugefügt is" << std::endl;
+
     }
 
   private:
@@ -34,22 +34,35 @@ namespace drawer_statemachine
 using namespace drawer_statemachine;
 int main(int argc, char* argv[ ])
 {
-  static rclcpp::Node::SharedPtr node_;
-  static BT::NodeConfiguration * config_;
-  static std::shared_ptr<BT::BehaviorTreeFactory> factory_;
-  BT::BehaviorTreeFactory factory;
-  
   rclcpp::init(argc, argv);
-  factory.registerNodeType<DrawerStatusCondition>("DrawerStatusCondition");
-  factory.registerNodeType<ChangeLED>("ChangeLED");
-  factory.registerNodeType<DrawerOpenReq>("DrawerOpenReq");
-  factory.registerNodeType<OpenDrawer>("OpenDrawer");
-  factory.registerNodeType<ApproachObject>("ApproachObject");
-  std::cout << "print" << std::endl;
-  auto tree = factory_->createTreeFromFile("/workspace/src/statemachine/drawer_sm/trees/drawer_sequence_simple.xml");
-  std::cout << "was los hier" << std::endl;
+  static rclcpp::Node::SharedPtr node_;
+  static BT::NodeConfiguration* config_;
+  static std::shared_ptr<BT::BehaviorTreeFactory> factory_ = std::make_shared<BT::BehaviorTreeFactory>(BT::BehaviorTreeFactory());
+  // BT::BehaviorTreeFactory factory;
+
+
+  config_ = new BT::NodeConfiguration();
+
+  // Create the blackboard that will be shared by all of the nodes in the tree
+  config_->blackboard = BT::Blackboard::create();
+  // Put items on the blackboard
+  config_->blackboard->set<rclcpp::Node::SharedPtr>(
+    "node",
+    node_);
+  config_->blackboard->set<std::chrono::milliseconds>(
+    "bt_loop_duration",
+    std::chrono::milliseconds(10));
+
+  factory_->registerNodeType<DrawerStatusCondition>("DrawerStatusCondition");
+  factory_->registerNodeType<ChangeLED>("ChangeLED");
+  factory_->registerNodeType<DrawerOpenReq>("DrawerOpenReq");
+  factory_->registerNodeType<OpenDrawer>("OpenDrawer");
+  factory_->registerNodeType<ApproachObject>("ApproachObject");
+  // std::cout << "print" << std::endl;
+  BT::Tree tree = factory_->createTreeFromFile("/workspace/src/statemachine/drawer_sm/trees/drawer_sequence_simple.xml");
+  // std::cout << "was los hier" << std::endl;
   tree.tickRootWhileRunning();
-  rclcpp::spin(std::make_shared<drawer_statemachine::BTTicker>(&factory_));
+  // rclcpp::spin(std::make_shared<drawer_statemachine::BTTicker>(&factory_));
   rclcpp::shutdown();
   return 0;
 }
