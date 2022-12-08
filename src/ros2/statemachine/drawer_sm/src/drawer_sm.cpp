a8f770a89958388a60f7a3e08a167966006e3a20
Tobi <t.alscher@robast.de>
2022-12-08 14:03:18 +0100

    es baut wieder, aber mit segementationfold

--- src/ros2/statemachine/drawer_sm/src/drawer_sm.cpp
+++ src/ros2/statemachine/drawer_sm/src/drawer_sm.cpp
@@ -13,7 +13,7 @@
 #include "bt_plugins/condition/drawer_status_condition.hpp"
 #include "bt_plugins/example_plugin.hpp"
 
-#include "../include/drawer_sm/behavior_tree_engine.hpp"
+#include "bt_plugins/behavior_tree_engine.hpp"
 
 // namespace drawer_statemachine
 // {
@@ -38,13 +38,30 @@ int main(int argc, char* argv[ ])
 {
   rclcpp::init(argc, argv);
   const std::vector<std::string> plugins = {
-    "change_led_action",
-    "drawer_open_request_action",
-    "open_drawer_action",
-    "drawer_status_condition",
-    "example_plugin"
+    "change_led_action_bt_node",
+    "drawer_open_request_action_bt_node",
+    "open_drawer_action_bt_node",
+    "drawer_status_condition_bt_node",
+    // "example_plugin"
   };
+  static rclcpp::Node::SharedPtr node_;
+  static BT::NodeConfiguration* config_;
+  config_ = new BT::NodeConfiguration();
+
+  // Create the blackboard that will be shared by all of the nodes in the tree
+  config_->blackboard = BT::Blackboard::create();
+  // Put items on the blackboard
+  config_->blackboard->set<rclcpp::Node::SharedPtr>(
+    "node",
+    node_);
+  config_->blackboard->set<std::chrono::milliseconds>(
+    "bt_loop_duration",
+    std::chrono::milliseconds(10));
   auto bt_engine = std::make_unique<BehaviorTreeEngine>(plugins);
+  std::cout << "lass mal baum laden"<< std::endl;
+  BT::Tree bt = bt_engine->createTreeFromFile("/workspace/src/statemachine/drawer_sm/trees/drawer_sequence_simple.xml", config_->blackboard);
+  std::cout << "lüppt" << std::endl;
+
   rclcpp::shutdown();
   return 0;
 }
