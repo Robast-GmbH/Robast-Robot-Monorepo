d47a415fb17a3f34b5b7f2fe887e9c8a9be27405
Tobi <t.alscher@robast.de>
2023-01-18 18:24:17 +0100

    entrypoint wieder hinzugefügt und vcpkg installation ergänzt

--- project_startups/statemachine/.devcontainer/entrypoint.sh
+++ project_startups/statemachine/.devcontainer/entrypoint.sh
@@ -7,5 +7,12 @@
 sudo apt-get update && sudo apt-get upgrade -y
 rosdep update
 rosdep install --from-paths /workspace/src --ignore-src -r -y
+
+cd /opt/vcpkg/
+sudo chmod a+rw /opt/vcpkg/
+mkdir ~/.vcpkg
+touch ~/.vcpkg/vcpkg.path.txt
+./bootstrap-vcpkg.sh && ./vcpkg integrate install && ./vcpkg integrate bash && echo 'export PATH=$PATH:/opt/vcpkg' >>~/.bashrc
+
 #sudo dos2unix shell_setup.sh
 #sudo /startup.sh
