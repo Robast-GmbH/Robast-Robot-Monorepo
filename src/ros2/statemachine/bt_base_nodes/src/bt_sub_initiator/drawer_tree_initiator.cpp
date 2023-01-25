788e945e9711e58701752049ead67227dd9f6fa9
Tobi <t.alscher@robast.de>
2023-01-25 10:26:49 +0100

    workflow fix. ; -> &&

--- src/ros2/statemachine/bt_base_nodes/src/bt_sub_initiator/drawer_tree_initiator.cpp
+++ src/ros2/statemachine/bt_base_nodes/src/bt_sub_initiator/drawer_tree_initiator.cpp
@@ -2,13 +2,13 @@
 
 namespace bt_base_nodes
 {
-    int main(int argc, char* argv[ ])
-    {
-        rclcpp::init(argc, argv);
-        auto node = std::make_shared<bt_base_nodes::DrawerTreeInitiator>();
-        node->configure();
-        rclcpp::spin(node);
-        rclcpp::shutdown();
-        return 0;
-    }
+}
+int main(int argc, char* argv[ ])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<bt_base_nodes::DrawerTreeInitiator>();
+    node->configure();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }
