1862ceb091281a9b596814f8156aedda2dd8c64d
Tobi <t.alscher@robast.de>
2023-01-25 10:52:23 +0100

    entypoint cleanup

--- project_startups/statemachine/.devcontainer/entrypoint.sh
+++ project_startups/statemachine/.devcontainer/entrypoint.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
-#mkdir -p ~/rviz2_ws/src
-#cd ~/rviz2_ws/src
-#git clone https://github.com/ros2/rviz.git
-#cp -r ./resource_retriever ~/rviz2_ws/src/rviz/rviz_rendering/include/
-#colcon build --merge-instal
 sudo apt-get update && sudo apt-get upgrade -y
 rosdep update
 rosdep install --from-paths /workspace/src --ignore-src -r -y
 
+# i dont want to accept that the vcpkg manage ris not working
 # cd /opt/vcpkg/
 # sudo chmod a+rw /opt/vcpkg/
 # mkdir ~/.vcpkg
 # touch ~/.vcpkg/vcpkg.path.txt
 # ./bootstrap-vcpkg.sh && ./vcpkg integrate install && ./vcpkg integrate bash && echo 'export PATH=$PATH:/opt/vcpkg' >>~/.bashrc
-
-#sudo dos2unix shell_setup.sh
-#sudo /startup.sh
