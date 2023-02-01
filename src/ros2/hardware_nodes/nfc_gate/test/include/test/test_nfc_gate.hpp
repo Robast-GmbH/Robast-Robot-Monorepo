b35f80f960d32816dd80f25e41ce07f31dd31ad7
Torben Zurhelle <t.zurhelle@robast.de>
2023-02-01 07:58:29 +0100

    pointer umbau

--- src/ros2/hardware_nodes/nfc_gate/test/include/test/test_nfc_gate.hpp
+++ src/ros2/hardware_nodes/nfc_gate/test/include/test/test_nfc_gate.hpp
@@ -11,8 +11,8 @@ namespace robast
         TestNFCGate(serial_helper::ISerialHelper* serial_connector, db_helper::IDBHelper* db_connector);
         TestNFCGate();
 
-        bool execute_scan(std::vector<std::string> permission_keys, std::string* validated_User );
-        bool validate_key(string scanned_key, std::vector<std::string> allValidKeys, std::string* validated_user );
-        bool scan_tag(std::string* tag_data);
+        bool execute_scan(std::vector<std::string> permission_keys, std::shared_ptr< std::string>validated_User );
+        bool validate_key(string scanned_key, std::vector<std::string> allValidKeys, std::shared_ptr<std::string> validated_user );
+        bool scan_tag(std::shared_ptr<std::string> tag_data);
     };
 }
