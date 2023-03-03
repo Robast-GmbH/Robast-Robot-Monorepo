e4443d8bbc4c3e5a08ad436506208ac7e7bbf497
TipluJacob <j.ritterbach@tiplu.de>
2023-03-03 11:14:20 +0100

    Much more unit tests

--- src/ros2/hardware_nodes/dryve_d1_gate/test/unit_tests/test_d1.cpp
+++ src/ros2/hardware_nodes/dryve_d1_gate/test/unit_tests/test_d1.cpp
@@ -28,11 +28,11 @@ namespace unit_test
     REQUIRE(listen(server_sock, 1) != -1);
   }
 
-  SCENARIO("Testing the functionality of the D1 class")
+  SCENARIO("Testing the Creating of the D1 class")
   {
     GIVEN("A test server")
     {
-      int port = 123;
+      int port = 12345;
       start_test_server(port);
       std::string ip_address = "127.0.0.1";
 
@@ -47,10 +47,13 @@ namespace unit_test
         }
       }
     }
+  }
 
-    GIVEN("A test server and the D1 class")
+  SCENARIO("Testing the set_dryve_shutdown_state function of the D1 class")
+  {
+    GIVEN("A test server, the D1 class and the expected calls")
     {
-      int port = 1234;
+      int port = 12345;
       start_test_server(port);
       std::string ip_address = "127.0.0.1";
       std::unique_ptr<MockSocketWrapper> mock_socket_wrapper = std::make_unique<MockSocketWrapper>();
@@ -80,7 +83,7 @@ namespace unit_test
       EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 23, 0))
           .WillOnce(
               testing::DoAll(testing::SetArrayArgument<1>(STATUS_SHUTDOWN, STATUS_SHUTDOWN + sizeof(STATUS_SHUTDOWN)),
-                             testing::Return(23)));
+                             testing::Return(21)));
 
       dryve_d1_gate::D1 d1 = dryve_d1_gate::D1(ip_address, port, std::move(mock_socket_wrapper));
 
@@ -90,7 +93,313 @@ namespace unit_test
 
         THEN("the previously defined expected calls should run through and throw no error")
         {
-          // There is no check we can do here as we only want the set_dryve_shutdown_state to run through with our
+          // There is no check we can do here as we only want the called function to run through with our
+          // predefined expected calls
+          REQUIRE(true);
+        }
+      }
+    }
+  }
+
+  SCENARIO("Testing the set_dryve_switch_on_state function of the D1 class")
+  {
+    GIVEN("A test server, the D1 class and the expected calls")
+    {
+      int port = 12345;
+      start_test_server(port);
+      std::string ip_address = "127.0.0.1";
+      std::unique_ptr<MockSocketWrapper> mock_socket_wrapper = std::make_unique<MockSocketWrapper>();
+
+      const unsigned char SEND_SWITCH_ON[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 64, 0, 0, 0, 0, 2, 7, 0};
+      char handshake_send_switch_on[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_send_switch_on[12] = SEND_SWITCH_ON[12];
+      handshake_send_switch_on[13] = SEND_SWITCH_ON[13];
+      handshake_send_switch_on[14] = SEND_SWITCH_ON[14];
+
+      const unsigned char READ_STATUS_WORD[19] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2};
+
+      const unsigned char STATUS_SWITCH_ON[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 35, 6};
+
+      EXPECT_CALL(*mock_socket_wrapper,
+                  sending(testing::_, testing::_, sizeof(SEND_SWITCH_ON) / sizeof(SEND_SWITCH_ON[0]), 0))
+          .WillOnce(testing::Return(sizeof(SEND_SWITCH_ON)));
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 19, 0))
+          .WillOnce(
+              testing::DoAll(testing::SetArrayArgument<1>(handshake_send_switch_on,
+                                                          handshake_send_switch_on + sizeof(handshake_send_switch_on)),
+                             testing::Return(sizeof(handshake_send_switch_on))));
+
+      EXPECT_CALL(*mock_socket_wrapper,
+                  sending(testing::_, testing::_, sizeof(READ_STATUS_WORD) / sizeof(READ_STATUS_WORD[0]), 0))
+          .WillOnce(testing::Return(sizeof(READ_STATUS_WORD)));
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 23, 0))
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(STATUS_SWITCH_ON, STATUS_SWITCH_ON + sizeof(STATUS_SWITCH_ON)),
+              testing::Return(21)));
+
+      dryve_d1_gate::D1 d1 = dryve_d1_gate::D1(ip_address, port, std::move(mock_socket_wrapper));
+
+      WHEN("set_dryve_switch_on_state is called")
+      {
+        d1.set_dryve_switch_on_state();
+
+        THEN("the previously defined expected calls should run through and throw no error")
+        {
+          // There is no check we can do here as we only want the called function to run through with our
+          // predefined expected calls
+          REQUIRE(true);
+        }
+      }
+    }
+  }
+
+  SCENARIO("Testing the set_dryve_operation_enable_state function of the D1 class")
+  {
+    GIVEN("A test server, the D1 class and the expected calls")
+    {
+      int port = 12345;
+      start_test_server(port);
+      std::string ip_address = "127.0.0.1";
+      std::unique_ptr<MockSocketWrapper> mock_socket_wrapper = std::make_unique<MockSocketWrapper>();
+
+      const unsigned char SEND_OPERATION_ENABLE[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                       0, 96, 64, 0, 0, 0,  0, 2,  15, 0};
+      char handshake_send_operation_enable[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_send_operation_enable[12] = SEND_OPERATION_ENABLE[12];
+      handshake_send_operation_enable[13] = SEND_OPERATION_ENABLE[13];
+      handshake_send_operation_enable[14] = SEND_OPERATION_ENABLE[14];
+
+      const unsigned char READ_STATUS_WORD[19] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2};
+
+      const unsigned char STATUS_OPERATION_ENABLE[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                         0, 96, 65, 0, 0, 0,  0, 2,  39, 6};
+      EXPECT_CALL(*mock_socket_wrapper,
+                  sending(testing::_, testing::_, sizeof(SEND_OPERATION_ENABLE) / sizeof(SEND_OPERATION_ENABLE[0]), 0))
+          .WillOnce(testing::Return(sizeof(SEND_OPERATION_ENABLE)));
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 19, 0))
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(handshake_send_operation_enable,
+                                           handshake_send_operation_enable + sizeof(handshake_send_operation_enable)),
+              testing::Return(sizeof(handshake_send_operation_enable))));
+
+      EXPECT_CALL(*mock_socket_wrapper,
+                  sending(testing::_, testing::_, sizeof(READ_STATUS_WORD) / sizeof(READ_STATUS_WORD[0]), 0))
+          .WillOnce(testing::Return(sizeof(READ_STATUS_WORD)));
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 23, 0))
+          .WillOnce(
+              testing::DoAll(testing::SetArrayArgument<1>(STATUS_OPERATION_ENABLE,
+                                                          STATUS_OPERATION_ENABLE + sizeof(STATUS_OPERATION_ENABLE)),
+                             testing::Return(21)));
+
+      dryve_d1_gate::D1 d1 = dryve_d1_gate::D1(ip_address, port, std::move(mock_socket_wrapper));
+
+      WHEN("set_dryve_operation_enable_state is called")
+      {
+        d1.set_dryve_operation_enable_state();
+
+        THEN("the previously defined expected calls should run through and throw no error")
+        {
+          // There is no check we can do here as we only want the called function to run through with our
+          // predefined expected calls
+          REQUIRE(true);
+        }
+      }
+    }
+  }
+
+  SCENARIO("Testing the send_constant_set_command function of the D1 class")
+  {
+    GIVEN("A test server, the D1 class and the expected calls")
+    {
+      int port = 12345;
+      start_test_server(port);
+      std::string ip_address = "127.0.0.1";
+      std::unique_ptr<MockSocketWrapper> mock_socket_wrapper = std::make_unique<MockSocketWrapper>();
+
+      const unsigned char SEND_OPERATION_ENABLE[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                       0, 96, 64, 0, 0, 0,  0, 2,  15, 0};
+      char handshake_send_operation_enable[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_send_operation_enable[12] = SEND_OPERATION_ENABLE[12];
+      handshake_send_operation_enable[13] = SEND_OPERATION_ENABLE[13];
+      handshake_send_operation_enable[14] = SEND_OPERATION_ENABLE[14];
+
+      const unsigned char STATUS_OPERATION_ENABLE[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                         0, 96, 65, 0, 0, 0,  0, 2,  39, 6};
+      EXPECT_CALL(*mock_socket_wrapper,
+                  sending(testing::_, testing::_, sizeof(SEND_OPERATION_ENABLE) / sizeof(SEND_OPERATION_ENABLE[0]), 0))
+          .WillOnce(testing::Return(sizeof(SEND_OPERATION_ENABLE)));
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 19, 0))
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(handshake_send_operation_enable,
+                                           handshake_send_operation_enable + sizeof(handshake_send_operation_enable)),
+              testing::Return(sizeof(handshake_send_operation_enable))));
+
+      dryve_d1_gate::D1 d1 = dryve_d1_gate::D1(ip_address, port, std::move(mock_socket_wrapper));
+
+      WHEN("send_constant_set_command is called")
+      {
+        d1.send_constant_set_command(SEND_OPERATION_ENABLE, sizeof(SEND_OPERATION_ENABLE));
+
+        THEN("the previously defined expected calls should run through and throw no error")
+        {
+          // There is no check we can do here as we only want the called function to run through with our
+          // predefined expected calls
+          REQUIRE(true);
+        }
+      }
+    }
+  }
+
+  SCENARIO("Testing the reset_dryve_status function of the D1 class")
+  {
+    GIVEN("A test server, the D1 class and the expected calls")
+    {
+      int port = 12345;
+      start_test_server(port);
+      std::string ip_address = "127.0.0.1";
+      std::unique_ptr<MockSocketWrapper> mock_socket_wrapper = std::make_unique<MockSocketWrapper>();
+
+      const unsigned char RESET_DRYVE_STATUS[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 64, 0, 0, 0, 0, 2, 0, 1};
+      char handshake_reset_dryve_status[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_reset_dryve_status[12] = RESET_DRYVE_STATUS[12];
+      handshake_reset_dryve_status[13] = RESET_DRYVE_STATUS[13];
+      handshake_reset_dryve_status[14] = RESET_DRYVE_STATUS[14];
+
+      const unsigned char STATUS_OPERATION_ENABLE[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                         0, 96, 65, 0, 0, 0,  0, 2,  39, 6};
+      EXPECT_CALL(*mock_socket_wrapper,
+                  sending(testing::_, testing::_, sizeof(RESET_DRYVE_STATUS) / sizeof(RESET_DRYVE_STATUS[0]), 0))
+          .WillOnce(testing::Return(sizeof(RESET_DRYVE_STATUS)));
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 19, 0))
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(handshake_reset_dryve_status,
+                                           handshake_reset_dryve_status + sizeof(handshake_reset_dryve_status)),
+              testing::Return(sizeof(handshake_reset_dryve_status))));
+
+      dryve_d1_gate::D1 d1 = dryve_d1_gate::D1(ip_address, port, std::move(mock_socket_wrapper));
+
+      WHEN("reset_dryve_status is called")
+      {
+        d1.reset_dryve_status();
+
+        THEN("the previously defined expected calls should run through and throw no error")
+        {
+          // There is no check we can do here as we only want the called function to run through with our
+          // predefined expected calls
+          REQUIRE(true);
+        }
+      }
+    }
+  }
+
+  SCENARIO("Testing the set_dryve_mode_of_operation function of the D1 class")
+  {
+    GIVEN("A test server, the D1 class and the expected calls")
+    {
+      int port = 12345;
+      start_test_server(port);
+      std::string ip_address = "127.0.0.1";
+      std::unique_ptr<MockSocketWrapper> mock_socket_wrapper = std::make_unique<MockSocketWrapper>();
+
+      unsigned char mode = 6;
+
+      unsigned char send_mode_of_operation[20] = {0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 96, 96, 0, 0, 0, 0, 1, mode};
+
+      char handshake_send_mode_of_operation[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_send_mode_of_operation[12] = send_mode_of_operation[12];
+      handshake_send_mode_of_operation[13] = send_mode_of_operation[13];
+      handshake_send_mode_of_operation[14] = send_mode_of_operation[14];
+
+      const unsigned char READ_MODES_DISPLAY[19] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 97, 0, 0, 0, 0, 1};
+
+      unsigned char status_mode_display[] = {0, 0, 0, 0, 0, 14, 0, 43, 13, 0, 0, 0, 96, 97, 0, 0, 0, 0, 1, mode};
+
+      EXPECT_CALL(
+          *mock_socket_wrapper,
+          sending(testing::_, testing::_, sizeof(send_mode_of_operation) / sizeof(send_mode_of_operation[0]), 0))
+          .WillOnce(testing::Return(sizeof(send_mode_of_operation)));
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 19, 0))
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(handshake_send_mode_of_operation,
+                                           handshake_send_mode_of_operation + sizeof(handshake_send_mode_of_operation)),
+              testing::Return(sizeof(handshake_send_mode_of_operation))));
+
+      EXPECT_CALL(*mock_socket_wrapper,
+                  sending(testing::_, testing::_, sizeof(READ_MODES_DISPLAY) / sizeof(READ_MODES_DISPLAY[0]), 0))
+          .WillOnce(testing::Return(sizeof(READ_MODES_DISPLAY)));
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 23, 0))
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(status_mode_display, status_mode_display + sizeof(status_mode_display)),
+              testing::Return(21)));
+
+      dryve_d1_gate::D1 d1 = dryve_d1_gate::D1(ip_address, port, std::move(mock_socket_wrapper));
+
+      WHEN("set_dryve_mode_of_operation is called")
+      {
+        d1.set_dryve_mode_of_operation(mode);
+
+        THEN("the previously defined expected calls should run through and throw no error")
+        {
+          // There is no check we can do here as we only want the called function to run through with our
+          // predefined expected calls
+          REQUIRE(true);
+        }
+      }
+    }
+  }
+
+  SCENARIO("Testing the start_dryve_homing function of the D1 class")
+  {
+    GIVEN("A test server, the D1 class and the expected calls")
+    {
+      int port = 12345;
+      start_test_server(port);
+      std::string ip_address = "127.0.0.1";
+      std::unique_ptr<MockSocketWrapper> mock_socket_wrapper = std::make_unique<MockSocketWrapper>();
+
+      unsigned char mode = 6;
+      float switch_velo = 10;
+      float zero_velo = 10;
+      float homing_acc = 10;
+
+      unsigned char send_mode_of_operation[20] = {0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 96, 96, 0, 0, 0, 0, 1, mode};
+
+      char handshake_send_mode_of_operation[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_send_mode_of_operation[12] = send_mode_of_operation[12];
+      handshake_send_mode_of_operation[13] = send_mode_of_operation[13];
+      handshake_send_mode_of_operation[14] = send_mode_of_operation[14];
+
+      const unsigned char READ_MODES_DISPLAY[19] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 97, 0, 0, 0, 0, 1};
+
+      unsigned char status_mode_display[] = {0, 0, 0, 0, 0, 14, 0, 43, 13, 0, 0, 0, 96, 97, 0, 0, 0, 0, 1, mode};
+
+      EXPECT_CALL(
+          *mock_socket_wrapper,
+          sending(testing::_, testing::_, sizeof(send_mode_of_operation) / sizeof(send_mode_of_operation[0]), 0))
+          .WillOnce(testing::Return(sizeof(send_mode_of_operation)));
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 19, 0))
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(handshake_send_mode_of_operation,
+                                           handshake_send_mode_of_operation + sizeof(handshake_send_mode_of_operation)),
+              testing::Return(sizeof(handshake_send_mode_of_operation))));
+
+      EXPECT_CALL(*mock_socket_wrapper,
+                  sending(testing::_, testing::_, sizeof(READ_MODES_DISPLAY) / sizeof(READ_MODES_DISPLAY[0]), 0))
+          .WillOnce(testing::Return(sizeof(READ_MODES_DISPLAY)));
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 23, 0))
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(status_mode_display, status_mode_display + sizeof(status_mode_display)),
+              testing::Return(21)));
+
+      dryve_d1_gate::D1 d1 = dryve_d1_gate::D1(ip_address, port, std::move(mock_socket_wrapper));
+
+      WHEN("start_dryve_homing is called")
+      {
+        d1.set_dryve_mode_of_operation(mode);
+
+        THEN("the previously defined expected calls should run through and throw no error")
+        {
+          // There is no check we can do here as we only want the called function to run through with our
           // predefined expected calls
           REQUIRE(true);
         }
