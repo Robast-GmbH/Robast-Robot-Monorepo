18d2cd940a4b82411e68e6c22be859425bab0fcc
TipluJacob <j.ritterbach@tiplu.de>
2023-03-06 18:26:40 +0100

    Unit test for start_dryve_homing

--- src/ros2/hardware_nodes/dryve_d1_gate/test/unit_tests/test_d1.cpp
+++ src/ros2/hardware_nodes/dryve_d1_gate/test/unit_tests/test_d1.cpp
@@ -924,7 +924,8 @@ namespace unit_test
           .WillOnce(testing::DoAll(
               testing::SetArrayArgument<1>(handshake_send_reset_start_rel,
                                            handshake_send_reset_start_rel + sizeof(handshake_send_reset_start_rel)),
-              testing::Return(sizeof(handshake_send_reset_start_rel))))   // mock for _SEND_RESET_START_REL(1)
+              testing::Return(sizeof(
+                  handshake_send_reset_start_rel))))   //  mock for send_command_telegram(_SEND_RESET_START_REL,...)
           .WillOnce(testing::DoAll(
               testing::SetArrayArgument<1>(handshake_start_movement,
                                            handshake_start_movement + sizeof(handshake_start_movement)),
@@ -1097,6 +1098,163 @@ namespace unit_test
       WHEN("set_profile_velocity is called")
       {
         std::string error_msg = d1.set_profile_velocity(target_velocity, target_accel, target_decel);
+
+        THEN(
+            "the previously defined expected calls should run through, throw no error and we should get an empty error "
+            "msg")
+        {
+          REQUIRE(testing::Mock::VerifyAndClearExpectations(&mock_socket_wrapper));
+          REQUIRE(error_msg.empty() == true);
+        }
+      }
+    }
+  }
+
+  SCENARIO("Testing the start_dryve_homing function of the D1 class")
+  {
+    GIVEN("A test server, the D1 class and the expected calls")
+    {
+      int port = 12345;
+      start_test_server(port);
+      std::string ip_address = "127.0.0.1";
+      std::unique_ptr<MockSocketWrapper> mock_socket_wrapper = std::make_unique<MockSocketWrapper>();
+
+      float target_switch_velo = 10.0;
+      float target_zero_velo = 0.0;
+      float target_homing_acc = 2.0;
+
+      // Preparation for set_dryve_mode_of_operation() func call
+      unsigned char mode = 6;
+      unsigned char send_mode_of_operation[20] = {0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 96, 96, 0, 0, 0, 0, 1, mode};
+      char handshake_send_mode_of_operation[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_send_mode_of_operation[12] = send_mode_of_operation[12];
+      handshake_send_mode_of_operation[13] = send_mode_of_operation[13];
+      handshake_send_mode_of_operation[14] = send_mode_of_operation[14];
+      const unsigned char status_mode_display[] = {0, 0, 0, 0, 0, 14, 0, 43, 13, 0, 0, 0, 96, 97, 0, 0, 0, 0, 1, mode};
+
+      // Preparation for get_si_unit_factor() func call
+      unsigned char expected_recv_buffer[23];
+      expected_recv_buffer[19] = 0;
+      expected_recv_buffer[20] = 0;
+      expected_recv_buffer[21] = MOVEMENT_TYPE_ROTARY;
+      expected_recv_buffer[22] = 0xFE;
+
+      // Preparation for check_for_dryve_error() func call
+      const unsigned char STATUS_READY[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 39, 22};
+
+      // Preparation for send_command_telegram(_send_switch_velocity, ...) func call
+      unsigned char send_switch_velocity[23] = {0,  0,   0, 0, 0, 17, 0, 43, 13, 1, 0, 0,
+                                                96, 153, 1, 0, 0, 0,  4, 0,  0,  0, 0};
+      char handshake_send_switch_velocity[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_send_switch_velocity[12] = send_switch_velocity[12];
+      handshake_send_switch_velocity[13] = send_switch_velocity[13];
+      handshake_send_switch_velocity[14] = send_switch_velocity[14];
+
+      // Preparation for send_command_telegram(_send_zero_velocity, ...) func call
+      unsigned char send_zero_velocity[23] = {0,  0,   0, 0, 0, 17, 0, 43, 13, 1, 0, 0,
+                                              96, 153, 2, 0, 0, 0,  4, 0,  0,  0, 0};
+      char handshake_send_zero_velocity[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_send_zero_velocity[12] = send_zero_velocity[12];
+      handshake_send_zero_velocity[13] = send_zero_velocity[13];
+      handshake_send_zero_velocity[14] = send_zero_velocity[14];
+
+      // Preparation for send_command_telegram(_send_homing_acceleration, ...) func
+      // call
+      unsigned char send_homing_acceleration[23] = {0,  0,   0, 0, 0, 17, 0, 43, 13, 1, 0, 0,
+                                                    96, 154, 0, 0, 0, 0,  4, 0,  0,  0, 0};
+      char handshake_send_homing_acceleration[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_send_homing_acceleration[12] = send_homing_acceleration[12];
+      handshake_send_homing_acceleration[13] = send_homing_acceleration[13];
+      handshake_send_homing_acceleration[14] = send_homing_acceleration[14];
+
+      // Preparation for send_command_telegram(_SEND_RESET_START, ...) func call
+      const unsigned char SEND_RESET_START[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 64, 0, 0, 0, 0, 2, 15, 0};
+      char handshake_send_reset_start[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_send_reset_start[12] = SEND_RESET_START[12];
+      handshake_send_reset_start[13] = SEND_RESET_START[13];
+      handshake_send_reset_start[14] = SEND_RESET_START[14];
+
+      // Preparation for send_command_telegram(_SEND_START_MOVEMENT, ...) func call
+      const unsigned char SEND_START_MOVEMENT[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                     0, 96, 64, 0, 0, 0,  0, 2,  31, 0};
+      char handshake_start_movement[] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+      handshake_start_movement[12] = SEND_START_MOVEMENT[12];
+      handshake_start_movement[13] = SEND_START_MOVEMENT[13];
+      handshake_start_movement[14] = SEND_START_MOVEMENT[14];
+
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 19, 0))
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(handshake_send_mode_of_operation,
+                                           handshake_send_mode_of_operation + sizeof(handshake_send_mode_of_operation)),
+              testing::Return(sizeof(handshake_send_mode_of_operation))))   // mock for set_dryve_mode_of_operation(1)
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(handshake_send_switch_velocity,
+                                           handshake_send_switch_velocity + sizeof(handshake_send_switch_velocity)),
+              testing::Return(sizeof(
+                  handshake_send_switch_velocity))))   // mock for send_command_telegram(_send_switch_velocity,...)
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(handshake_send_zero_velocity,
+                                           handshake_send_zero_velocity + sizeof(handshake_send_zero_velocity)),
+              testing::Return(
+                  sizeof(handshake_send_zero_velocity))))   // mock for send_command_telegram(_send_zero_velocity,...)
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(
+                  handshake_send_homing_acceleration,
+                  handshake_send_homing_acceleration + sizeof(handshake_send_homing_acceleration)),
+              testing::Return(sizeof(
+                  handshake_send_homing_acceleration))))   // mock for
+                                                           // send_command_telegram(_send_homing_acceleration,...)
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(handshake_send_reset_start,
+                                           handshake_send_reset_start + sizeof(handshake_send_reset_start)),
+              testing::Return(
+                  sizeof(handshake_send_reset_start))))   // mock for send_command_telegram(_SEND_RESET_START,...)
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(handshake_start_movement,
+                                           handshake_start_movement + sizeof(handshake_start_movement)),
+              testing::Return(
+                  sizeof(handshake_start_movement))));   // mock for send_command_telegram(_SEND_START_MOVEMENT,...)
+
+      EXPECT_CALL(
+          *mock_socket_wrapper,
+          sending(testing::_, testing::_, sizeof(send_mode_of_operation) / sizeof(send_mode_of_operation[0]), 0))
+          .WillOnce(testing::Return(sizeof(send_mode_of_operation)));
+
+      EXPECT_CALL(*mock_socket_wrapper, receiving(testing::_, testing::_, 23, 0))
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(status_mode_display, status_mode_display + sizeof(status_mode_display)),
+              testing::Return(21)))   // mock for set_dryve_mode_of_operation(1)
+          .WillOnce(testing::DoAll(
+              testing::SetArrayArgument<1>(expected_recv_buffer, expected_recv_buffer + sizeof(expected_recv_buffer)),
+              testing::Return(sizeof(expected_recv_buffer))))   // mock for get_si_unit_factor()
+          .WillOnce(testing::DoAll(testing::SetArrayArgument<1>(STATUS_READY, STATUS_READY + sizeof(STATUS_READY)),
+                                   testing::Return(sizeof(STATUS_READY))))   // mock for check_for_dryve_error()
+          .WillOnce(testing::DoAll(testing::SetArrayArgument<1>(STATUS_READY, STATUS_READY + sizeof(STATUS_READY)),
+                                   testing::Return(sizeof(STATUS_READY))))   // mock for wait_for_homing()
+          .WillOnce(testing::DoAll(testing::SetArrayArgument<1>(STATUS_READY, STATUS_READY + sizeof(STATUS_READY)),
+                                   testing::Return(sizeof(STATUS_READY))));   // mock for wait_for_homing()
+
+      EXPECT_CALL(*mock_socket_wrapper, sending(testing::_, testing::_, 19, 0))
+          .WillOnce(testing::Return(19))    // mock for set_dryve_mode_of_operation(1)
+          .WillOnce(testing::Return(19))    // mock for get_si_unit_factor()
+          .WillOnce(testing::Return(19))    // mock for check_for_dryve_error()
+          .WillOnce(testing::Return(19))    // mock for wait_for_homing()
+          .WillOnce(testing::Return(19));   // mock for wait_for_homing()
+
+      EXPECT_CALL(*mock_socket_wrapper, sending(testing::_, testing::_, 21, 0))
+          .WillOnce(testing::Return(21))    // mock for send_command_telegram(_SEND_RESET_START,...)
+          .WillOnce(testing::Return(21));   // mock for send_command_telegram(_SEND_START_MOVEMENT,...)
+
+      EXPECT_CALL(*mock_socket_wrapper, sending(testing::_, testing::_, 23, 0))
+          .WillOnce(testing::Return(23))    // mock for send_command_telegram(_send_switch_velocity,...)
+          .WillOnce(testing::Return(23))    // mock for send_command_telegram(_send_zero_velocity,...)
+          .WillOnce(testing::Return(23));   // mock for send_command_telegram(_send_homing_acceleration,...)
+
+      dryve_d1_gate::D1 d1 = dryve_d1_gate::D1(ip_address, port, std::move(mock_socket_wrapper));
+
+      WHEN("start_dryve_homing is called")
+      {
+        std::string error_msg = d1.start_dryve_homing(target_switch_velo, target_zero_velo, target_homing_acc);
 
         THEN(
             "the previously defined expected calls should run through, throw no error and we should get an empty error "
