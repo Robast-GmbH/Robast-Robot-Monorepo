9ccbdf120adae02d4bbbfc26f30597194bbe461f
TipluJacob <j.ritterbach@tiplu.de>
2023-03-08 17:01:06 +0100

    const -> static constexpr

--- src/ros2/hardware_nodes/dryve_d1_gate/include/dryve_d1_gate/d1.hpp
+++ src/ros2/hardware_nodes/dryve_d1_gate/include/dryve_d1_gate/d1.hpp
@@ -116,30 +116,40 @@ namespace dryve_d1_gate
 
     // Define Telegrams for frequently used objects
     // State machine
-    const unsigned char _SEND_SHUTDOWN[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 64, 0, 0, 0, 0, 2, 6, 0};
-    const unsigned char _SEND_SWITCH_ON[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 64, 0, 0, 0, 0, 2, 7, 0};
-    const unsigned char _SEND_OPERATION_ENABLE[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
-                                                      0, 96, 64, 0, 0, 0,  0, 2,  15, 0};
-    const unsigned char _RESET_DRYVE_STATUS[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 64, 0, 0, 0, 0, 2, 0, 1};
+    static constexpr unsigned char _SEND_SHUTDOWN[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                         0, 96, 64, 0, 0, 0,  0, 2,  6,  0};
+    static constexpr unsigned char _SEND_SWITCH_ON[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                          0, 96, 64, 0, 0, 0,  0, 2,  7,  0};
+    static constexpr unsigned char _SEND_OPERATION_ENABLE[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                                 0, 96, 64, 0, 0, 0,  0, 2,  15, 0};
+    static constexpr unsigned char _RESET_DRYVE_STATUS[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                              0, 96, 64, 0, 0, 0,  0, 2,  0,  1};
 
     // Telegrams for resetting the dryve status
-    const unsigned char _SEND_RESET_ERROR[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 64, 0, 0, 0, 0, 2, 0, 1};
-    const unsigned char _SEND_RESET_ARRAY[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 64, 0, 0, 0, 0, 2, 143, 0};
+    static constexpr unsigned char _SEND_RESET_ERROR[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                            0, 96, 64, 0, 0, 0,  0, 2,  0,  1};
+    static constexpr unsigned char _SEND_RESET_ARRAY[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13,  1, 0,
+                                                            0, 96, 64, 0, 0, 0,  0, 2,  143, 0};
 
     // Telegrams to read status and values of objects
-    const unsigned char _READ_STATUS_WORD[19] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2};
-    const unsigned char _READ_MODES_DISPLAY[19] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 97, 0, 0, 0, 0, 1};
-    const unsigned char _READ_POSITION_ACTUAL_VALUE[19] = {
+    static constexpr unsigned char _READ_STATUS_WORD[19] = {
+        0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2};
+    static constexpr unsigned char _READ_MODES_DISPLAY[19] = {
+        0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 97, 0, 0, 0, 0, 1};
+    static constexpr unsigned char _READ_POSITION_ACTUAL_VALUE[19] = {
         0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 100, 0, 0, 0, 0, 4};
-    const unsigned char _READ_VELOCITY_ACTUAL_VALUE[19] = {
+    static constexpr unsigned char _READ_VELOCITY_ACTUAL_VALUE[19] = {
         0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 108, 0, 0, 0, 0, 4};
-    const unsigned char _READ_CURRENT_ACTUAL_VALUE[19] = {
+    static constexpr unsigned char _READ_CURRENT_ACTUAL_VALUE[19] = {
         0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 120, 0, 0, 0, 0, 4};
-    const unsigned char _READ_FOLLOWING_ERROR_ACTUAL_VALUE[19] = {
+    static constexpr unsigned char _READ_FOLLOWING_ERROR_ACTUAL_VALUE[19] = {
         0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 244, 0, 0, 0, 0, 4};
-    const unsigned char _READ_ERROR_CODE[19] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 63, 0, 0, 0, 0, 2};
-    const unsigned char _READ_SI_UNIT_FACTOR[19] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 168, 0, 0, 0, 0, 4};
-    const unsigned char _READ_CONTROLLER_TEMP[19] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 32, 19, 0, 0, 0, 0, 4};
+    static constexpr unsigned char _READ_ERROR_CODE[19] = {
+        0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 63, 0, 0, 0, 0, 2};
+    static constexpr unsigned char _READ_SI_UNIT_FACTOR[19] = {
+        0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 96, 168, 0, 0, 0, 0, 4};
+    static constexpr unsigned char _READ_CONTROLLER_TEMP[19] = {
+        0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 32, 19, 0, 0, 0, 0, 4};
     unsigned char _read_buffer[19] = {0, 0, 0, 0, 0, 13, 0, 43, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4};
 
     // Telegrams for initial parameters
@@ -152,7 +162,8 @@ namespace dryve_d1_gate
     unsigned char _send_mode_of_operation[20] = {0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 96, 96, 0, 0, 0, 0, 1, 0};
 
     // Telegrams for homing/referencing
-    const unsigned char _SEND_MODE_HOMING[20] = {0, 0, 0, 0, 0, 14, 0, 43, 13, 1, 0, 0, 96, 96, 0, 0, 0, 0, 1, 6};
+    static constexpr unsigned char _SEND_MODE_HOMING[20] = {0, 0, 0,  0,  0, 14, 0, 43, 13, 1,
+                                                            0, 0, 96, 96, 0, 0,  0, 0,  1,  6};
     unsigned char _send_switch_velocity[23] = {0,  0,   0, 0, 0, 17, 0, 43, 13, 1, 0, 0,
                                                96, 153, 1, 0, 0, 0,  4, 0,  0,  0, 0};
     unsigned char _send_zero_velocity[23] = {0, 0, 0, 0, 0, 17, 0, 43, 13, 1, 0, 0, 96, 153, 2, 0, 0, 0, 4, 0, 0, 0, 0};
@@ -160,10 +171,10 @@ namespace dryve_d1_gate
                                                    96, 154, 0, 0, 0, 0,  4, 0,  0,  0, 0};
 
     // Telegrams for Profile Position Mode and Profile Velocity Mode
-    const unsigned char _SEND_MODE_PROFILE_POSITION[20] = {0, 0, 0,  0,  0, 14, 0, 43, 13, 1,
-                                                           0, 0, 96, 96, 0, 0,  0, 0,  1,  1};
-    const unsigned char _SEND_MODE_PROFILE_VELOCITY[20] = {0, 0, 0,  0,  0, 14, 0, 43, 13, 1,
-                                                           0, 0, 96, 96, 0, 0,  0, 0,  1,  3};
+    static constexpr unsigned char _SEND_MODE_PROFILE_POSITION[20] = {0, 0, 0,  0,  0, 14, 0, 43, 13, 1,
+                                                                      0, 0, 96, 96, 0, 0,  0, 0,  1,  1};
+    static constexpr unsigned char _SEND_MODE_PROFILE_VELOCITY[20] = {0, 0, 0,  0,  0, 14, 0, 43, 13, 1,
+                                                                      0, 0, 96, 96, 0, 0,  0, 0,  1,  3};
     unsigned char _send_profile_velocity[23] = {0,  0,   0, 0, 0, 17, 0, 43, 13, 1, 0, 0,
                                                 96, 129, 0, 0, 0, 0,  4, 0,  0,  0, 0};
     unsigned char _send_profile_acceleration[23] = {0,  0,   0, 0, 0, 17, 0, 43, 13, 1, 0, 0,
@@ -176,49 +187,66 @@ namespace dryve_d1_gate
                                                96, 122, 0, 0, 0, 0,  4, 0,  0,  0, 0};
 
     // Telegrams to start a movement
-    const unsigned char _SEND_START_MOVEMENT[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
-                                                    0, 96, 64, 0, 0, 0,  0, 2,  31, 0};
-    const unsigned char _SEND_START_MOVEMENT_REL[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
-                                                        0, 96, 64, 0, 0, 0,  0, 2,  95, 0};
-    const unsigned char _SEND_RESET_START[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 1, 0, 0, 96, 64, 0, 0, 0, 0, 2, 15, 0};
-    const unsigned char _SEND_RESET_START_ABS[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
-                                                     0, 96, 64, 0, 0, 0,  0, 2,  15, 0};
-    const unsigned char _SEND_RESET_START_REL[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
-                                                     0, 96, 64, 0, 0, 0,  0, 2,  79, 0};
+    static constexpr unsigned char _SEND_START_MOVEMENT[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                               0, 96, 64, 0, 0, 0,  0, 2,  31, 0};
+    static constexpr unsigned char _SEND_START_MOVEMENT_REL[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                                   0, 96, 64, 0, 0, 0,  0, 2,  95, 0};
+    static constexpr unsigned char _SEND_RESET_START[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                            0, 96, 64, 0, 0, 0,  0, 2,  15, 0};
+    static constexpr unsigned char _SEND_RESET_START_ABS[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                                0, 96, 64, 0, 0, 0,  0, 2,  15, 0};
+    static constexpr unsigned char _SEND_RESET_START_REL[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 1, 0,
+                                                                0, 96, 64, 0, 0, 0,  0, 2,  79, 0};
 
     // Status Word to check if the homing or the movement was completet correctly
-    const unsigned char _STATUS_READY[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 39, 22};
-    const unsigned char _STATUS_READY_2[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 8, 6};
-    const unsigned char _STATUS_READY_3[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 8, 2};
-    const unsigned char _STATUS_READY_4[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 8, 34};
-    const unsigned char _STATUS_READY_5[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 64, 22};
-    const unsigned char _STATUS_READY_6[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 64, 18};
-    const unsigned char _STATUS_READY_7[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 64, 2};
+    static constexpr unsigned char _STATUS_READY[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                        0, 96, 65, 0, 0, 0,  0, 2,  39, 22};
+    static constexpr unsigned char _STATUS_READY_2[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                          0, 96, 65, 0, 0, 0,  0, 2,  8,  6};
+    static constexpr unsigned char _STATUS_READY_3[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                          0, 96, 65, 0, 0, 0,  0, 2,  8,  2};
+    static constexpr unsigned char _STATUS_READY_4[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                          0, 96, 65, 0, 0, 0,  0, 2,  8,  34};
+    static constexpr unsigned char _STATUS_READY_5[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                          0, 96, 65, 0, 0, 0,  0, 2,  64, 22};
+    static constexpr unsigned char _STATUS_READY_6[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                          0, 96, 65, 0, 0, 0,  0, 2,  64, 18};
+    static constexpr unsigned char _STATUS_READY_7[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                          0, 96, 65, 0, 0, 0,  0, 2,  64, 2};
 
     // Status Word to that indicates that an error occured
-    const unsigned char _STATUS_ERROR[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 8, 6};
-    const unsigned char _STATUS_ERROR_2[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 8, 38};
-    const unsigned char _STATUS_ERROR_3[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 8, 16};
-    const unsigned char _STATUS_ERROR_4[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 8, 22};
+    static constexpr unsigned char _STATUS_ERROR[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                        0, 96, 65, 0, 0, 0,  0, 2,  8,  6};
+    static constexpr unsigned char _STATUS_ERROR_2[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                          0, 96, 65, 0, 0, 0,  0, 2,  8,  38};
+    static constexpr unsigned char _STATUS_ERROR_3[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                          0, 96, 65, 0, 0, 0,  0, 2,  8,  16};
+    static constexpr unsigned char _STATUS_ERROR_4[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                          0, 96, 65, 0, 0, 0,  0, 2,  8,  22};
 
     // Status Word to check if the shutdown was done correctly
-    const unsigned char _STATUS_SHUTDOWN[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 33, 6};
-    const unsigned char _STATUS_SHUTDOWN_2[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 33, 22};
-    const unsigned char _STATUS_SHUTDOWN_3[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 33, 2};
+    static constexpr unsigned char _STATUS_SHUTDOWN[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                           0, 96, 65, 0, 0, 0,  0, 2,  33, 6};
+    static constexpr unsigned char _STATUS_SHUTDOWN_2[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                             0, 96, 65, 0, 0, 0,  0, 2,  33, 22};
+    static constexpr unsigned char _STATUS_SHUTDOWN_3[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                             0, 96, 65, 0, 0, 0,  0, 2,  33, 2};
 
     // Status Word to check if the switch on was done correctly
-    const unsigned char _STATUS_SWITCH_ON[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 35, 6};
-    const unsigned char _STATUS_SWITCH_ON_2[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
-                                                   0, 96, 65, 0, 0, 0,  0, 2,  35, 22};
-    const unsigned char _STATUS_SWITCH_ON_3[21] = {0, 0, 0, 0, 0, 15, 0, 43, 13, 0, 0, 0, 96, 65, 0, 0, 0, 0, 2, 35, 2};
+    static constexpr unsigned char _STATUS_SWITCH_ON[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                            0, 96, 65, 0, 0, 0,  0, 2,  35, 6};
+    static constexpr unsigned char _STATUS_SWITCH_ON_2[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                              0, 96, 65, 0, 0, 0,  0, 2,  35, 22};
+    static constexpr unsigned char _STATUS_SWITCH_ON_3[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                              0, 96, 65, 0, 0, 0,  0, 2,  35, 2};
 
     // Status Word to check if the operation enable was done correctly
-    const unsigned char _STATUS_OPERATION_ENABLE[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
-                                                        0, 96, 65, 0, 0, 0,  0, 2,  39, 6};
-    const unsigned char _STATUS_OPERATION_ENABLE_2[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
-                                                          0, 96, 65, 0, 0, 0,  0, 2,  39, 22};
-    const unsigned char _STATUS_OPERATION_ENABLE_3[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
-                                                          0, 96, 65, 0, 0, 0,  0, 2,  39, 2};
+    static constexpr unsigned char _STATUS_OPERATION_ENABLE[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                                   0, 96, 65, 0, 0, 0,  0, 2,  39, 6};
+    static constexpr unsigned char _STATUS_OPERATION_ENABLE_2[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                                     0, 96, 65, 0, 0, 0,  0, 2,  39, 22};
+    static constexpr unsigned char _STATUS_OPERATION_ENABLE_3[21] = {0, 0,  0,  0, 0, 15, 0, 43, 13, 0, 0,
+                                                                     0, 96, 65, 0, 0, 0,  0, 2,  39, 2};
   };
 }   // namespace dryve_d1_gate
 
