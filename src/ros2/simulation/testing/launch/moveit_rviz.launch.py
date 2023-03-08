c3fa5be1963b98c4d2fd1aadd3e6bb3c7588f4a2
TipluJacob <j.ritterbach@tiplu.de>
2023-03-08 08:38:11 +0100

    Removing unused files

--- src/ros2/simulation/testing/launch/moveit_rviz.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_moveit_rviz_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("testron", package_name="testing").to_moveit_configs()
-    return generate_moveit_rviz_launch(moveit_config)
