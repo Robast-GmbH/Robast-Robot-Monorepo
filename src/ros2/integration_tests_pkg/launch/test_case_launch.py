b68dee665294d6c13f6b7488cc9cce3682d5fbcd
Tobi <t.alscher@robast.de>
2023-03-14 09:57:44 +0100

    changed the simulation start to headless

--- src/ros2/integration_tests_pkg/launch/test_case_launch.py
+++ src/ros2/integration_tests_pkg/launch/test_case_launch.py
@@ -29,9 +29,14 @@ def generate_launch_description():
     context = LaunchContext()
     new_yaml = configured_params.perform(context)
     
+    sim_arguments = {
+        "headless": " -s ",
+        "world_model": os.path.join(get_package_share_directory("tiplu_world"), "worlds", "6OG" + ".sdf")
+    }.items()
+    
     ignition_tiplu_world_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(sim_bringup_dir, 'launch', 'tiplu_world_launch.py')))
+            os.path.join(sim_bringup_dir, 'launch', 'tiplu_world_launch.py')),launch_arguments=sim_arguments)
     
     nav2_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -57,25 +62,25 @@ def generate_launch_description():
     ld.add_action(SetEnvironmentVariable('RCUTILS_LOGGING_USE_STDOUT', '1'))
     
     ld.add_action(ignition_tiplu_world_launch)
-    # ld.add_action(robot_localization_launch)
-    # ld.add_action(slam_launch)
-    # ld.add_action(nav2_launch)
+    ld.add_action(robot_localization_launch)
+    ld.add_action(slam_launch)
+    ld.add_action(nav2_launch)
     
     return ld
 
-# def main(argv=sys.argv[1:]):
-#     ld = generate_launch_description()
+def main(argv=sys.argv[1:]):
+    ld = generate_launch_description()
 
-#     test1_action = ExecuteProcess(
-#         cmd=[os.path.join(os.getenv('TEST_DIR'), 'tester.py')],
-#         name='tester_node',
-#         output='screen')
+    test1_action = ExecuteProcess(
+        cmd=[os.path.join(os.getenv('TEST_DIR'), 'tester.py')],
+        name='tester_node',
+        output='screen')
 
-#     lts = LaunchTestService()
-#     lts.add_test_action(ld, test1_action)
-#     ls = LaunchService(argv=argv)
-#     ls.include_launch_description(ld)
-#     return lts.run(ls)
+    lts = LaunchTestService()
+    lts.add_test_action(ld, test1_action)
+    ls = LaunchService(argv=argv)
+    ls.include_launch_description(ld)
+    return lts.run(ls)
 
 
 # if __name__ == '__main__':
