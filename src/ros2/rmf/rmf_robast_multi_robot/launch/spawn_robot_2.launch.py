97a5795e3339c689221ff8915ecc24fa46c2cba8
Torben Zurhelle <t.zurhelle@robast.de>
2023-04-12 09:12:16 +0200

    gazebo sim config and swanspam fix

---
+++ src/ros2/rmf/rmf_robast_multi_robot/launch/spawn_robot_2.launch.py
@@ -0,0 +1,84 @@
+import os
+
+import xacro
+import yaml
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node, PushRosNamespace
+from launch.actions import GroupAction
+from launch.launch_description_sources import AnyLaunchDescriptionSource
+
+
+def generate_launch_description():
+    with open("environment_vars.yaml", "r") as stream:
+        try:
+            environment_yaml = yaml.safe_load(stream)
+            print(environment_yaml)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    use_sim_time =True
+    namespace="rb2"
+    robot_xml2 = xacro.process_file(
+        os.path.join(
+            get_package_share_directory("rb_theron_description"),
+            "robots",
+            environment_yaml["robot"] + ".urdf.xacro",
+        ),
+        mappings={"prefix": environment_yaml["prefix"]},
+    ).toxml()
+    
+    declare_robot_model_cmd = DeclareLaunchArgument(
+        "robot_name",
+        default_value="rb_theron",
+        description="name of the robot in the simulation",
+    )
+   
+    start_robot_state_publisher_cmd = Node(
+        name="robot_state_publisher2",
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[{"use_sim_time": use_sim_time}, {"robot_description": robot_xml2}],
+        remappings=[('/tf', 'tf'), ('/tf_static','tf_static')],
+    )
+    
+
+    spawn_robot_cmd = Node(
+       
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-name",
+            "tb_2",
+            "-topic",
+            "robot_description",
+             "-z",
+            "0.2",
+            "-x",
+            "7",
+            "-y",
+            "0",
+            "-Y",
+            "0",
+           
+        ],
+       
+    )    
+    
+    group = GroupAction([
+        PushRosNamespace(namespace=namespace), 
+        declare_robot_model_cmd,
+        start_robot_state_publisher_cmd,
+        spawn_robot_cmd,
+    ])
+
+    ld = LaunchDescription()
+    ld.add_action(group)
+
+
+    
+    return ld
