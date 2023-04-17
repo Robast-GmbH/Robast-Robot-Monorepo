348d0ebe173d67b79275aedb373b3877b29c7df5
Andreas <a.neumann@robast.de>
2023-04-17 16:54:44 +0200

    Move shelf setup into own file

--- src/ros2/hardware_nodes/drawer_gate/src/drawer_gate.cpp
+++ src/ros2/hardware_nodes/drawer_gate/src/drawer_gate.cpp
@@ -144,61 +144,6 @@ namespace drawer_gate
     this->receive_can_msgs_timer_->cancel();
   }
 
-
-
-
-
-  std::vector<communication_interfaces::msg::Drawer> DrawerGate::get_all_mounted_drawers()
-  {
-    // TODO: This should actually be done automatically by polling all drawer_controller on the CAN bus
-    communication_interfaces::msg::Box box_10x40x1;
-    box_10x40x1.x = DRAWER_INSIDE_WIDTH_10x40x1;
-    box_10x40x1.y = DRAWER_INSIDE_DEPTH_10x40x1;
-    box_10x40x1.z = DRAWER_INSIDE_HEIGHT_10x40x1;
-
-    communication_interfaces::msg::Box box_20x40x1;
-    box_20x40x1.x = DRAWER_INSIDE_WIDTH_20x40x1;
-    box_20x40x1.y = DRAWER_INSIDE_DEPTH_20x40x1;
-    box_20x40x1.z = DRAWER_INSIDE_HEIGHT_20x40x1;
-
-    communication_interfaces::msg::Box box_30x40x1;
-    box_30x40x1.x = DRAWER_INSIDE_WIDTH_30x40x1;
-    box_30x40x1.y = DRAWER_INSIDE_DEPTH_30x40x1;
-    box_30x40x1.z = DRAWER_INSIDE_HEIGHT_30x40x1;
-
-    communication_interfaces::msg::Drawer drawer_1;
-    drawer_1.drawer_address.drawer_controller_id = 1;
-    drawer_1.drawer_address.drawer_id = 1;
-    drawer_1.number_of_drawers = 1;
-    drawer_1.drawer_size = box_10x40x1;
-
-    communication_interfaces::msg::Drawer drawer_2;
-    drawer_2.drawer_address.drawer_controller_id = 2;
-    drawer_2.drawer_address.drawer_id = 1;
-    drawer_2.number_of_drawers = 1;
-    drawer_2.drawer_size = box_10x40x1;
-
-    communication_interfaces::msg::Drawer drawer_3;
-    drawer_3.drawer_address.drawer_controller_id = 3;
-    drawer_3.drawer_address.drawer_id = 1;
-    drawer_3.number_of_drawers = 1;
-    drawer_3.drawer_size = box_10x40x1;
-
-    communication_interfaces::msg::Drawer drawer_4;
-    drawer_4.drawer_address.drawer_controller_id = 4;
-    drawer_4.drawer_address.drawer_id = 1;
-    drawer_4.number_of_drawers = 1;
-    drawer_4.drawer_size = box_20x40x1;
-
-    communication_interfaces::msg::Drawer drawer_5;
-    drawer_5.drawer_address.drawer_controller_id = 5;
-    drawer_5.drawer_address.drawer_id = 1;
-    drawer_5.number_of_drawers = 1;
-    drawer_5.drawer_size = box_30x40x1;
-
-    return {drawer_1, drawer_2, drawer_3, drawer_4, drawer_5};
-  }
-
   void DrawerGate::provide_shelf_setup_info_callback(const std::shared_ptr<ShelfSetupInfo::Request> request,
                                                      std::shared_ptr<ShelfSetupInfo::Response> response)
   {
