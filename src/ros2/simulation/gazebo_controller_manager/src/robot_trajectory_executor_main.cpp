7107021f43f4b9a713035229dcc31ee3080fb840
TipluJacob <j.ritterbach@tiplu.de>
2023-04-24 15:39:57 +0200

    Changing default joint names for robot_trajectory_executor

--- src/ros2/simulation/gazebo_controller_manager/src/robot_trajectory_executor_main.cpp
+++ src/ros2/simulation/gazebo_controller_manager/src/robot_trajectory_executor_main.cpp
@@ -8,8 +8,9 @@ int main(int argc, char* argv[])
   auto ros_node = std::make_shared<rclcpp::Node>("robot_trajectory_executor");
   // variable
   std::vector<std::string> joint_names;
-  std::vector<std::string> default_joint_names = {
-      "drawer_1_joint", "drawer_2_joint", "drawer_3_joint", "drawer_4_joint", "drawer_5_joint"};
+  std::vector<std::string> default_joint_names = {"door_opening_mechanism_joint_y_axis_slide",
+                                                  "door_opening_mechanism_joint_x_axis_slide",
+                                                  "door_opening_mechanism_joint_tilting_hook"};
   // parameters
   ros_node->declare_parameter("joint_names", default_joint_names);
   joint_names = ros_node->get_parameter("joint_names").get_parameter_value().get<std::vector<std::string>>();
