52ed4e3536d560e5021fe2eb6a95c373e6754d62
Andreas <a.neumann@robast.de>
2023-05-15 12:35:16 +0200

    Reformat

--- src/Hardware/cpp/Drawer_Controller/include/i_drawer.hpp
+++ src/Hardware/cpp/Drawer_Controller/include/i_drawer.hpp
@@ -5,13 +5,13 @@
 
 namespace drawer_controller
 {
-    class IDrawer
-    {
-    public:
-        virtual ~IDrawer(){};
-        virtual void can_in(robast_can_msgs::CanMessage msg) = 0;
-        virtual std::optional<robast_can_msgs::CanMessage> can_out() = 0;
-        virtual void update_state() = 0;
-    };
-}
+  class IDrawer
+  {
+   public:
+    virtual ~IDrawer(){};
+    virtual void can_in(robast_can_msgs::CanMessage msg) = 0;
+    virtual std::optional<robast_can_msgs::CanMessage> can_out() = 0;
+    virtual void update_state() = 0;
+  };
+}   // namespace drawer_controller
 #endif
