6c6b3fac82151c434db6f23a93fb29654a41bdcd
Andreas <a.neumann@robast.de>
2023-05-15 12:37:42 +0200

    Add namespace

--- src/ros2/hardware_nodes/drawer_bridge/include/drawer_bridge/can_encoder_decoder.hpp
+++ src/ros2/hardware_nodes/drawer_bridge/include/drawer_bridge/can_encoder_decoder.hpp
@@ -1,22 +1,23 @@
 #ifndef DRAWER_BRIDGE__CAN_ENCODER_DECODER_HPP_
 #define DRAWER_BRIDGE__CAN_ENCODER_DECODER_HPP_
 
-#include "can_msgs/msg/frame.hpp"
-#include "can/can_message.h"
-#include "can/can_helper.h"
 #include "can/can_db.hpp"
+#include "can/can_helper.h"
+#include "can/can_message.h"
+#include "can_msgs/msg/frame.hpp"
 
-class CanEncoderDecoder {
-public:
-     can_msgs::msg::Frame encode_msg(robast_can_msgs::CanMessage msg)const;
+namespace drawer_bridge
+{
+  class CanEncoderDecoder
+  {
+   public:
+    can_msgs::msg::Frame encode_msg(robast_can_msgs::CanMessage msg) const;
 
-     std::optional<robast_can_msgs::CanMessage> decode_msg(can_msgs::msg::Frame msg)const;
+    std::optional<robast_can_msgs::CanMessage> decode_msg(can_msgs::msg::Frame msg) const;
 
-private:
-    
-      robast_can_msgs::CanDb can_db = robast_can_msgs::CanDb();
-
-
-};
+   private:
+    robast_can_msgs::CanDb can_db = robast_can_msgs::CanDb();
+  };
+}   // namespace drawer_bridge
 
 #endif
--- src/ros2/hardware_nodes/drawer_bridge/src/can_encoder_decoder.cpp
+++ src/ros2/hardware_nodes/drawer_bridge/src/can_encoder_decoder.cpp
@@ -1,19 +1,18 @@
 #include "drawer_bridge/can_encoder_decoder.hpp"
 
-
-std::optional<robast_can_msgs::CanMessage> CanEncoderDecoder::decode_msg(can_msgs::msg::Frame msg) const
+std::optional<robast_can_msgs::CanMessage> drawer_bridge::CanEncoderDecoder::decode_msg(can_msgs::msg::Frame msg) const
 {
-    return robast_can_msgs::decode_can_message(msg.id, msg.data.begin(), msg.dlc, this->can_db.can_messages);
+  return robast_can_msgs::decode_can_message(msg.id, msg.data.begin(), msg.dlc, this->can_db.can_messages);
 }
 
-can_msgs::msg::Frame CanEncoderDecoder::encode_msg(robast_can_msgs::CanMessage msg) const
+can_msgs::msg::Frame drawer_bridge::CanEncoderDecoder::encode_msg(robast_can_msgs::CanMessage msg) const
 {
-    robast_can_msgs::CanFrame can_frame = encode_can_message_into_can_frame(msg, this->can_db.can_messages);
+  robast_can_msgs::CanFrame can_frame = encode_can_message_into_can_frame(msg, this->can_db.can_messages);
 
-    can_msgs::msg::Frame frame = can_msgs::msg::Frame();
-    frame.id = can_frame.get_id();
-    frame.dlc = can_frame.get_dlc();
-    memcpy((void*)frame.data.begin(), (void*)can_frame.get_data(), frame.dlc);
+  can_msgs::msg::Frame frame = can_msgs::msg::Frame();
+  frame.id = can_frame.get_id();
+  frame.dlc = can_frame.get_dlc();
+  memcpy((void*) frame.data.begin(), (void*) can_frame.get_data(), frame.dlc);
 
-    return frame;
+  return frame;
 }
