a2b342323394125ffed7f194a70cdc5db0c194b7
Andreas <a.neumann@robast.de>
2023-05-30 15:20:34 +0200

    Reformat motor.hpp

--- src/Hardware/cpp/Drawer_Controller/include/motor.hpp
+++ src/Hardware/cpp/Drawer_Controller/include/motor.hpp
@@ -2,41 +2,45 @@
 #define MOTOR
 
 #include <TMCStepper.h>
+
 #include "pinout_defines.h"
 
-#define SERIAL_PORT Serial2 // HardwareSerial port for Teensy 4.0
-#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
-#define R_SENSE 0.11f // Match to your driver
+#define SERIAL_PORT    Serial2   // HardwareSerial port for Teensy 4.0
+#define DRIVER_ADDRESS 0b00      // TMC2209 Driver address according to MS1 and MS2
+#define R_SENSE        0.11f     // Match to your driver
 
+enum Direction
+{
+  clockwise,
+  counter_clockwise
+};
 
-enum Direction{clockwise,counter_clockwise};
+class Motor
+{
+ private:
+  TMC2209Stepper* driver;
 
-class Motor{
-    private:
-        TMC2209Stepper* driver;
+  int32_t speed = 0;
 
-        uint8_t stepper_diag_pin_;
-        uint8_t stepper_enable_pin_;
-        int32_t speed = 0;
+  static bool isStalled;
+  bool isStallGuardEnabled = false;
+  Direction shaftDirection = clockwise;
 
-        static bool isStalled;
-        bool isStallGuardEnabled = false;
-        Direction shaftDirection = clockwise;
+  static void stallISR();
+  bool directionToShaftBool();
 
-        static void stallISR();
-        bool directionToShaftBool();
-    public:
-        void init(TMC2209Stepper*,uint8_t,uint8_t);
+ public:
+  Motor(TMC2209Stepper*);
+  void init();
+  void setSpeed(int speed, int accelarationTime);
+  void setDirection(Direction direction);
+  void setStallGuard(bool enable);
+  void resetStallGuard();
 
-        void setSpeed(int speed, int accelarationTime);
-        void setDirection(Direction direction);
-        void setStallGuard(bool enable);
-        void resetStallGuard();
-
-        int getSpeed();
-        bool getIsStalled();
-        Direction getDirection();
-        void printStatus();
+  int getSpeed();
+  bool getIsStalled();
+  Direction getDirection();
+  void printStatus();
 };
 
 #endif
