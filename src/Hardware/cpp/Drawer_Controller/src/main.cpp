5e8f3d796af5f9a57c84c6537c6e5197c854d435
Andreas <a.neumann@robast.de>
2023-05-30 15:21:24 +0200

    Adapt main.cpp for e_drawer prototype

--- src/Hardware/cpp/Drawer_Controller/src/main.cpp
+++ src/Hardware/cpp/Drawer_Controller/src/main.cpp
@@ -1,10 +1,15 @@
 #include "can.hpp"
-#include "led_strip.hpp"
 #include "drawer.hpp"
+#include "electrical_drawer.hpp"
+#include "led_strip.hpp"
 
 #define MODULE_ID 1
+#define R_SENSE   0.11f
 
-drawer_controller::Drawer drawer0 = drawer_controller::Drawer(MODULE_ID, 0);
+// drawer_controller::Drawer drawer0 = drawer_controller::Drawer(MODULE_ID, 0);
+drawer_controller::ElectricalDrawer e_drawer_0 =
+    drawer_controller::ElectricalDrawer(MODULE_ID, 0, ENCODER_A_PIN, ENCODER_B_PIN);
+TMC2209Stepper stepper_driver(&Serial2, R_SENSE, 0);
 std::vector<drawer_controller::IDrawer *> drawers = std::vector<drawer_controller::IDrawer *>();
 drawer_controller::Can CAN = drawer_controller::Can(MODULE_ID);
 
@@ -13,13 +18,16 @@ std::optional<robast_can_msgs::CanMessage> to_be_send_message;
 
 void setup()
 {
-  Serial.begin(115200); // Init serial port and set baudrate
+  Serial.begin(115200);   // Init serial port and set baudrate
   while (!Serial)
-    ; // Wait for serial port to connect
+    ;                     // Wait for serial port to connect
   Serial.println("\nStart...");
-  drawer0.init_lock(PWR_OPEN_LOCK1_PIN, PWR_CLOSE_LOCK1_PIN, SENSOR_LOCK1_PIN, SENSOR_DRAWER1_CLOSED_PIN);
-  drawers.push_back(&drawer0);
-  led_strip::initialize_led_strip();
+  // drawer0.init_lock(PWR_OPEN_LOCK1_PIN, PWR_CLOSE_LOCK1_PIN, SENSOR_LOCK1_PIN, SENSOR_DRAWER1_CLOSED_PIN);
+  // drawers.push_back(&drawer0);
+  // led_strip::initialize_led_strip();
+  e_drawer_0.init_lock(PWR_OPEN_LOCK1_PIN, PWR_CLOSE_LOCK1_PIN, SENSOR_LOCK1_PIN, SENSOR_DRAWER1_CLOSED_PIN);
+  e_drawer_0.init_motor(&stepper_driver);
+  drawers.push_back(&e_drawer_0);
   CAN.initialize_can_controller();
 }
 
@@ -33,24 +41,39 @@ void loop()
     {
       switch (received_message->get_id())
       {
-      case CAN_ID_DRAWER_UNLOCK || CAN_ID_ELECTRICAL_DRAWER_TASK:
-      {
-        uint8_t drawer_id = received_message->get_can_signals().at(CAN_SIGNAL_DRAWER_ID).get_data();
-        drawers.at(drawer_id)->can_in(received_message.value());
-      }
-      break;
-      case CAN_ID_DRAWER_LED:
-        led_strip::add_led_strip_mode_to_queue(*received_message);
-        led_strip::debug_prints_drawer_led(*received_message);
+        case CAN_ID_DRAWER_UNLOCK:
+        {
+          uint8_t drawer_id = received_message->get_can_signals().at(CAN_SIGNAL_DRAWER_ID).get_data();
+          drawers.at(drawer_id)->can_in(received_message.value());
+        }
         break;
-      default:
-        Serial.println("Received unsupported CAN message.");
+        case CAN_ID_ELECTRICAL_DRAWER_TASK:
+        {
+          uint8_t drawer_id = received_message->get_can_signals().at(CAN_SIGNAL_DRAWER_ID).get_data();
+          drawers.at(drawer_id)->can_in(received_message.value());
+        }
         break;
+        case CAN_ID_DRAWER_LED:
+          led_strip::add_led_strip_mode_to_queue(*received_message);
+          led_strip::debug_prints_drawer_led(*received_message);
+          break;
+        default:
+          Serial.println("Received unsupported CAN message.");
+          break;
       }
     }
   }
+  if (Serial.available())
+  {
+    char input = Serial.read();
+    if (input == '-')
+    {
+      Serial.println("stop motor");
+      e_drawer_0.stop_motor();
+    }
+  }
 
-  led_strip::handle_led_control();
+  // led_strip::handle_led_control();
 
   for (drawer_controller::IDrawer *drawer : drawers)
   {
