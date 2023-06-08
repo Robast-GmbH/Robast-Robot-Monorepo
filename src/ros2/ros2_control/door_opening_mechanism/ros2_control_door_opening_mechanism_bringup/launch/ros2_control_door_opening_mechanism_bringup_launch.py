668a7667907415759a7908b7c0ffa331509e2e99
TipluJacob <j.ritterbach@tiplu.de>
2023-06-08 09:09:36 +0200

    First draft for ros2_control_plugin that builds without errors for now

---
+++ src/ros2/ros2_control/door_opening_mechanism/ros2_control_door_opening_mechanism_bringup/launch/ros2_control_door_opening_mechanism_bringup_launch.py
@@ -0,0 +1,76 @@
+import os
+import xacro
+import yaml
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    with open("/workspace/src/navigation/environment_vars.yaml", "r") as stream:
+        try:
+            environment_yaml = yaml.safe_load(stream)
+            print(environment_yaml)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    # Get URDF via xacro
+    robot_xml = xacro.process_file(
+        os.path.join(
+            get_package_share_directory("rb_theron_description"),
+            "robots",
+            environment_yaml["robot"] + ".urdf.xacro",
+        ),
+        mappings={"prefix": environment_yaml["prefix"]},
+    ).toxml()
+
+    robot_description = {"robot_description": robot_xml}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("ros2_control_door_opening_mechanism_bringup"),
+            "config",
+            "door_mechanism_controllers.yaml",
+        ]
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_controllers],
+        output="both",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
+    )
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+
+    nodes = [
+        control_node,
+        joint_state_broadcaster_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+    ]
+
+    return LaunchDescription(nodes)
