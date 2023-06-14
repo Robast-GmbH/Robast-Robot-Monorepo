1ccb1142a4bdfbb2399c25ba1d8f92600adc1f2c
TipluJacob <j.ritterbach@tiplu.de>
2023-06-14 15:52:42 +0200

    Adding control commands for velocity

--- src/ros2/ros2_control/door_opening_mechanism/ros2_control_door_opening_mechanism_bringup/launch/ros2_control_door_opening_mechanism_bringup_launch.py
+++ src/ros2/ros2_control/door_opening_mechanism/ros2_control_door_opening_mechanism_bringup/launch/ros2_control_door_opening_mechanism_bringup_launch.py
@@ -66,7 +66,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
+        arguments=["forward_velocity_controller", "--controller-manager", "/controller_manager"],
         parameters=[{"use_sim_time": use_sim_time}],
     )
     # Delay start of robot_controller after `joint_state_broadcaster`
--- src/ros2/ros2_control/door_opening_mechanism/ros2_control_plugin_door_opening_mechanism/src/door_opening_mechansim.cpp
+++ src/ros2/ros2_control/door_opening_mechanism/ros2_control_plugin_door_opening_mechanism/src/door_opening_mechansim.cpp
@@ -243,14 +243,22 @@ namespace ros2_control_plugin_door_opening_mechanism
     // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
     RCLCPP_INFO(rclcpp::get_logger("DoorOpeningMechanismSystemPositionOnlyHardware"), "Writing...");
 
-    _x_axis.set_profile_velocity(_hw_position_commands[0], _X_AXIS_ACCELERATION, _X_AXIS_DECELERATION);
+    _x_axis.set_profile_velocity(_hw_velocity_commands[0], _X_AXIS_ACCELERATION, _X_AXIS_DECELERATION);
 
     for (uint i = 0; i < _hw_position_commands.size(); i++)
     {
       // Simulate sending commands to the hardware
       RCLCPP_INFO(rclcpp::get_logger("DoorOpeningMechanismSystemPositionOnlyHardware"),
-                  "Got command %.5f for joint %d!",
+                  "Got position command %.5f for joint %d!",
                   _hw_position_commands[i],
+                  i);
+    }
+    for (uint i = 0; i < _hw_velocity_commands.size(); i++)
+    {
+      // Simulate sending commands to the hardware
+      RCLCPP_INFO(rclcpp::get_logger("DoorOpeningMechanismSystemPositionOnlyHardware"),
+                  "Got velocity command %.5f for joint %d!",
+                  _hw_velocity_commands[i],
                   i);
     }
     RCLCPP_INFO(rclcpp::get_logger("DoorOpeningMechanismSystemPositionOnlyHardware"), "Joints successfully written!");
