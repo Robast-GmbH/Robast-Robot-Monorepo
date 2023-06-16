b0b28fb74c7c4296055876b4329c8d9baa467a00
Tobi <t.alscher@robast.de>
2023-06-16 09:18:29 +0200

    e-drawer bt launchable (in theory)

--- src/ros2/statemachine/bt_base_nodes/src/bt_sub_initiator/electrical_drawer_tree_initiator.cpp
+++ src/ros2/statemachine/bt_base_nodes/src/bt_sub_initiator/electrical_drawer_tree_initiator.cpp
@@ -1,10 +1,10 @@
-#include "bt_base_nodes/bt_sub_initiator/drawer_tree_initiator.hpp"
+#include "bt_base_nodes/bt_sub_initiator/electrical_drawer_tree_initiator.hpp"
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<bt_base_nodes::DrawerTreeInitiator>(
-        "trigger_electric_drawer_tree");
+    auto node = std::make_shared<bt_base_nodes::ElectricalDrawerTreeInitiator>();
+    node->configure("trigger_electric_drawer_tree");
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
