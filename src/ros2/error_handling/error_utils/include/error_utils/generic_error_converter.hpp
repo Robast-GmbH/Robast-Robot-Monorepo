75c4b50837b485655185ea4311f0e1cce8cc021b
Tobi <t.alscher@robast.de>
2023-07-04 11:11:28 +0200

    error converter fix

--- src/ros2/error_handling/error_utils/include/error_utils/generic_error_converter.hpp
+++ src/ros2/error_handling/error_utils/include/error_utils/generic_error_converter.hpp
@@ -9,30 +9,20 @@ template <typename MsgT>
 class MessageConverter
 {
 public:
-  MessageConverter()
-  {
-  }
+  MessageConverter() {}
 
   std::string messageToString(const MsgT &msg)
   {
-    auto serialized_msg = std::make_shared<MsgT>();
-    serialized_msg->serialize(msg);
-
-    std::string serialized_str(reinterpret_cast<const char *>(serialized_msg->buffer.data()),
-                               serialized_msg->buffer.size());
-
+    std::shared_ptr<MsgT> serialized_msg = std::make_shared<MsgT>(msg);
+    std::string serialized_str(reinterpret_cast<const char *>(serialized_msg.get()), sizeof(MsgT));
+    serialized_msg.reset();  // Freigabe des std::shared_ptr-Objekts
     return serialized_str;
   }
 
   MsgT stringToMessage(const std::string &serialized_str)
   {
-    auto serialized_msg = std::make_shared<MsgT>();
-    serialized_msg->buffer.resize(serialized_str.size());
-    std::memcpy(serialized_msg->buffer.data(), serialized_str.data(), serialized_str.size());
-
     MsgT deserialized_msg;
-    deserialized_msg.deserialize(*serialized_msg);
-
+    memcpy(&deserialized_msg, serialized_str.data(), sizeof(MsgT));
     return deserialized_msg;
   }
 };
