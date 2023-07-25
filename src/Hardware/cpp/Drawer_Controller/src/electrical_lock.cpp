a8508469d16cfdd8e16a0ce989fab900d5fb8869
TipluJacob <j.ritterbach@tiplu.de>
2023-07-25 15:35:32 +0200

    fix: close_lock_on_setup() needs the lock to be opened before it can be closed. Explanation is added as well.

--- src/Hardware/cpp/Drawer_Controller/src/electrical_lock.cpp
+++ src/Hardware/cpp/Drawer_Controller/src/electrical_lock.cpp
@@ -21,8 +21,7 @@ namespace drawer_controller
     _gpio_wrapper->set_pin_mode(_sensor_lock_pin_id, _gpio_wrapper->get_gpio_input_pin_mode());
     _gpio_wrapper->set_pin_mode(_sensor_drawer_closed_pin, _gpio_wrapper->get_gpio_input_pin_mode());
 
-    _gpio_wrapper->digital_write(_power_open_pin_id, LOW);
-    _gpio_wrapper->digital_write(_power_close_pin_id, LOW);
+    set_lock_output_low();
 
     _open_lock_previous_step = false;
     set_open_lock_current_step(false);
@@ -34,6 +33,20 @@ namespace drawer_controller
 
   void ElectricalLock::close_lock_on_setup()
   {
+    // delay by a random time up to 2 seconds to better distribute the power consumption of the lock setup process
+    delay(random(1, 2000));
+    // Please note:
+    // When turning on the power for the drawer controller pcbs (v3.0), the port expander will have high outputs as soon
+    // as the port expander has power. This unfortunately results in the lock being opened a very tiny bit each time the
+    // power turns on.
+    // This could only be solved by a hardware fix by exchaning the port expander or adding a enable mechanism for the
+    // port expander.
+    // Additionally the electrical lock can only be closed, once it is completely open.
+    // Therefore on startup we need to first open the lock and only then we can close it again.
+    // TODO@Jacob: Fix that with the new hardware version of the drawer controller pcb
+    open_lock();
+    delay(ELECTRICAL_LOCK_MECHANISM_TIME);
+    set_lock_output_low();
     close_lock();
     delay(ELECTRICAL_LOCK_MECHANISM_TIME);
     set_lock_output_low();
