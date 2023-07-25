36253684e5d42c97f10649d41e9912e25e8ebe2a
Torben Zurhelle <t.zurhelle@robast.de>
2023-07-25 15:58:56 +0200

    update to the submodule of the main

--- src/ros2/simulation/tiplu_world/launch/tiplu_world_launch.py
+++ src/ros2/simulation/tiplu_world/launch/tiplu_world_launch.py
@@ -82,7 +82,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py"),
         ),
-        launch_arguments={"gz_args": ["-r ", headless, " ", world_model, " --render-engine ogre2"],
+        launch_arguments={"gz_args": ["-r ", headless, " ", world_model], # , " --render-engine ogre2"],
                           "gz_version": "7",                          
                           }.items(),
     )
