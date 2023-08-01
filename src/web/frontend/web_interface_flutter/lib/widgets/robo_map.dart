aa0d39e72a036a2cb1e88d8550c9a25f129e5b9a
Andreas <a.neumann@robast.de>
2023-08-01 16:45:04 +0200

    add hint text for long press

--- src/web/frontend/web_interface_flutter/lib/widgets/robo_map.dart
+++ src/web/frontend/web_interface_flutter/lib/widgets/robo_map.dart
@@ -15,60 +15,71 @@ class _RoboMapState extends State<RoboMap> {
 
   @override
   Widget build(BuildContext context) {
-    return Zoom(
-      maxScale: 5,
-      initTotalZoomOut: true,
-      child: SizedBox(
-        width: 827,
-        height: 355,
-        child: GestureDetector(
-          onTapDown: (details) {
-            tempPos = details.localPosition;
-          },
-          onLongPress: () {
-            widget.controller.position = tempPos;
-            setState(() {});
-          },
-          child: Stack(
-            children: [
-              Image.asset("assets/RL_Tiplu_6.png"),
-              Positioned(
-                left: robotPos.dx - 8,
-                top: robotPos.dy - 8,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.all(Radius.circular(4)),
-                    color: Theme.of(context).colorScheme.inversePrimary,
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.all(0),
-                    child: Icon(
-                      Icons.arrow_forward,
-                      size: 16,
+    return Stack(
+      alignment: Alignment.topCenter,
+      children: [
+        Zoom(
+          maxScale: 5,
+          initTotalZoomOut: true,
+          child: SizedBox(
+            width: 827,
+            height: 355,
+            child: GestureDetector(
+              onTapDown: (details) {
+                tempPos = details.localPosition;
+              },
+              onLongPress: () {
+                widget.controller.position = tempPos;
+                setState(() {});
+              },
+              child: Stack(
+                children: [
+                  Image.asset("assets/RL_Tiplu_6.png"),
+                  Positioned(
+                    left: robotPos.dx - 8,
+                    top: robotPos.dy - 8,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.all(Radius.circular(4)),
+                        color: Theme.of(context).colorScheme.inversePrimary,
+                      ),
+                      child: const Padding(
+                        padding: EdgeInsets.all(0),
+                        child: Icon(
+                          Icons.arrow_forward,
+                          size: 16,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  if (widget.controller.position != null) ...[
+                    Positioned(
+                      left: widget.controller.position!.dx - 8,
+                      top: widget.controller.position!.dy - 8,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(4)),
+                          color: Colors.orange,
+                        ),
+                        child: const Icon(
+                          Icons.clear,
+                          size: 16,
+                        ),
+                      ),
+                    ),
+                  ],
+                ],
               ),
-              if (widget.controller.position != null) ...[
-                Positioned(
-                  left: widget.controller.position!.dx - 8,
-                  top: widget.controller.position!.dy - 8,
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(4)),
-                      color: Colors.orange,
-                    ),
-                    child: const Icon(
-                      Icons.clear,
-                      size: 16,
-                    ),
-                  ),
-                ),
-              ]
-            ],
+            ),
           ),
         ),
-      ),
+        if (widget.controller.position == null) ...[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: const Text("Zur Zielauswahl lange drücken"),
+          )
+        ]
+      ],
     );
   }
 }
