5d76c58a32b678cfda5192bfbfe04d38858f2167
Andreas <a.neumann@robast.de>
2023-08-02 15:35:33 +0200

    move robot clone to the right place

--- src/web/frontend/web_interface_flutter/lib/pages/admin_page.dart
+++ src/web/frontend/web_interface_flutter/lib/pages/admin_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:web_interface_flutter/widgets/docker_overview.dart';
+import 'package:web_interface_flutter/widgets/robot_clone.dart';
 import 'package:web_interface_flutter/widgets/rosbag_overview.dart';
 import 'package:web_interface_flutter/widgets/status_overview.dart';
 import 'package:web_interface_flutter/widgets/tasks_overview.dart';
@@ -61,7 +62,7 @@ class _AdminPageState extends State<AdminPage> {
           ),
           Expanded(
             flex: 5,
-            child: menuWidget[selectedIndex],
+            child: Center(child: menuWidget[selectedIndex]),
           ),
         ],
       ),
--- src/web/frontend/web_interface_flutter/lib/pages/manual_control_page.dart
+++ src/web/frontend/web_interface_flutter/lib/pages/manual_control_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import 'package:web_interface_flutter/models/robot_provider.dart';
 import 'package:web_interface_flutter/widgets/drawer_tile.dart';
+import 'package:web_interface_flutter/widgets/robot_clone.dart';
 
 class ManualControlPage extends StatefulWidget {
   const ManualControlPage({super.key});
@@ -18,65 +19,10 @@ class _ManualControlPageState extends State<ManualControlPage> {
     final robotProvider = Provider.of<RobotProvider>(context, listen: false);
     final robots = robotProvider.robots;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("Manuelle Kontrolle"),
-      ),
-      body: robotProvider.robots.isEmpty
-          ? const Center(
-              child: Text("Aktuell sind keine Roboter verfügbar."),
-            )
-          : Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: const BoxDecoration(color: Colors.white, boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey, // Shadow color
-                        offset: Offset(0, 2), // Changes position of shadow
-                        blurRadius: 2, // Changes size of shadow
-                        spreadRadius: 1, // Expands the shadow
-                      ),
-                    ]),
-                    child: ListView(
-                        children: ListTile.divideTiles(
-                      context: context,
-                      tiles: List.generate(
-                        robotProvider.robots.length,
-                        (index) => ListTile(
-                          selected: selectedRobotIndex == index,
-                          title: Text(robotProvider.robots[index].name),
-                          onTap: () {
-                            selectedRobotIndex = index;
-                            setState(() {});
-                          },
-                        ),
-                      ),
-                    ).toList()),
-                  ),
-                ),
-                Expanded(
-                  flex: 5,
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: robotProvider.modules[robots[selectedRobotIndex]]
-                                  ?.map((e) => DrawerTile(
-                                        moduleID: e.moduleID,
-                                        drawerID: e.drawerID,
-                                        robotName: robots[selectedRobotIndex].name,
-                                      ))
-                                  .toList() ??
-                              [],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("Manuelle Kontrolle"),
+        ),
+        body: Center(child: const RobotClone()));
   }
 }
