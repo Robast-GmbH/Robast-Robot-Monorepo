c6471bf9bf1b99a9550bb14d1da1cbaa50f4b7b5
Tobi <t.alscher@robast.de>
2023-08-04 14:24:15 +0200

    nav+statemachine start automatically

--- bringups/Robo_A/start_scripts/nav_start.sh
+++ bringups/Robo_A/start_scripts/nav_start.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 . /robast/humble/setup.sh
-ros2 launch nav_bringup robot_localization_odom_to_base_launch.py
-. /robast/humble/setup.sh
+ros2 launch nav_bringup robot_localization_odom_to_base_launch.py &
 ros2 launch nav_bringup slam_toolbox_launch.py &
-. /robast/humble/setup.sh
 ros2 launch nav_bringup nav_without_localization_launch.py &
-. /robast/humble/setup.sh
-ros2 launch nav_bringup mask_server_launch.py &
+ros2 launch nav_bringup mask_server_launch.py 
--- bringups/Robo_A/start_scripts/statemachine_start.sh
+++ bringups/Robo_A/start_scripts/statemachine_start.sh
@@ -2,5 +2,4 @@
 
 . /robast/humble/setup.sh
 ros2 launch drawer_sm electrical_drawer_launch.py &
-. /robast/humble/setup.sh
-ros2 launch drawer_sm drawer_statemachine_launch.py &
+ros2 launch drawer_sm drawer_statemachine_launch.py
