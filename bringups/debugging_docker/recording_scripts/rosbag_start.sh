1947a9e804d790e9dfbd2fb664b23e1b89b5a628
Tobi <t.alscher@robast.de>
2023-08-05 14:33:17 +0200

    added a debugging docker build as a manual triggered workflow

---
+++ bringups/debugging_docker/recording_scripts/rosbag_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /opt/ros/humble/setup.bash
+ros2 bag record -o /workspace/bags/robot_imu_2.db3 \
+    tf \
+    tf_static \
+    robot/robotnik_base_control/odom \
+    robot/imu/data \
+    robot/vectornav/imu/data\
+    robot/pad_teleop/cmd_vel \
+    robot/vectornav/odom \
+    clock
