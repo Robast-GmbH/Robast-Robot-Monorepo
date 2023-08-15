a09b57799a4ecb56455f0075484448deeeddfcf7
Tobi <t.alscher@robast.de>
2023-08-15 11:13:49 +0200

    no need to reconfigure statemachine

--- bringups/Robo_A/start_scripts/statemachine_start.sh
+++ bringups/Robo_A/start_scripts/statemachine_start.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 . /robast/humble/setup.sh
-cd /workspace
-colcon build --symlink-install
-. install/setup.sh
 ros2 launch drawer_sm electrical_drawer_launch.py &
 ros2 launch drawer_sm drawer_statemachine_launch.py
