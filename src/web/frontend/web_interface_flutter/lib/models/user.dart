cc1617ac70659e082d9653eb7569cc5d82714b5e
Andreas <a.neumann@robast.de>
2023-08-16 16:08:42 +0200

    remove tasks list from user class

--- src/web/frontend/web_interface_flutter/lib/models/user.dart
+++ src/web/frontend/web_interface_flutter/lib/models/user.dart
@@ -1,15 +1,11 @@
-import 'package:web_interface_flutter/models/task.dart';
-
 class User {
   final int id;
   final String name;
   final String fullName;
   final bool isActive;
   final bool isAdmin;
-  final List<Task> tasks;
 
   User({
-    required this.tasks,
     required this.id,
     required this.name,
     required this.fullName,
@@ -19,11 +15,11 @@ class User {
 
   static User fromJson(Map<String, dynamic> data) {
     return User(
-        fullName: data["full_name"],
-        name: data["name"],
-        id: data["id"],
-        isActive: data["is_active"],
-        isAdmin: data["admin"],
-        tasks: (data["orders"] as List<dynamic>).map((e) => Task.fromJson(e)).toList());
+      fullName: data["full_name"],
+      name: data["name"],
+      id: data["id"],
+      isActive: data["is_active"],
+      isAdmin: data["admin"],
+    );
   }
 }
