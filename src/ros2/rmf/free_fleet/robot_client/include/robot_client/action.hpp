9e56ee3a1cafda89c7abf72701e288d7b8da90fe
Torben Zurhelle <t.zurhelle@robast.de>
2023-09-06 13:12:36 +0200

    feat: extend action to continue to the next action through calling a callback funktion

--- src/ros2/rmf/free_fleet/robot_client/include/robot_client/action.hpp
+++ src/ros2/rmf/free_fleet/robot_client/include/robot_client/action.hpp
@@ -5,24 +5,28 @@
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "fleet_interfaces/msg/free_fleet_data_task_state.hpp"
+#include <functional>
 
 namespace rmf_robot_client
 {
   class Action
-  {private:
+  {
+    protected: 
       using FreeFleetDataTaskInfo = fleet_interfaces::msg::FreeFleetDataTaskState;
       int task_id;
       int step;
       std::shared_ptr<rclcpp::Node> ros_node;
       std::map<std::string, std::string> config;
-      //task_info_publisher
+      rclcpp::Publisher<FreeFleetDataTaskInfo>::SharedPtr task_info_publisher;
+      std::function<void(bool)> finish_action;
 
     public:
       Action(int task_id, int step, std::shared_ptr<rclcpp::Node> ros_node, std::map<std::string,std::string> config);
     //  ~Action();
-      virtual bool start()=0;
+      virtual bool start(std::function<void(bool)> next_action_callback)=0;
       virtual bool cancel()=0;
       virtual bool receive_new_settings(std::string command, std::string value)=0;
+      virtual std::string get_type() = 0;
       void publish_task_state(std::string status, std::string message, bool completed);
 
       int get_step();
