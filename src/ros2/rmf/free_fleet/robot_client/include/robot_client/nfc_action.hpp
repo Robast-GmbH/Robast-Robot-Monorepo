711f9efa9bf632e1756b8d0f3f5806e981aaa6b6
Torben Zurhelle <t.zurhelle@robast.de>
2023-09-07 08:14:38 +0200

    feat:NFC anpassungen

--- src/ros2/rmf/free_fleet/robot_client/include/robot_client/nfc_action.hpp
+++ src/ros2/rmf/free_fleet/robot_client/include/robot_client/nfc_action.hpp
@@ -2,6 +2,8 @@
 #define RMF__ROBOT_CLIENT__NFC_ACTION_HPP_
 
 #include "action.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "communication_interfaces/action/create_user_nfc_tag.hpp"
 
 namespace rmf_robot_client
 {
@@ -9,15 +11,24 @@ namespace rmf_robot_client
   {
     private:
       int user_id;
+      using CreateUserNfcTag = communication_interfaces::action::CreateUserNfcTag;
+      using GoalHandleCreateUserNfcTag = rclcpp_action::ClientGoalHandle<CreateUserNfcTag>;
+      rclcpp_action::Client<CreateUserNfcTag>::SharedPtr nfc_write_new_nfc_card_client_;
+      GoalHandleCreateUserNfcTag::SharedPtr current_action_goal_handle;
 
     public:
-      NFCAction(int task_id, int step, std::shared_ptr<rclcpp::Node> ros_node, std::map<std::string,std::string> config, int user_id);
+      NFCAction(int task_id, int step, std::shared_ptr<rclcpp::Node> ros_node, std::map<std::string, std::string> config, int user_id);
       //~NFCAction();
 
       bool start(std::function<void(bool)> next_action_callback);
       bool cancel();
       std::string get_type();
       bool receive_new_settings(std::string command, std::string value);
+      void feedback_callback(GoalHandleCreateUserNfcTag::SharedPtr, const std::shared_ptr<const CreateUserNfcTag::Feedback> feedback);
+      void result_callback(const GoalHandleCreateUserNfcTag::WrappedResult &result);
+
+      void start_writing_procedure();
+      void goal_response_callback(std::shared_future<GoalHandleCreateUserNfcTag::SharedPtr> future);
   };
 }// namespace robot_client
 #endif   // RMF__ROBOT_CLIENT__NFC_ACTION_HPP_
