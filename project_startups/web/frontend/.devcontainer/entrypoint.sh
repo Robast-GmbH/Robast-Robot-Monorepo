4b83be841b7ddfa986d45804a751060be303168d
Torben Zurhelle <t.zurhelle@robast.de>
2023-09-11 12:10:20 +0200

    clean remove the old frontend

--- project_startups/web/frontend/.devcontainer/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-npm i  openapi-typescript openapi-typescript-codegen -D
-sudo apt-get update
-sudo apt-get install iputils-ping
-#sudo /startup.sh
-sudo apt update
-sudo apt install iputils-ping
