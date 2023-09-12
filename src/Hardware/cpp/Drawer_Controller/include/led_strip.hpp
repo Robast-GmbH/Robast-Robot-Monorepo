f1bdab7b40930822f264ac0d89fec1465a37fc3a
TipluJacob <j.ritterbach@tiplu.de>
2023-09-12 17:21:54 +0200

    fix: adds deep copy assignment operator for LedAnimation

--- src/Hardware/cpp/Drawer_Controller/include/led_strip.hpp
+++ src/Hardware/cpp/Drawer_Controller/include/led_strip.hpp
@@ -75,6 +75,30 @@ namespace drawer_controller
           start_index_led_states(start_index_led_states_input)
     {
     }
+
+    // Deep copy assignment operator
+    LedAnimation& operator=(const LedAnimation& other)
+    {
+      if (this == &other)
+      {
+        return *this;   // Handle self-assignment
+      }
+
+      // Copy all the non-vector members
+      fade_time_in_hundreds_of_ms = other.fade_time_in_hundreds_of_ms;
+      num_of_led_states_to_change = other.num_of_led_states_to_change;
+      start_index_led_states = other.start_index_led_states;
+
+      // Deep copy the vector member 'target_led_states'
+      target_led_states.clear();   // Clear existing data
+      target_led_states.reserve(other.target_led_states.size());
+      for (const LedState& state : other.target_led_states)
+      {
+        target_led_states.push_back(state);   // You can create a deep copy of LedState if needed
+      }
+
+      return *this;
+    }
   };
 
   class LedStrip
