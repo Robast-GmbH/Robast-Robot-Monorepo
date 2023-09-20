521bbe935716adfc28575998ab3872483a87793d
Torben Zurhelle <t.zurhelle@robast.de>
2023-09-20 07:55:07 +0200

    feat: error handling in ffserver backend not running

--- src/ros2/rmf/free_fleet/api_connector/api_connector/rest_interface.py
+++ src/ros2/rmf/free_fleet/api_connector/api_connector/rest_interface.py
@@ -16,6 +16,7 @@ class RestInterface():
         self.app = app
         self.config= ros_node.get_node_config()
         self.response_api=self.config["backend_address"]
+        self.is_backend_running=False
         
         @self.app.post("/test")
         def test():
@@ -25,51 +26,51 @@ class RestInterface():
         def do_task( task:schemas.Task):
             for action in task.actions:
                 if action.type== schemas.ActionType.NAVIGATION:
-                    self.ros_node.handle_destination_request( task.robot.fleet_name, task.robot.robot_name, str(task.task_id)+"#"+str(action.step), action.action.pose.x, action.action.pose.y,action.action.yaw)
+                    self.ros_node.handle_destination_request( task.robot.fleet_name, task.robot.robot_name, str(task.task_id),str(action.step), action.action.pose.x, action.action.pose.y,action.action.yaw)
                 elif action.type== schemas.ActionType.OPEN_DRAWER:
-                    self.ros_node.handle_slide_drawer_request( task.robot.fleet_name, task.robot.robot_name, str(task.task_id)+"#"+str(action.step), action.action.module_id, action.action.drawer_id, action.action.locked_for, action.action.is_edrawer,True)
+                    self.ros_node.handle_slide_drawer_request( task.robot.fleet_name, task.robot.robot_name, str(task.task_id), str(action.step), action.action.module_id, action.action.drawer_id, action.action.locked_for, action.action.is_edrawer,True)
                 elif action.type== schemas.ActionType.NEW_USER:
-                    self.ros_node.handle_new_user_request( task.robot.fleet_name, task.robot.robot_name, str(task.task_id)+"#"+str(action.step), action.action.user_id)
+                    self.ros_node.handle_new_user_request( task.robot.fleet_name, task.robot.robot_name, str(task.task_id), str(action.step), action.action.user_id)
             
         @self.app.post("/settings/navigation/pause")
         def pause_robot(robot:schemas.Robot):
-            self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name,"move", "pause")
+            self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name,"move", {"Pause"})
             return
         
         @self.app.post("/settings/navigation/resume")
         def resume_robot(robot:schemas.Robot):
-            self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name,"move", "resume")
+            self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name,"move", {"Resume"})
             return
 
         @self.app.post("/settings/navigation/cancel")
         def cancel_robot(robot:schemas.Robot ):
-            self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name,"move", "cancel")
+            self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name,"move", {"Cancel"})
             return  
         
         @self.app.post("/settings/navigation/loop")
         def loop_robot(robot:schemas.Robot, loop:Annotated[bool,Body()] ):
             if loop:
-                self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name,"loop", "start")
+                self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name,"loop", {"Start"})
             else:
-                self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name,"loop", "stop")
+                self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name,"loop", {"Stop"})
             return  
         
         
         @self.app.post("/settings/drawer/close")
         def close_drawer(robot:schemas.Robot, drawer_id:Annotated[int,Body()], module_id:Annotated[int,Body()], e_drawer:Annotated[bool,Body()]):
             if e_drawer:
-                self.ros_node.handle_setting_request( robot.robot_name, robot.fleet_name,"drawer","Closed#"+str(module_id)+"#"+str(drawer_id))
+                self.ros_node.handle_setting_request( robot.robot_name, robot.fleet_name,"drawer",{"Closed", str(module_id), str(drawer_id)})
             else:
                     raise HTTPException(status_code=423, detail="drawer has to be closed manually")
             
         @self.app.post("/settings/drawer/reset")
         def reset_drawer(robot:schemas.Robot):
-                self.ros_node.handle_setting_request( robot.robot_name, robot.fleet_name, "reset_tree", "e-drawer" )
+                self.ros_node.handle_setting_request( robot.robot_name, robot.fleet_name, "reset_tree", {"E-drawer"} )
 
         
         @self.app.post("/settings/drawer/completed")
         def end_drawer_action(robot:schemas.Robot):
-                self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name, "drawer", "completed")
+                self.ros_node.handle_setting_request(robot.robot_name, robot.fleet_name, "drawer", {"Completed"})
 
 
     def run(self, host='0.0.0.0', log_level='warning'):
@@ -89,9 +90,14 @@ class RestInterface():
         sender = requests.Session()
         try:
             sender.post(url= self.response_api+"/robots/status", data= json.dumps(message), verify=False)
+            if(self.is_backend_running==False):
+                self.ros_node.get_logger().info('Backend is operational')
+                self.is_backend_running=True
+
         except ConnectionError:
-            self.ros_node.get_logger().error('Backend is not responding')
-            
+            if(self.is_backend_running==True):
+                self.ros_node.get_logger().error('Backend is not responding')
+                self.is_backend_running=False
 
  
     def handle_drawer_status_change(self, task_id, module_id, drawer_id, status):
