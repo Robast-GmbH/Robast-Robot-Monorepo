0f8913e742f9bb7f0b486237769d642676efdf15
Tobi <t.alscher@robast.de>
2023-09-22 13:50:56 +0200

    is charging check added to base bt

--- src/ros2/statemachine/bt_plugins/src/condition/bool_topic_condition.cpp
+++ src/ros2/statemachine/bt_plugins/src/condition/bool_topic_condition.cpp
@@ -41,9 +41,9 @@ namespace statemachine
         }
     }
 
-    void BoolTopicCondition::callbackDrawerFeedback(const std_msgs::msg::Bool::SharedPtr msg)
+    void BoolTopicCondition::callbackTopicFeedback(const std_msgs::msg::Bool::SharedPtr msg)
     {
-        RCLCPP_DEBUG(rclcpp::get_logger("BoolTopicCondition"), "callback got called ");
+        RCLCPP_INFO(rclcpp::get_logger("BoolTopicCondition"), "callback got called ");
         last_message_ = *msg;
         _compare_result = comparator(last_message_, target_value_);
         new_value_received_ = true;
