7288444599a7ca815115d600dba4981e07f5a0ea
sagarvl96 <s.lingaraj@robast.de>
2023-09-28 15:03:51 +0200

    removes install python package from entrypoint and adds ´requirements.txt´

--- project_startups/simulation/navigation/.devcontainer/entrypoint.sh
+++ project_startups/simulation/navigation/.devcontainer/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
-pip install yolov5 # TODO remove before merging to main
 rosdep update
 rosdep install --from-paths /workspace/src --ignore-src -r -y
 
