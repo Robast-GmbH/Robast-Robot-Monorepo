aef6256cfa09ced429a5190265f1a43d58c74091
TipluJacob <j.ritterbach@tiplu.de>
2023-09-28 11:13:00 +0200

    fix: package namings in test_case_launch

--- src/ros2/integration_tests_pkg/src/drawer_sys_test/test_case_launch.py
+++ src/ros2/integration_tests_pkg/src/drawer_sys_test/test_case_launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
     statemachine_bringup_dir = get_package_share_directory('drawer_sm')
     sim_bringup_dir = get_package_share_directory('tiplu_world')
     drawer_sim_dir = get_package_share_directory('drawer_bridge_simulation')
-    contoller_manager_sim_dir = get_package_share_directory('gazebo_controller_manager')
+    gazebo_trajectory_executor_dir = get_package_share_directory('gazebo_trajectory_executor')
     drawer_moveit_sim_dir = get_package_share_directory('moveit_drawer_config')
     
     sim_arguments = {
@@ -26,7 +26,7 @@ def generate_launch_description():
         "world_model": os.path.join(get_package_share_directory("tiplu_world"), "worlds", "6OG" + ".sdf")
     }.items()
     
-    contoller_manager_sim_arguments = {
+    gazebo_trajectory_executor_arguments = {
         "follow_joint_trajectory_action": "/drawer_planning_group_controller/follow_joint_trajectory",
         "joint_names_list": "['drawer_1_joint','drawer_2_joint','drawer_3_joint','drawer_4_joint','drawer_5_joint']"
     }.items()
@@ -48,8 +48,8 @@ def generate_launch_description():
     
     contoller_manager_sim_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(contoller_manager_sim_dir, 'launch', 'gazebo_controller_manager_launch.py')), 
-                                      launch_arguments=contoller_manager_sim_arguments)
+            os.path.join(gazebo_trajectory_executor_dir, 'launch', 'gazebo_trajectory_executor_launch.py')), 
+                                      launch_arguments=gazebo_trajectory_executor_arguments)
     
     drawer_moveit_sim_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
