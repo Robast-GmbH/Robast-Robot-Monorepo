fb786ff519222a331be609a31079a61d7f05064a
TipluJacob <j.ritterbach@tiplu.de>
2023-10-02 08:54:15 +0200

    chore: testing to reduce can data rate

--- bringups/hardware_nodes/setup_jetson_can.sh
+++ bringups/hardware_nodes/setup_jetson_can.sh
@@ -9,6 +9,6 @@ sudo modprobe can-raw
 sudo modprobe mttcan
 # Bring up the CAN network interface can0, setting the bitrate to 500,000 bps,
 # data bitrate to 1,000,000 bps, enabling error reporting (berr-reporting), and enabling flexible data-rate (fd)
-sudo ip link set can0 up type can bitrate 500000 dbitrate 1000000 berr-reporting on fd on
+sudo ip link set can0 up type can bitrate 50000 dbitrate 100000 berr-reporting on fd on
 # Set the transmit queue length for can0 interface to 1000
 sudo ifconfig can0 txqueuelen 1000
