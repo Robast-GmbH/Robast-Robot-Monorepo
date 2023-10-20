171f6ff6402bcf9d7d7b05f33be0e9965942d73d
Torben Zurhelle <t.zurhelle@robast.de>
2023-10-20 17:43:06 +0200

    fix: for pull_request: "dev/RE-1706 #65"

--- src/ros2/hardware_nodes/nfc_bridge/launch/nfc_bridge_robot_launch.py
+++ src/ros2/hardware_nodes/nfc_bridge/launch/nfc_bridge_robot_launch.py
@@ -6,7 +6,7 @@ def generate_launch_description():
     # Parameters
 
     # Nodes launching commands
-    start_nfc_bridge_cmd = launch_ros.actions.Node(
+    start_nfc_bridge_cmd1 = launch_ros.actions.Node(
         package='nfc_bridge',
         executable='nfc_bridge',
         output='screen',
@@ -15,7 +15,7 @@ def generate_launch_description():
         emulate_tty=True,  # https://github.com/ros2/launch/issues/188
     )
 
-    start_nfc_bridge_cmd = launch_ros.actions.Node(
+    start_nfc_bridge_cmd2 = launch_ros.actions.Node(
         package='nfc_bridge',
         executable='nfc_bridge',
         output='screen',
@@ -24,5 +24,6 @@ def generate_launch_description():
         emulate_tty=True,  # https://github.com/ros2/launch/issues/188
     )
     ld = LaunchDescription()
-    ld.add_action(start_nfc_bridge_cmd)
+    ld.add_action(start_nfc_bridge_cmd1)
+    ld.add_action(start_nfc_bridge_cmd2)
     return ld
