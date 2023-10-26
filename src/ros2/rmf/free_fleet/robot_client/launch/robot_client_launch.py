5351e5c96c76627eda4472646414c953f5d1b857
Torben Zurhelle <t.zurhelle@robast.de>
2023-10-26 12:36:27 +0200

    linting fixes

--- src/ros2/rmf/free_fleet/robot_client/launch/robot_client_launch.py
+++ src/ros2/rmf/free_fleet/robot_client/launch/robot_client_launch.py
@@ -3,9 +3,9 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
-    ld= LaunchDescription()
 
+def generate_launch_description():
+    ld = LaunchDescription()
 
     config = os.path.join(
         get_package_share_directory('robot_client'),
@@ -13,11 +13,11 @@ def generate_launch_description():
         'params.yaml'
         )
 
-    robot_client= Node(
+    robot_client = Node(
                     package='robot_client',
                     executable='robot_client',
                     name='robot_client',
-                    parameters = [config]
+                    parameters=[config]
                     )
     ld.add_action(robot_client)
     return ld
