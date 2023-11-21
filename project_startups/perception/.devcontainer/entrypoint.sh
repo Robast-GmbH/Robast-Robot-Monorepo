7c89f739a8357a9bfb77c2b6ab43f85e0cf9d194
sagarvl96 <s.lingaraj@robast.de>
2023-11-21 13:47:18 +0100

    added the python installation path to path variable

--- project_startups/perception/.devcontainer/entrypoint.sh
+++ project_startups/perception/.devcontainer/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo 'export PATH=$PATH:/home/robast/.local/bin' >> /home/robast/.bashrc
 pip install -r /tmp/requirements.txt
 sudo apt-get update && sudo apt-get upgrade -y
 rosdep update
