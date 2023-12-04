532f3c70e50b2a337f9f63070a1ad5e927ffbca7
TipluJacob <j.ritterbach@tiplu.de>
2023-12-04 16:44:44 +0100

    feat(WIP): adds first draft for ros2_control plugin for linear_base_movement

--- src/ros2/moveit/moveit_resources/moveit_door_opening_mechanism_rotating_arm_config/launch/moveit_rviz_simulation_launch.py
+++ src/ros2/moveit/moveit_resources/moveit_door_opening_mechanism_rotating_arm_config/launch/moveit_rviz_simulation_launch.py
@@ -31,6 +31,7 @@ def generate_launch_description():
 
     launch_arguments = {
         "ros2_control_hardware_type": "mock_components",
+        "ros2_control_hardware_type_positon_joint": "linear_base_movement",
         "model_position_joint": "true",
     }
 
