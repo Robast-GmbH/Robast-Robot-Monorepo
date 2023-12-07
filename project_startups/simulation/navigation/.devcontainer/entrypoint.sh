7b638678222edcbd41119e2f117eea463141f0b2
TAlscher <t.alscher@tiplu.de>
2023-12-07 09:56:22 +0100

    removed manual mppi builds

--- project_startups/simulation/navigation/.devcontainer/entrypoint.sh
+++ project_startups/simulation/navigation/.devcontainer/entrypoint.sh
@@ -2,6 +2,3 @@
 sudo apt-get update && sudo apt-get upgrade -y
 rosdep update
 rosdep install --from-paths /workspace/src --ignore-src -r -y
-
-rosdep install --from-paths /mppi --ignore-src -r -y # TODO remove this line when mppi works from package install
-cd /mppi; colcon build --packages-select nav2_mppi_controller # TODO remove this line when mppi works from package install
