c2eb48263d15562631663efcf9cf577990cc3260
TipluJacob <j.ritterbach@tiplu.de>
2023-12-19 15:14:54 +0100

    WIP

--- src/ros2/sensors/ultrasonic_hc_sr04/include/ultrasonic_hc_sr04/ultrasonic_hc_sr04.hpp
+++ src/ros2/sensors/ultrasonic_hc_sr04/include/ultrasonic_hc_sr04/ultrasonic_hc_sr04.hpp
@@ -4,20 +4,16 @@
 #include <inttypes.h>
 
 #include <chrono>
+#include <iostream>
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
+#include <thread>
 // C library headers
+#include <pigpio.h>
 #include <stdio.h>
 #include <string.h>
 
-#include <condition_variable>
-#include <iostream>
-#include <map>
-#include <mutex>
-#include <queue>
-#include <thread>
-
-using namespace std::chrono_literals;
+#include <std_msgs/msg/float32.hpp>
 
 namespace ultrasonic_sensor
 {
@@ -28,7 +24,18 @@ namespace ultrasonic_sensor
     UltrasonicHCSR04();
 
    private:
+    uint8_t _trigger_pin;
+    uint8_t _echo_pin;
+    std::string _distance_topic;
+    std::string _distance_frame;
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr _distance_publisher;
+
     void setup_publishers();
+    void declare_parameters();
+    void init_sensor();
+    double get_distance(uint32_t timeout);
+    long get_travel_time_in_us();
+    long micros();
   };
 }   // namespace ultrasonic_sensor
 #endif   // ULTRASONIC_HC_SR04__ULTRASONIC_HC_SR04_HPP_
--- src/ros2/sensors/ultrasonic_hc_sr04/src/ultrasonic_hc_sr04.cpp
+++ src/ros2/sensors/ultrasonic_hc_sr04/src/ultrasonic_hc_sr04.cpp
@@ -5,11 +5,72 @@ namespace ultrasonic_sensor
   UltrasonicHCSR04::UltrasonicHCSR04() : Node("drawer_bridge")
   {
     setup_publishers();
+    declare_parameters();
+    init_sensor();
+  }
+
+  void UltrasonicHCSR04::declare_parameters()
+  {
+    // Declare parameters
+    this->declare_parameter("trigger_pin", 0);
+    this->declare_parameter("echo_pin", 0);
+    this->declare_parameter("distance_topic", "ultrasonic_distance");
+    this->declare_parameter("distance_frame", "ultrasonic_distance_frame");
+  }
+
+  void UltrasonicHCSR04::init_sensor()
+  {
+    _trigger_pin = this->get_parameter("trigger_pin").as_int();
+    _echo_pin = this->get_parameter("echo_pin").as_int();
+    gpioSetMode(_trigger_pin, PI_OUTPUT);
+    gpioSetMode(_echo_pin, PI_INPUT);
+    gpioWrite(_trigger_pin, 0);
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+
+  double UltrasonicHCSR04::get_distance(uint32_t timeout)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+    gpioWrite(_trigger_pin, 1);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    gpioWrite(_trigger_pin, 0);
+
+    long trigger_timestamp = micros();
+
+    while (gpioRead(_echo_pin) == 0 && micros() - trigger_timestamp < timeout)
+    {
+    }
+
+    long travel_time_us = get_travel_time_in_us();
+
+    double distance_in_meters = 100 * ((travel_time_us / 1000000.0) * 340.29) / 2;
+
+    return distance_in_meters;
+  }
+
+  long UltrasonicHCSR04::micros()
+  {
+    return std::chrono::duration_cast<std::chrono::microseconds>(
+               std::chrono::high_resolution_clock::now().time_since_epoch())
+        .count();
+  }
+
+  long UltrasonicHCSR04::get_travel_time_in_us()
+  {
+    long start_time_us = micros();
+    while (gpioRead(_echo_pin) == 1)
+    {
+    }
+    long end_time_us = micros();
+    return (end_time_us - start_time_us);
   }
 
   void UltrasonicHCSR04::setup_publishers()
   {
-    // TODO@Jacob: Add a publisher to publish the results of the ultrasonic sensor
+    // Add a publisher to publish the results of the ultrasonic sensor
+    _distance_publisher =
+        this->create_publisher<std_msgs::msg::Float32>(this->get_parameter("distance_topic").as_string(), 10);
   }
 
 }   // namespace ultrasonic_sensor
