7da90429518fc5980530ac5dffee50f388f23268
TipluJacob <j.ritterbach@tiplu.de>
2024-01-31 13:29:40 +0100

    chore: formatting with black-formatter

--- src/ros2/simulation/drawer_bridge_simulation/launch/drawer_bridge_simulation_launch.py
+++ src/ros2/simulation/drawer_bridge_simulation/launch/drawer_bridge_simulation_launch.py
@@ -1,4 +1,3 @@
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
@@ -7,14 +6,15 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     drawer_bridge_simulation_node = Node(
-                package='drawer_bridge_simulation',
-                executable='drawer_bridge_simulation',
-                name="drawer_bridge_simulation",
-                parameters=[
-                            {"time_until_drawer_closes_automatically_in_ms": 5000},
-                            {"num_of_drawers": 5},
-                           ],
-                output='screen')
+        package="drawer_bridge_simulation",
+        executable="drawer_bridge_simulation",
+        name="drawer_bridge_simulation",
+        parameters=[
+            {"time_until_drawer_closes_automatically_in_ms": 5000},
+            {"num_of_drawers": 5},
+        ],
+        output="screen",
+    )
 
     ld.add_action(drawer_bridge_simulation_node)
     return ld
--- src/ros2/simulation/tiplu_world/launch/tiplu_world_launch.py
+++ src/ros2/simulation/tiplu_world/launch/tiplu_world_launch.py
@@ -4,7 +4,14 @@ import xacro
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler, OpaqueFunction, GroupAction
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    ExecuteProcess,
+    RegisterEventHandler,
+    OpaqueFunction,
+    GroupAction,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node
@@ -12,39 +19,44 @@ from launch.event_handlers import OnProcessExit
 from launch.launch_context import LaunchContext
 from launch.conditions import IfCondition, UnlessCondition
 
-def launch_robot_state_publisher(context, *args, **settings):
 
+def launch_robot_state_publisher(context, *args, **settings):
     model_position_joint = LaunchConfiguration("model_position_joint").perform(context)
 
     robot_xml = xacro.process_file(
         os.path.join(
             get_package_share_directory("rb_theron_description"),
             "robots",
-            settings['robot'] + ".urdf.xacro",
+            settings["robot"] + ".urdf.xacro",
         ),
-        mappings={"prefix": settings['prefix'],
-                  "ros2_control_hardware_type": "gz_ros2_control",
-                  "ros2_control_hardware_type_positon_joint": "gz_ros2_control",
-                  "ros_distro": settings['ros_distro'],
-                  "model_position_joint": model_position_joint},
+        mappings={
+            "prefix": settings["prefix"],
+            "ros2_control_hardware_type": "gz_ros2_control",
+            "ros2_control_hardware_type_positon_joint": "gz_ros2_control",
+            "ros_distro": settings["ros_distro"],
+            "model_position_joint": model_position_joint,
+        },
     ).toxml()
 
     start_robot_state_publisher_cmd = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         name="robot_state_publisher",
-        parameters=[{"use_sim_time": settings['use_sim_time']}, {"robot_description": robot_xml}],
+        parameters=[
+            {"use_sim_time": settings["use_sim_time"]},
+            {"robot_description": robot_xml},
+        ],
         output="screen",
     )
 
     return [start_robot_state_publisher_cmd]
 
 
-#TODO @all: There are still some small issues with loading packages, but it starts and seems to work fine.
+# TODO @all: There are still some small issues with loading packages, but it starts and seems to work fine.
+
 
 def create_world_urdf(context, *args, **settings):
-
-    world_model = LaunchConfiguration('world_model').perform(context)
+    world_model = LaunchConfiguration("world_model").perform(context)
 
     # In order to swap out 'package://' paths with absolute path we need to:
     # (1) Replace the paths in the urdf text
@@ -52,7 +64,9 @@ def create_world_urdf(context, *args, **settings):
     # (3) Pass the path of the modified_world_file to Gazebo launch
 
     modified_world_file = os.path.join(
-        get_package_share_directory("tiplu_world"), "worlds", "auto_created_gazebo_world.sdf"
+        get_package_share_directory("tiplu_world"),
+        "worlds",
+        "auto_created_gazebo_world.sdf",
     )
     with open(world_model, "r") as file:
         world_sdf = path_pattern_change_for_gazebo(file.read())
@@ -61,12 +75,15 @@ def create_world_urdf(context, *args, **settings):
 
     gz_sim_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            settings['gz_sim_launch'],
+            settings["gz_sim_launch"],
         ),
-        launch_arguments={"gz_args": ["-r ", settings['headless'], " ", modified_world_file]}.items(),
+        launch_arguments={
+            "gz_args": ["-r ", settings["headless"], " ", modified_world_file]
+        }.items(),
     )
 
     return [gz_sim_cmd]
+
 
 def path_pattern_change_for_gazebo(urdf_string):
     """
@@ -81,8 +98,8 @@ def path_pattern_change_for_gazebo(urdf_string):
 
     return data
 
-def generate_launch_description():
 
+def generate_launch_description():
     context = LaunchContext()
 
     declare_model_position_joint_cmd = DeclareLaunchArgument(
@@ -90,7 +107,7 @@ def generate_launch_description():
         default_value="false",
         description="whether to model the position joint or not",
     )
-    
+
     ros_distro = os.environ["ROS_DISTRO"]
     gz_version = os.environ["GZ_VERSION"]
 
@@ -99,21 +116,24 @@ def generate_launch_description():
     headless = LaunchConfiguration("headless")
     robot_name = LaunchConfiguration("robot_name")
     model_position_joint = LaunchConfiguration("model_position_joint")
-    init_x = os.environ['init_x']
+    init_x = os.environ["init_x"]
     init_y = os.environ["init_y"]
     init_yaw = os.environ["init_yaw"]
     prefix = os.environ["prefix"]
     robot = os.environ["robot"]
 
-    if (gz_version == "fortress"):
+    if gz_version == "fortress":
         pkg_ros_gz_sim = get_package_share_directory("ros_ign_gazebo")
         gz_sim_launch = os.path.join(pkg_ros_gz_sim, "launch", "ign_gazebo.launch.py")
-        gz_ros_bridge_yaml = os.path.join(get_package_share_directory("tiplu_world"), "config", "ign_ros_bridge.yaml")
-    if (gz_version == "garden" or gz_version == "harmonic"):
+        gz_ros_bridge_yaml = os.path.join(
+            get_package_share_directory("tiplu_world"), "config", "ign_ros_bridge.yaml"
+        )
+    if gz_version == "garden" or gz_version == "harmonic":
         pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
         gz_sim_launch = os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
-        gz_ros_bridge_yaml = os.path.join(get_package_share_directory("tiplu_world"), "config", "gz_ros_bridge.yaml")
-        
+        gz_ros_bridge_yaml = os.path.join(
+            get_package_share_directory("tiplu_world"), "config", "gz_ros_bridge.yaml"
+        )
 
     robot_xml = xacro.process_file(
         os.path.join(
@@ -121,15 +141,17 @@ def generate_launch_description():
             "robots",
             os.environ["robot"] + ".urdf.xacro",
         ),
-        mappings={"prefix": os.environ["prefix"],
-                  "ros2_control_hardware_type": "gz_ros2_control",
-                  "ros_distro": ros_distro},
+        mappings={
+            "prefix": os.environ["prefix"],
+            "ros2_control_hardware_type": "gz_ros2_control",
+            "ros_distro": ros_distro,
+        },
     ).toxml()
 
     use_sim_time = LaunchConfiguration("use_sim_time")
     headless = LaunchConfiguration("headless")
     robot_name = LaunchConfiguration("robot_name")
-    init_x = os.environ['init_x']
+    init_x = os.environ["init_x"]
     init_y = os.environ["init_y"]
     init_yaw = os.environ["init_yaw"]
 
@@ -147,47 +169,61 @@ def generate_launch_description():
         "world_model",
         default_value=os.path.join(
             get_package_share_directory("tiplu_world"), "worlds", "6OG" + ".sdf"
-            
         ),
         description="path to the world model, alternative get_package_share_directory(rmf_gazebo), maps, tiplu_ign , tiplu.world",
     )
-    
+
     # Add world/models to the path
-    ign_resource_path = os.environ.get('IGN_GAZEBO_RESOURCE_PATH', '')
-    world_path = os.path.join(get_package_share_directory("rmf_gazebo"), "maps", "tiplu_ign" , "tiplu.world")
-    if world_path not in ign_resource_path.split(':'):
-        ign_resource_path += ':' + world_path
-    world_path = os.path.join(get_package_share_directory("rmf_gazebo"), "maps", "tiplu_ign" , "models")
-    if world_path not in ign_resource_path.split(':'):
-        ign_resource_path += ':' + world_path
-    world_path = os.path.join(get_package_share_directory("rmf_gazebo"), "maps", "tiplu" , "models")
-    if world_path not in ign_resource_path.split(':'):
-        ign_resource_path += ':' + world_path
-    world_path = os.path.join(get_package_share_directory("tiplu_world"), "models", "6_OG_normal_doors")
-    if world_path not in ign_resource_path.split(':'):
-        ign_resource_path += ':' + world_path
-    world_path = os.path.join(os.environ['HOME'],".gazebo", "models")
-    if world_path not in ign_resource_path.split(':'):
-        ign_resource_path += ':' + world_path
-    os.environ['IGN_GAZEBO_RESOURCE_PATH'] = ign_resource_path
-    
+    ign_resource_path = os.environ.get("IGN_GAZEBO_RESOURCE_PATH", "")
+    world_path = os.path.join(
+        get_package_share_directory("rmf_gazebo"), "maps", "tiplu_ign", "tiplu.world"
+    )
+    if world_path not in ign_resource_path.split(":"):
+        ign_resource_path += ":" + world_path
+    world_path = os.path.join(
+        get_package_share_directory("rmf_gazebo"), "maps", "tiplu_ign", "models"
+    )
+    if world_path not in ign_resource_path.split(":"):
+        ign_resource_path += ":" + world_path
+    world_path = os.path.join(
+        get_package_share_directory("rmf_gazebo"), "maps", "tiplu", "models"
+    )
+    if world_path not in ign_resource_path.split(":"):
+        ign_resource_path += ":" + world_path
+    world_path = os.path.join(
+        get_package_share_directory("tiplu_world"), "models", "6_OG_normal_doors"
+    )
+    if world_path not in ign_resource_path.split(":"):
+        ign_resource_path += ":" + world_path
+    world_path = os.path.join(os.environ["HOME"], ".gazebo", "models")
+    if world_path not in ign_resource_path.split(":"):
+        ign_resource_path += ":" + world_path
+    os.environ["IGN_GAZEBO_RESOURCE_PATH"] = ign_resource_path
+
     # Add plugins to the path
-    ign_plugin_path = os.environ.get('IGN_GAZEBO_SYSTEM_PLUGIN_PATH', '')
-    world_path = os.path.join(get_package_share_directory("rmf_robot_sim_gz_plugins"), "lib", "rmf_robot_sim_gz_plugins")
-    if world_path not in ign_plugin_path.split(':'):
-        ign_plugin_path += ':' + world_path
-    world_path = os.path.join(get_package_share_directory("rmf_building_sim_gz_plugins"), "lib", "rmf_building_sim_gz_plugins")
-    if world_path not in ign_plugin_path.split(':'):
-        ign_plugin_path += ':' + world_path
-    os.environ['IGN_GAZEBO_SYSTEM_PLUGIN_PATH'] = ign_plugin_path
-    os.environ['IGN_GUI_PLUGIN_PATH'] = ign_plugin_path
-    
-    
+    ign_plugin_path = os.environ.get("IGN_GAZEBO_SYSTEM_PLUGIN_PATH", "")
+    world_path = os.path.join(
+        get_package_share_directory("rmf_robot_sim_gz_plugins"),
+        "lib",
+        "rmf_robot_sim_gz_plugins",
+    )
+    if world_path not in ign_plugin_path.split(":"):
+        ign_plugin_path += ":" + world_path
+    world_path = os.path.join(
+        get_package_share_directory("rmf_building_sim_gz_plugins"),
+        "lib",
+        "rmf_building_sim_gz_plugins",
+    )
+    if world_path not in ign_plugin_path.split(":"):
+        ign_plugin_path += ":" + world_path
+    os.environ["IGN_GAZEBO_SYSTEM_PLUGIN_PATH"] = ign_plugin_path
+    os.environ["IGN_GUI_PLUGIN_PATH"] = ign_plugin_path
+
     declare_headless_cmd = DeclareLaunchArgument(
         "headless",
         default_value=" ",
         description="Weather to run in headless mode (-s) or with gui ''",
-    )    
+    )
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         "use_sim_time",
@@ -201,17 +237,21 @@ def generate_launch_description():
         description="whether to model the position joint or not",
     )
 
-
     # As far as I understand, to get the value of a launch argument we need a OpaqueFunction for this as described here:
     # https://robotics.stackexchange.com/questions/104340/getting-the-value-of-launchargument-inside-python-launch-file
     launch_gazebo_opaque_func = OpaqueFunction(
         function=create_world_urdf,
-        kwargs={'gz_sim_launch': gz_sim_launch, 'headless': headless}
+        kwargs={"gz_sim_launch": gz_sim_launch, "headless": headless},
     )
 
     launch_robot_state_publisher_opaque_func = OpaqueFunction(
         function=launch_robot_state_publisher,
-        kwargs={'use_sim_time': use_sim_time, 'robot': robot, 'prefix': prefix, 'ros_distro': ros_distro}
+        kwargs={
+            "use_sim_time": use_sim_time,
+            "robot": robot,
+            "prefix": prefix,
+            "ros_distro": ros_distro,
+        },
     )
 
     spawn_robot_cmd = Node(
@@ -244,88 +284,129 @@ def generate_launch_description():
     )
 
     load_joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_state_broadcaster',
-             '--use-sim-time'],
-        output='screen'
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state",
+            "active",
+            "joint_state_broadcaster",
+            "--use-sim-time",
+        ],
+        output="screen",
     )
 
     load_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_trajectory_controller',
-             '--use-sim-time'],
-        output='screen'
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state",
+            "active",
+            "joint_trajectory_controller",
+            "--use-sim-time",
+        ],
+        output="screen",
     )
 
     load_mobile_base_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'mobile_base_controller_cmd_vel',
-             '--use-sim-time'],
-        output='screen'
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state",
+            "active",
+            "mobile_base_controller_cmd_vel",
+            "--use-sim-time",
+        ],
+        output="screen",
     )
 
     load_diff_drive_base_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'diff_drive_base_controller',
-             '--use-sim-time'],
-        output='screen'
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state",
+            "active",
+            "diff_drive_base_controller",
+            "--use-sim-time",
+        ],
+        output="screen",
     )
 
     load_drawer_joint_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'drawer_joint_trajectory_controller',
-             '--use-sim-time'],
-        output='screen'
+        cmd=[
+            "ros2",
+            "control",
+            "load_controller",
+            "--set-state",
+            "active",
+            "drawer_joint_trajectory_controller",
+            "--use-sim-time",
+        ],
+        output="screen",
     )
 
     spawn_ros2_controller_without_arm = GroupAction(
         condition=UnlessCondition(model_position_joint),
-        actions=[RegisterEventHandler(
-                    event_handler=OnProcessExit(
-                        target_action=spawn_robot_cmd,
-                        on_exit=[load_joint_state_broadcaster],
-                    )
-                ),
-                RegisterEventHandler(
-                    event_handler=OnProcessExit(
-                        target_action=load_joint_state_broadcaster,
-                        on_exit=[load_diff_drive_base_controller],
-                    )
-                ),
-                RegisterEventHandler(
-                    event_handler=OnProcessExit(
-                        target_action=load_diff_drive_base_controller,
-                        on_exit=[load_drawer_joint_trajectory_controller],
-                    )
-                )])
+        actions=[
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=spawn_robot_cmd,
+                    on_exit=[load_joint_state_broadcaster],
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=load_joint_state_broadcaster,
+                    on_exit=[load_diff_drive_base_controller],
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=load_diff_drive_base_controller,
+                    on_exit=[load_drawer_joint_trajectory_controller],
+                )
+            ),
+        ],
+    )
 
     spawn_ros2_controller_with_arm = GroupAction(
         condition=IfCondition(PythonExpression([model_position_joint])),
-        actions=[RegisterEventHandler(
-                    event_handler=OnProcessExit(
-                        target_action=spawn_robot_cmd,
-                        on_exit=[load_joint_state_broadcaster],
-                    )
-                ),
-                RegisterEventHandler(
-                    event_handler=OnProcessExit(
-                        target_action=load_joint_state_broadcaster,
-                        on_exit=[load_mobile_base_controller],
-                    )
-                ),
-                RegisterEventHandler(
-                    event_handler=OnProcessExit(
-                        target_action=load_mobile_base_controller,
-                        on_exit=[load_joint_trajectory_controller],
-                    )
-                ),
-                RegisterEventHandler(
-                    event_handler=OnProcessExit(
-                        target_action=load_joint_trajectory_controller,
-                        on_exit=[load_diff_drive_base_controller],
-                    )
-                ),
-                RegisterEventHandler(
-                    event_handler=OnProcessExit(
-                        target_action=load_diff_drive_base_controller,
-                        on_exit=[load_drawer_joint_trajectory_controller],
-                    )
-                )])
+        actions=[
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=spawn_robot_cmd,
+                    on_exit=[load_joint_state_broadcaster],
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=load_joint_state_broadcaster,
+                    on_exit=[load_mobile_base_controller],
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=load_mobile_base_controller,
+                    on_exit=[load_joint_trajectory_controller],
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=load_joint_trajectory_controller,
+                    on_exit=[load_diff_drive_base_controller],
+                )
+            ),
+            RegisterEventHandler(
+                event_handler=OnProcessExit(
+                    target_action=load_diff_drive_base_controller,
+                    on_exit=[load_drawer_joint_trajectory_controller],
+                )
+            ),
+        ],
+    )
 
     ld = LaunchDescription()
 
