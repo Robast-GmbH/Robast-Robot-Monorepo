aea25af442e3de08d15374ed5d7efbf9c3bfe274
TAlscher <t.alscher@tiplu.de>
2024-03-21 12:11:23 +0100

    example rosbridge web

---
+++ src/ros2/fleet_management/fleet_manager/fleet_manager/get_ros_values.py
@@ -0,0 +1,55 @@
+#pip install roslibpy
+
+from __future__ import print_function
+
+import threading
+import time
+
+from roslibpy import Header, Message, Ros, Time, Topic
+
+def test_topic_pubsub():
+    context = dict(wait=threading.Event(), counter=0)
+
+    ros = Ros("127.0.0.1", 9090)
+    ros.run()
+
+    listener_map = Topic(ros, "/map", "std_msgs/String")
+    publisher = Topic(ros, "/nav_goal", "")
+
+    def receive_message(message):
+        context["counter"] += 1
+        assert message["data"] == "hello world", "Unexpected message content"
+
+        if context["counter"] == 3:
+            listener_map.unsubscribe()
+            context["wait"].set()
+
+    def start_sending():
+        while True:
+            if context["counter"] >= 3:
+                break
+            publisher.publish(Message({"data": "0 1 2"}))
+            time.sleep(0.1)
+        publisher.unadvertise()
+
+    def start_receiving():
+        listener_map.subscribe(receive_message)
+
+    t1 = threading.Thread(target=start_receiving)
+    t2 = threading.Thread(target=start_sending)
+
+    t1.start()
+    t2.start()
+
+    if not context["wait"].wait(10):
+        raise Exception
+
+    t1.join()
+    t2.join()
+
+    assert context["counter"] >= 3, "Expected at least 3 messages but got " + str(context["counter"])
+    ros.close()
+    
+if __name__ == "__main__":
+    test_topic_pubsub()
+    print("All tests passed!")
