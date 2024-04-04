f7de8f6619adb75a213e3ebde48dcae74bb87ff1
TAlscher <t.alscher@tiplu.de>
2024-04-04 09:46:19 +0200

    more examples

--- src/Hardware/py/PSS2000_conntector/src/establish_connection.py
+++ src/Hardware/py/PSS2000_conntector/src/establish_connection.py
@@ -20,37 +20,51 @@ class TCPConnection:
         # Close socket
         if self.sock is not None:
             self.sock.close()
-
+            
+    
     def send(self, message):
-        # Convert message to bytes
-        bytes_to_send = message.encode()
+        # Add STX and <CR> to the message
+        message_with_stx_cr = "\x02" + message + "\x03"
 
-        # Send length of byte array
-        self.sock.sendall(len(bytes_to_send).to_bytes(4, byteorder="big"))
+        # Convert message to bytes
+        bytes_to_send = message_with_stx_cr.encode()
 
         # Send byte array
         self.sock.sendall(bytes_to_send)
 
-    def receive(self) -> bytes:
+    def receive(self):
         # Receive STX byte
         stx_byte = self.sock.recv(1)
         if stx_byte != b'\x02':
-            raise ValueError("Invalid message format: missing STX byte")
+            print("Invalid message format: missing STX byte")
+            return b''.decode(), b''.decode()
 
         # Receive length byte
-        length_byte = self.sock.recv(1)
-        bytes_length = int.from_bytes(length_byte, byteorder="big")
+        length_byte = self.sock.recv(2)
+        
+        # Check if any byte of length_byte contains '\x03'
+        if b'\x03' in length_byte:
+            return b''.decode(), b''.decode()
+        
+        bytes_length = hex_to_decimal(length_byte.decode())*2
 
         # Receive byte array
         bytes_received = self.sock.recv(bytes_length)
 
-        # Receive ETX byte
+        # # Receive ETX byte
         # etx_byte = self.sock.recv(1)
         # if etx_byte != b'\x03':
         #     raise ValueError("Invalid message format: missing ETX byte")
 
         # Convert byte array to string
-        return bytes_received
+        return bytes_received.decode(), bytes_length
+    
+def hex_to_decimal(hex_string):
+    try:
+        decimal_number = int(hex_string, 16)
+    except ValueError:
+        return 0
+    return decimal_number
 
 # Main function
 def main():
@@ -60,20 +74,17 @@ def main():
 
     # Connect to server
     with TCPConnection(ip, port) as connection:
-        # # Create message to send
-        # message_to_send = "Hello, server!"
-
-        # # Send message
-        # connection.send(message_to_send)
+        # Create message to send
+        # message_to_send = "\x020E2E765903030000000000000065\x03"
+        # message_to_send = "0327CF" #Get Software Version
+        message_to_send = "0326FE" #Reader Status
+        # Send message
         
-        for i in range(0, 20):
+        for i in range(0, 10):
             # Receive message
-            message_received = connection.receive()
-            # Print received message
-            decoded_message = message_received.decode()
-            if (len(decoded_message)+1) % 2 != 0:
-                decoded_message = '0' + decoded_message
-            print("Received message:", decoded_message)
+            connection.send(message_to_send)
+            message_received, length = connection.receive()
+            print(f"Received message: {message_received} with length: {length}")
 if __name__ == "__main__":
     main()
 
@@ -85,4 +96,14 @@ oder Reader Status	0x02	n 	26	0x06	Output	Modis	Input	LF-Dist.	Status	max.LF-Amp
 
 0x00035976 -> ASCII:5976
 
+
+test message:
+stx: 0x02
+n: 0x0E
+cmd: 2E
+ID: 0x00035976 -> 765903
+Aktor: 0x80 
+0x00 00 00 00 00 00 00
+CRC: 
+ETX: 0x03
 '''
