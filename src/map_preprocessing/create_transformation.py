971be3cf530385e183e1652d6f829ad9b38e3bb6
sagarvl96 <s.lingaraj@robast.de>
2024-04-11 09:00:49 +0200

    created transformation matrix betweem two images

--- src/map_preprocessing/create_transformation.py
+++ src/map_preprocessing/create_transformation.py
@@ -5,8 +5,8 @@ import cv2 as cv
 from LightGlue.lightglue import LightGlue, SuperPoint, DISK, SIFT, ALIKED, DoGHardNet, viz2d
 from LightGlue.lightglue.utils import load_image, rbd
 
-image1 = load_image("src/map_preprocessing/LightGlue/assets/DSC_0410.JPG")
-image2 = load_image("src/map_preprocessing/LightGlue/assets/DSC_0411.JPG")
+image1 = load_image("/workspace/src/map_preprocessing/output.jpg")
+image2 = load_image("/workspace/src/map_preprocessing/tiplu_6OG.jpg")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -25,8 +25,9 @@ feats0, feats1, matches01 = [
 kpts1, kpts2, matches = feats1["keypoints"], feats2["keypoints"], matches12["matches"]
 
 kpc1, kpc2 = viz2d.cm_prune(matches12["prune0"]), viz2d.cm_prune(matches12["prune1"])
-viz2d.plot_images([image1, image2])
-viz2d.plot_keypoints([kpts1, kpts2])
+print(kpc1, kpc2)
+#viz2d.plot_images([image1, image2])
+#iz2d.plot_keypoints([kpts1, kpts2])
 #kpts1_np, kpts2_np = kpts1.cpu().numpy(), kpts2.cpu().numpy()
 #print(kpts1)
 #m_kpts1, m_kpts2 = kpts1[matches[..., 0]], kpts2[matches[..., 1]]
