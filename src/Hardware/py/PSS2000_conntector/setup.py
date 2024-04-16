fd5e5ac937f32dd3023a42cc09ea848d5ff7bf96
TAlscher <t.alscher@tiplu.de>
2024-04-16 17:38:55 +0200

    pss2000 naming fixed

--- src/Hardware/py/PSS2000_conntector/setup.py
+++ src/Hardware/py/PSS2000_conntector/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PSS200_connector",
-    version="0.1",
+    name="PSS2000_connector",
+    version="0.2",
     packages=find_packages(),
 )
