175ebc050769663d19eb289d37fbae328bddc877
ane-robast <a.neumann@robast.de>
2024-04-18 17:23:18 +0200

    Update fleet_management devcontainer.json

--- project_startups/fleet_management/.devcontainer/entrypoint.sh
+++ project_startups/fleet_management/.devcontainer/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y
 rosdep update
-rosdep install --from-paths /workspace/src --ignore-src -r -y
+rosdep install --from-paths /workspace/src/open_rmf --ignore-src -r -y
+rosdep install --from-paths /workspace/src/communication_interfaces --ignore-src -r -y
