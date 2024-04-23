c31c07d3af8ad3983900934f1a944832b29cc271
ane-robast <a.neumann@robast.de>
2024-04-23 14:08:13 +0200

    Add small robot_backend hint

--- src/robot_backend/main.py
+++ src/robot_backend/main.py
@@ -2,5 +2,5 @@ from robot_api import app
 import uvicorn
 
 if __name__ == "__main__":
-    # "192.168.0.200"
+    # host="192.168.0.200" on robot "0.0.0.0" for localhost
     uvicorn.run(app, host="0.0.0.0", port=8005)
