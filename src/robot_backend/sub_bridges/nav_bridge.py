68f0ef0c101b519bd7a426e48d253ab99d871006
ane-robast <a.neumann@robast.de>
2024-04-25 15:07:09 +0200

    Optimize Quaternion from Euler function for yaw only use

--- src/robot_backend/sub_bridges/nav_bridge.py
+++ src/robot_backend/sub_bridges/nav_bridge.py
@@ -26,37 +26,25 @@ class NavBridge(BaseBridge):
             "std_msgs/msg/Bool",
         )
 
-    def get_quaternion_from_euler(self, roll, pitch, yaw):
+    def get_quaternion_from_euler(self, yaw):
         """
         Convert an Euler angle to a quaternion.
 
         Input
-            :param roll: The roll (rotation around x-axis) angle in radians.
-            :param pitch: The pitch (rotation around y-axis) angle in radians.
             :param yaw: The yaw (rotation around z-axis) angle in radians.
 
         Output
             :return qx, qy, qz, qw: The orientation in quaternion [x,y,z,w] format
         """
-        qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - np.cos(
-            roll / 2
-        ) * np.sin(pitch / 2) * np.sin(yaw / 2)
-        qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + np.sin(
-            roll / 2
-        ) * np.cos(pitch / 2) * np.sin(yaw / 2)
-        qz = np.cos(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2) - np.sin(
-            roll / 2
-        ) * np.sin(pitch / 2) * np.cos(yaw / 2)
-        qw = np.cos(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) + np.sin(
-            roll / 2
-        ) * np.sin(pitch / 2) * np.sin(yaw / 2)
+        qx = 0
+        qy = 0
+        qz = np.sin(yaw / 2)
+        qw = np.cos(yaw / 2)
 
-        return [qx, qy, qz, qw]
+        return {"x": qx, "y": qy, "z": qz, "w": qw}
 
     def navigate_to_goal_pose(self, robot_name, goal_pose):
         self.context["/is_navigating"] =  {"data": True}
-        goal_rotation = self.get_quaternion_from_euler(0, 0, goal_pose[2])
-
         goal_msg = Message(
             {
                 "position": {
@@ -64,12 +52,7 @@ class NavBridge(BaseBridge):
                     "y": goal_pose[1],
                     "z": 0.0,
                 },
-                "orientation": {
-                    "x": goal_rotation[0],
-                    "y": goal_rotation[1],
-                    "z": goal_rotation[2],
-                    "w": goal_rotation[3],
-                },
+                "orientation": self.get_quaternion_from_euler(goal_pose[2]),
             }
         )
         self._goal_pose_publisher.publish(goal_msg)
