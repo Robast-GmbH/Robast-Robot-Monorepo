53f7e7c4ad0bf829c5a64b2465dc97a614baed2d
TipluJacob <j.ritterbach@tiplu.de>
2024-05-22 10:40:27 +0200

    fix: updates docker_run script for moveit

--- project_startups/moveit/.devcontainer/docker_run.sh
+++ project_startups/moveit/.devcontainer/docker_run.sh
@@ -1,4 +1,5 @@
 # this script is used to run the docker container on the robot to have a rviz gui
+# Please mind that you have to run this via Remmina
 docker run \
 	--name=moveit_devel \
 	--network=host \
@@ -9,13 +10,15 @@ docker run \
 	--volume=/home/robot/robast/Monorepo/src/ros2/robot_description:/workspace/src/robot_description \
 	--volume=/home/robot/robast/Monorepo/src/ros2/ros2_control/hardware_interfaces/ros2_control_plugin_dryve_d1:/workspace/src/ros2_control/hardware_interfaces/ros2_control_plugin_dryve_d1 \
 	--volume=/home/robot/robast/Monorepo/src/ros2/ros2_control/hardware_interfaces/ros2_control_base_movement:/workspace/src/ros2_control/hardware_interfaces/ros2_control_base_movement \
+	--volume=/home/robot/robast/Monorepo/src/ros2/ros2_control/hardware_interfaces/hardware_interface_utils:/workspace/src/ros2_control/hardware_interfaces/hardware_interface_utils \
 	--volume=/home/robot/robast/Monorepo/src/ros2/moveit:/workspace/src/moveit \
 	--volume=/home/robot/robast/Monorepo/src/ros2/ros2_control/ros2_controllers:/workspace/src/ros2_control/ros2_controllers \
 	--volume=/home/robot/robast/Monorepo/src/ros2/dds_configs:/workspace/dds_configs \
-	--volume=/home/robot/robast/Monorepo/src/ros2/utils:/workspace/src/utils,type=bind \
+	--volume=/home/robot/robast/Monorepo/src/ros2/utils:/workspace/src/utils \
 	-e DISPLAY=:0 \
-	--env ROS_DOMAIN_ID=0 \
+	--env ROS_DOMAIN_ID=23 \
 	--env robot=rb_theron \
 	--env prefix="" \
+	--user robast \
 	ghcr.io/robast-gmbh/monorepo/moveit:devel-humble
 
