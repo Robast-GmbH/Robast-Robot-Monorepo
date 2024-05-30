75edcaa3db99e022fa47916596b302edea7c11a5
TipluJacob <j.ritterbach@tiplu.de>
2024-05-30 14:37:42 +0200

    chore: probably finally reduced code duplication enough

--- project_startups/oak-d/src/oak_bringup/launch/door_opening_cam_launch.py
+++ project_startups/oak-d/src/oak_bringup/launch/door_opening_cam_launch.py
@@ -2,18 +2,33 @@ import os
 import sys
 
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription, launch_description_sources
-from launch.actions import IncludeLaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-import launch_ros.actions
-import launch_ros.descriptions
+from launch import LaunchDescription
 
 sys.path.append(os.path.join(get_package_share_directory('oak_bringup'), 'launch'))
-from utils.oak_launch_arguments import add_launch_arguments
+from utils.oak_launch_helper import generate_stereo_launch_description
 
 def generate_launch_description():
-    urdf_launch_dir = os.path.join(get_package_share_directory('depthai_descriptions'), 'launch')
+    urdf_launch_dir = os.path.join(os.path.join(get_package_share_directory('oak_bringup'), 'launch'), 'door_opening_cam_urdf_launch.py')
+
+    launch_configurations_oak_d = {
+        'camera_model': 'OAK-D',
+        'tf_prefix': 'oak',
+        'base_frame': 'back_top_oak_d_camera',
+        'parent_frame': 'door_opening_mechanism_link_y_axis_slide',
+        'cam_pos_x': '-0.05',
+        'cam_pos_y': '0.0',
+        'cam_pos_z': '0.13721',
+        'cam_roll': '0',
+        'cam_pitch': '0.5235988',
+        'cam_yaw': '3.14159',
+        'mode': 'depth',
+        'lrcheck': True,
+        'extended': True,
+        'subpixel': False,
+        'confidence': 120,
+        'LRchecktresh': 5,
+        'monoResolution': '400p'
+    }
 
     TF_TOPIC = "/tf"
     TF_STATIC_TOPIC = "/tf_static"
@@ -23,102 +38,6 @@ def generate_launch_description():
         (TF_STATIC_TOPIC, "/" + NAMESPACE_ARM + TF_STATIC_TOPIC),
     ]
 
-    camera_model = LaunchConfiguration('camera_model',  default = 'OAK-D')
-    tf_prefix  = LaunchConfiguration('tf_prefix',       default = 'oak')
-    base_frame   = LaunchConfiguration('base_frame',    default = 'back_top_oak_d_camera')
-    parent_frame = LaunchConfiguration('parent_frame',  default = 'door_opening_mechanism_link_y_axis_slide')
+    actions_to_launch = generate_stereo_launch_description(launch_configurations_oak_d, urdf_launch_dir, NAMESPACE_ARM, remappings_tf)
 
-    cam_pos_x  = LaunchConfiguration('cam_pos_x',     default = '-0.05')
-    cam_pos_y  = LaunchConfiguration('cam_pos_y',     default = '0.0')
-    cam_pos_z  = LaunchConfiguration('cam_pos_z',     default = '0.13721')
-    cam_roll   = LaunchConfiguration('cam_roll',      default = '0')
-    cam_pitch  = LaunchConfiguration('cam_pitch',     default = '0.5235988')
-    cam_yaw    = LaunchConfiguration('cam_yaw',       default = '3.14159')
-
-    mode           = LaunchConfiguration('mode', default = 'depth')
-    lrcheck        = LaunchConfiguration('lrcheck', default = True)
-    extended       = LaunchConfiguration('extended', default = True)
-    subpixel       = LaunchConfiguration('subpixel', default = False)
-    confidence     = LaunchConfiguration('confidence', default = 120)
-    LRchecktresh   = LaunchConfiguration('LRchecktresh', default = 5)
-    monoResolution = LaunchConfiguration('monoResolution',  default = '400p')
-
-    urdf_launch = IncludeLaunchDescription(
-        launch_description_sources.PythonLaunchDescriptionSource(
-            os.path.join(os.path.join(get_package_share_directory('oak_bringup'), 'launch'), 'door_opening_cam_urdf_launch.py')),
-                launch_arguments={'tf_prefix' : tf_prefix,
-                                    'camera_model': camera_model,
-                                    'base_frame'  : base_frame,
-                                    'parent_frame': parent_frame,
-                                    'cam_pos_x'   : cam_pos_x,
-                                    'cam_pos_y'   : cam_pos_y,
-                                    'cam_pos_z'   : cam_pos_z,
-                                    'cam_roll'    : cam_roll,
-                                    'cam_pitch'   : cam_pitch,
-                                    'cam_yaw'     : cam_yaw,
-                                    'namespace'   : NAMESPACE_ARM}.items())
-
-    stereo_node = launch_ros.actions.Node(
-            package='depthai_examples', executable='stereo_node',
-            output='screen',
-            remappings=remappings_tf,
-            namespace=NAMESPACE_ARM,
-            parameters=[{'tf_prefix': tf_prefix},
-                        {'mode': mode},
-                        {'lrcheck': lrcheck},
-                        {'extended': extended},
-                        {'subpixel': subpixel},
-                        {'confidence': confidence},
-                        {'LRchecktresh': LRchecktresh},
-                        {'monoResolution': monoResolution}])
-
-
-    metric_converter_node = launch_ros.actions.ComposableNodeContainer(
-            name='container',
-            namespace=NAMESPACE_ARM,
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                # Driver itself
-                launch_ros.descriptions.ComposableNode(
-                    package='depth_image_proc',
-                    plugin='depth_image_proc::ConvertMetricNode',
-                    name='convert_metric_node',
-                    namespace=NAMESPACE_ARM,
-                    remappings=[('image_raw', 'stereo/depth'),
-                                ('camera_info', 'stereo/camera_info'),
-                                ('image', 'stereo/converted_depth')]
-                ),
-            ],
-            output='screen',)
-
-    point_cloud_node = launch_ros.actions.ComposableNodeContainer(
-            name='container2',
-            namespace=NAMESPACE_ARM,
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                # Driver itself
-                launch_ros.descriptions.ComposableNode(
-                    package='depth_image_proc',
-                    plugin='depth_image_proc::PointCloudXyziNode',
-                    name='point_cloud_xyzi',
-                    namespace=NAMESPACE_ARM,
-                    remappings=[('depth/image_rect', 'stereo/converted_depth'),
-                                ('intensity/image_rect', 'right/image_rect'),
-                                ('intensity/camera_info', 'right/camera_info'),
-                                ('points', 'stereo/points')]
-                ),
-            ],
-            output='screen',)
-
-    ld = LaunchDescription()
-
-    add_launch_arguments(ld)
-
-    ld.add_action(stereo_node)
-    ld.add_action(urdf_launch)
-
-    ld.add_action(metric_converter_node)
-    ld.add_action(point_cloud_node)
-    return ld
+    return LaunchDescription(actions_to_launch)
--- project_startups/oak-d/src/oak_bringup/launch/stereo_launch.py
+++ project_startups/oak-d/src/oak_bringup/launch/stereo_launch.py
@@ -2,111 +2,38 @@ import os
 import sys
 
 from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription, launch_description_sources
-from launch.actions import IncludeLaunchDescription
-import launch_ros.actions
-import launch_ros.descriptions
-from launch.substitutions import LaunchConfiguration
+from launch import LaunchDescription
 
 sys.path.append(os.path.join(get_package_share_directory('oak_bringup'), 'launch'))
-from utils.oak_launch_arguments import add_launch_arguments
+from utils.oak_launch_helper import generate_stereo_launch_description
 
 
 def generate_launch_description():
-    urdf_launch_dir = os.path.join(get_package_share_directory('depthai_descriptions'), 'launch')
+    urdf_launch_dir = os.path.join(get_package_share_directory('depthai_descriptions'), 'launch', 'urdf_launch.py')
 
-    camera_model = LaunchConfiguration('camera_model',  default = 'OAK-D')
-    tf_prefix  = LaunchConfiguration('tf_prefix',   default = 'oak_s')
-    base_frame   = LaunchConfiguration('base_frame',    default = 'oak-d_frame')
-    parent_frame = LaunchConfiguration('parent_frame',  default = 'robot_base_link')
+    launch_configurations_oak_d = {
+        'camera_model': 'OAK-D',
+        'tf_prefix': 'oak_s',
+        'base_frame': 'oak-d_frame',
+        'parent_frame': 'robot/base_link',
+        'cam_pos_x': '0.317',
+        'cam_pos_y': '-0.197',
+        'cam_pos_z': '1.3044',
+        'cam_roll': '0.0',
+        'cam_pitch': '0.0',
+        'cam_yaw': '0.0',
+        'mode': 'depth',
+        'lrcheck': True,
+        'extended': False,
+        'subpixel': True,
+        'confidence': 120,
+        'LRchecktresh': 5,
+        'monoResolution': '720p'
+    }
 
-    cam_pos_x  = LaunchConfiguration('cam_pos_x',     default = '0.317')
-    cam_pos_y  = LaunchConfiguration('cam_pos_y',     default = '-0.197')
-    cam_pos_z  = LaunchConfiguration('cam_pos_z',     default = '1.3044')
-    cam_roll   = LaunchConfiguration('cam_roll',      default = '0.0')
-    cam_pitch  = LaunchConfiguration('cam_pitch',     default = '0.0')
-    cam_yaw    = LaunchConfiguration('cam_yaw',       default = '0.0')
+    namespace = ''
+    remappings = []
 
-    mode           = LaunchConfiguration('mode', default = 'depth')
-    lrcheck        = LaunchConfiguration('lrcheck', default = True)
-    extended       = LaunchConfiguration('extended', default = False)
-    subpixel       = LaunchConfiguration('subpixel', default = True)
-    confidence     = LaunchConfiguration('confidence', default = 120)
-    LRchecktresh   = LaunchConfiguration('LRchecktresh', default = 5)
-    monoResolution = LaunchConfiguration('monoResolution',  default = '720p')
+    actions_to_launch = generate_stereo_launch_description(launch_configurations_oak_d, urdf_launch_dir, namespace, remappings)
 
-    urdf_launch = IncludeLaunchDescription(
-                            launch_description_sources.PythonLaunchDescriptionSource(
-                                    os.path.join(urdf_launch_dir, 'urdf_launch.py')),
-                            launch_arguments={'tf_prefix' : tf_prefix,
-                                              'camera_model': camera_model,
-                                              'base_frame'  : base_frame,
-                                              'parent_frame': parent_frame,
-                                              'cam_pos_x'   : cam_pos_x,
-                                              'cam_pos_y'   : cam_pos_y,
-                                              'cam_pos_z'   : cam_pos_z,
-                                              'cam_roll'    : cam_roll,
-                                              'cam_pitch'   : cam_pitch,
-                                              'cam_yaw'     : cam_yaw}.items())
-
-    stereo_node = launch_ros.actions.Node(
-            package='depthai_examples', executable='stereo_node',
-            output='screen',
-            parameters=[{'tf_prefix': tf_prefix},
-                        {'mode': mode},
-                        {'lrcheck': lrcheck},
-                        {'extended': extended},
-                        {'subpixel': subpixel},
-                        {'confidence': confidence},
-                        {'LRchecktresh': LRchecktresh},
-                        {'monoResolution': monoResolution}])
-
-
-    metric_converter_node = launch_ros.actions.ComposableNodeContainer(
-            name='container',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                # Driver itself
-                launch_ros.descriptions.ComposableNode(
-                    package='depth_image_proc',
-                    plugin='depth_image_proc::ConvertMetricNode',
-                    name='convert_metric_node',
-                    remappings=[('image_raw', '/stereo/depth'),
-                                ('camera_info', '/stereo/camera_info'),
-                                ('image', '/stereo/converted_depth')]
-                ),
-            ],
-            output='screen',)
-
-    point_cloud_node = launch_ros.actions.ComposableNodeContainer(
-            name='container2',
-            namespace='',
-            package='rclcpp_components',
-            executable='component_container',
-            composable_node_descriptions=[
-                # Driver itself
-                launch_ros.descriptions.ComposableNode(
-                    package='depth_image_proc',
-                    plugin='depth_image_proc::PointCloudXyziNode',
-                    name='point_cloud_xyzi',
-
-                    remappings=[('depth/image_rect', '/stereo/converted_depth'),
-                                ('intensity/image_rect', '/right/image_rect'),
-                                ('intensity/camera_info', '/right/camera_info'),
-                                ('points', '/stereo/points')]
-                ),
-            ],
-            output='screen',)
-
-    ld = LaunchDescription()
-
-    add_launch_arguments(ld)
-
-    ld.add_action(stereo_node)
-    ld.add_action(urdf_launch)
-
-    ld.add_action(metric_converter_node)
-    ld.add_action(point_cloud_node)
-    return ld
+    return LaunchDescription(actions_to_launch)
