f100d900b81ba51acdcac2a7698cade60c7a4acb
sagarvl96 <s.lingaraj@robast.de>
2024-06-12 15:41:33 +0200

    moved feuerplan publisher and frame broadcaster into the same node

--- src/ros2/machine_learning_tasks/image_transformer/image_transformer/transform_images.py
+++ src/ros2/machine_learning_tasks/image_transformer/image_transformer/transform_images.py
@@ -6,45 +6,50 @@ import cv2 as cv
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy
 from nav_msgs.msg import OccupancyGrid
+from tf2_ros import TransformBroadcaster
+from geometry_msgs.msg import TransformStamped
 from lightglue.lightglue import LightGlue
 from lightglue.utils import numpy_image_to_torch, rbd
 from lightglue.superpoint import SuperPoint
 
 
-class MinimalSubscriber(Node):
+class FeuerplanPublisher(Node):
 
     def __init__(self):
-        super().__init__('minimal_subscriber')
-        self.subscription_feuerplan = self.create_subscription(
-            OccupancyGrid,
-            '/global_costmap/feuerplan_image_topic',
-            self.listener_callback_feuerplan,
-            qos_profile = self.get_qos_profile())
-        self.subscription_feuerplan  
+        super().__init__('feuerplan_publisher')
 
-        self.subscription_map = self.create_subscription(
-            OccupancyGrid,
-            'map',
-            self.listener_callback_map,
-            10)
+        self.declare_parameter('feuerplan_path', '')
+
+        self.feuerplan_path = self.get_parameter('feuerplan_path').get_parameter_value().string_value 
+
+        map_qos = QoSProfile(depth=1)
+        map_qos.durability = QoSDurabilityPolicy.TRANSIENT_LOCAL
+        map_qos.reliability = rclpy.qos.ReliabilityPolicy.RELIABLE
+        map_qos.history = rclpy.qos.HistoryPolicy.KEEP_LAST
+
+        self.publisher_ = self.create_publisher(OccupancyGrid, 'global_costmap/feuerplan_image_topic', qos_profile=map_qos)
+        self.subscription_map = self.create_subscription(OccupancyGrid, 'map', self.listener_callback_map, 10)
+        self.tf_broadcaster = TransformBroadcaster(self)
+
         self.subscription_map  
-
-        self.map_msg = self.feuerplan_msg = None
-    
-    def get_qos_profile(self):
-        qos_profile = QoSProfile(depth=10)
-        qos_profile.durability = QoSDurabilityPolicy.TRANSIENT_LOCAL
-        return qos_profile    
+        self.is_message_published = False
+        self.map_msg = None
 
     def listener_callback_map(self, msg):
         self.map_msg = msg
-        self.get_logger().info('Map recieved')
-        self.transform_point_on_image()
-    
-    def listener_callback_feuerplan(self, msg):
-        self.feuerplan_msg = msg
-        self.get_logger().info('Feuerplan recieved')
-        self.transform_point_on_image()
+        if not self.is_message_published:
+            self.get_logger().info('Map recieved')
+            self.broadcast_frame_and_message()
+
+    def preprocess_image(self, feuerplan_image):
+        processed_image = cv.GaussianBlur(feuerplan_image, (3, 3), 3, 3)
+        processed_image = cv.Canny(processed_image, 50, 150, 3)
+        dilation_size = 1
+        dilation_element = cv.getStructuringElement(cv.MORPH_RECT, (2 * dilation_size + 1, 2 * dilation_size + 1))
+        processed_image = cv.dilate(processed_image, dilation_element)
+        feuerplan_image &= processed_image
+        _, preprocessed_feuerplan = cv.threshold(feuerplan_image, 150, 127, cv.THRESH_BINARY)
+        return preprocessed_feuerplan
 
     def ros_msg_to_image(self, ros_msg):
         image = np.zeros((ros_msg.info.height,ros_msg.info.width), dtype=np.uint8)
@@ -61,13 +66,18 @@ class MinimalSubscriber(Node):
                 index += 1
         return image
     
-    def get_three_best_matches(self, confidence_values, matches, keypoints1, keypoints2):
-        _, top_indices = torch.topk(confidence_values, k=3)
-        top_matches = matches[top_indices]
+    def get_best_match(self, confidence_values, matches, keypoints1, keypoints2):
+        max_value = max(confidence_values.tolist())
+        top_matches = matches[confidence_values.tolist().index(max_value)]
         points1, points2 = keypoints1[top_matches[..., 0]], keypoints2[top_matches[..., 1]]
         return points1, points2
+
+    def pixel_coord_to_world_coord(self, pixel, resolution, origin):
+        world_x = pixel[0] * resolution + origin[0]
+        world_y = pixel[1] * resolution + origin[1]
+        return (world_x, world_y)
     
-    def create_transform_matrix(self, image1, image2):
+    def find_matching_keypoints(self, image1, image2):
             # Initialize feature extractor and matcher
             device = "cpu" 
             extractor = SuperPoint(max_num_keypoints = 2048).eval().to(device)
@@ -77,27 +87,87 @@ class MinimalSubscriber(Node):
             feats1 = extractor.extract(numpy_image_to_torch(image1))
             feats2 = extractor.extract(numpy_image_to_torch(image2))
             # Match features between images
-            self.get_logger().info(f'Obtaining matches...')
+            self.get_logger().info(f'Obtaining best match...')
             matches12 = matcher({'image0': feats1, 'image1': feats2})
             # Refine features and matches
             feats1, feats2, matches12 = [rbd(x) for x in [feats1, feats2, matches12]]
             # Get the three best matches according to confidence values
-            points1, points2 = self.get_three_best_matches(matches12['scores'], matches12['matches'], feats1["keypoints"], feats2["keypoints"])
-            # Compute the transformation matrix
-            transformation_matrix = cv.getAffineTransform(np.round(points1.numpy()), np.round(points2.numpy()))
-            return transformation_matrix
+            points1, points2 = self.get_best_match(matches12['scores'], matches12['matches'], feats1["keypoints"], feats2["keypoints"])
+            return points1, points2
     
-    def transform_point_on_image(self):
-        if self.map_msg and self.feuerplan_msg:
-            image1 = self.ros_msg_to_image(self.map_msg)
-            image2 = self.ros_msg_to_image(self.feuerplan_msg)
-            transformation_matrix = self.create_transform_matrix(image1, image2)
-            self.get_logger().info(f'Created transformation matrix')
-    
+    def transform_feuerplan_origin(self, map_msg, feuerplan_image):
+        map_image = self.ros_msg_to_image(map_msg)
+        points1, points2 = self.find_matching_keypoints(map_image, feuerplan_image)
+        world1x, world1y = self.pixel_coord_to_world_coord(points1.numpy(), map_msg.info.resolution,(map_msg.info.origin.position.x, map_msg.info.origin.position.y))
+        world2x, world2y = self.pixel_coord_to_world_coord(points2.numpy(), map_msg.info.resolution,(0,0))            
+        translation_x = world1x - world2x
+        translation_y = world1y - world2y
+        return translation_x, translation_y
+
+    def publish_occupancy_grid(self, feuerplan_image, translation_x, translation_y):
+        data = feuerplan_image.flatten().tolist()
+
+        ros_image_msg = OccupancyGrid()
+        ros_image_msg.header.stamp = self.get_clock().now().to_msg()
+        ros_image_msg.header.frame_id = 'feuerplan'
+        ros_image_msg.info.map_load_time.sec = 0
+        ros_image_msg.info.map_load_time.nanosec = 0
+        ros_image_msg.info.resolution = 0.05
+        ros_image_msg.info.width = feuerplan_image.shape[1]
+        ros_image_msg.info.height = feuerplan_image.shape[0]
+        ros_image_msg.info.origin.position.x = translation_x
+        ros_image_msg.info.origin.position.y = translation_y
+        ros_image_msg.info.origin.position.z = 0.0
+        ros_image_msg.info.origin.orientation.x = 1.0
+        ros_image_msg.info.origin.orientation.y = 0.0
+        ros_image_msg.info.origin.orientation.z = 0.0
+        ros_image_msg.info.origin.orientation.w = 0.0
+        ros_image_msg.data = data
+
+        self.get_logger().info('Feuerplan published')
+        self.publisher_.publish(ros_image_msg)
+        self.is_message_published = True
+
+    def is_valid_data(self, data):
+        if not isinstance(data, (list, tuple)):
+            self.get_logger().info(f'list')
+        
+        for item in data:
+            if not isinstance(item, int):
+                self.get_logger().info(f'int')
+            elif not (-128 <= item <= 127):
+                self.get_logger().info(f'range')
+        
+        self.get_logger().info(f'true')
+
+
+    def broadcast_frame_and_message(self):
+        feuerplan_image = cv.imread(self.feuerplan_path, cv.IMREAD_GRAYSCALE)
+        preprocessed_feuerplan = self.preprocess_image(feuerplan_image)
+        translation_x, translation_y = self.transform_feuerplan_origin(self.map_msg, preprocessed_feuerplan)
+        self.get_logger().info(f'{translation_x, translation_y}')
+        transform = TransformStamped()
+
+        transform.header.stamp = self.get_clock().now().to_msg()
+        transform.header.frame_id = 'map'
+        transform.child_frame_id = 'feuerplan'
+
+        transform.transform.translation.x = translation_x
+        transform.transform.translation.y = translation_y
+        transform.transform.translation.z = self.map_msg.info.origin.position.z
+
+        transform.transform.rotation.x = self.map_msg.info.origin.orientation.x
+        transform.transform.rotation.y = self.map_msg.info.origin.orientation.y
+        transform.transform.rotation.z = self.map_msg.info.origin.orientation.z
+        transform.transform.rotation.w = self.map_msg.info.origin.orientation.w
+
+        self.tf_broadcaster.sendTransform(transform)
+        self.publish_occupancy_grid(preprocessed_feuerplan, translation_x, translation_y)
+
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_subscriber = MinimalSubscriber()
+    minimal_subscriber = FeuerplanPublisher()
 
     rclpy.spin(minimal_subscriber)
     rclpy.shutdown()
--- src/ros2/machine_learning_tasks/image_transformer/launch/image_transformer.launch.py
+++ src/ros2/machine_learning_tasks/image_transformer/launch/image_transformer.launch.py
@@ -1,14 +1,35 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
+
+    feuerplan_publisher_dir = get_package_share_directory('image_transformer')
+
+    default_feuerplan_image_path = os.path.join(feuerplan_publisher_dir, 'images','slide1.jpg')
+
+    feuerplan_path = LaunchConfiguration('feuerplan_path',  default = default_feuerplan_image_path)
+
+    declare_feuerplan_image_cmd = DeclareLaunchArgument(
+        'feuerplan_path',
+        default_value=feuerplan_path,
+        description='Path to the feuerplan image')
     
     start_image_transformer = Node(
         package='image_transformer',
-        executable='image_transformer_node'
+        executable='image_transformer_node',
+        parameters=[
+                    {'feuerplan_path': feuerplan_path}
+        ]
     )
 
     ld = LaunchDescription()
+
+    ld.add_action(declare_feuerplan_image_cmd)
 
     ld.add_action(start_image_transformer)
 
