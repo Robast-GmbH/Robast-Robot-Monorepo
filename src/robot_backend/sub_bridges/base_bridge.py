bc40748fc337ab94fbacfb546d763364afe790d1
ane-robast <a.neumann@robast.de>
2024-06-24 17:23:04 +0200

    Remove ModuleProcess from RobotBackend

--- src/robot_backend/sub_bridges/base_bridge.py
+++ src/robot_backend/sub_bridges/base_bridge.py
@@ -6,9 +6,9 @@ from thread_safe_dict import ThreadSafeDict
 class BaseBridge:
     def __init__(self, ros: Ros) -> None:
         self.context = ThreadSafeDict()
-        self.ros = ros
+        self.__ros = ros
 
-    def _create_on_msg_callback(self, topic: str) -> Callable:
+    def __create_on_msg_callback(self, topic: str) -> Callable:
         def on_msg_callback(message: dict[str, Any]):
             self.context[topic] = message
 
@@ -17,13 +17,13 @@ class BaseBridge:
     def start_subscriber(
         self, topic: str, msg_type: str, on_msg_callback: Optional[Callable] = None
     ) -> Topic:
-        listener = Topic(self.ros, topic, msg_type)
+        listener = Topic(self.__ros, topic, msg_type)
         if on_msg_callback:
             listener.subscribe(on_msg_callback)
         else:
-            listener.subscribe(self._create_on_msg_callback(topic))
+            listener.subscribe(self.__create_on_msg_callback(topic))
         return listener
 
     def start_publisher(self, topic: str, msg_type: str) -> Topic:
-        publisher = Topic(self.ros, topic, msg_type)
+        publisher = Topic(self.__ros, topic, msg_type)
         return publisher
