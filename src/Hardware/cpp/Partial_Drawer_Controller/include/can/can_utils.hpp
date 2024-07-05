fe0e56c37540b1896770231b5f449f5adca034d1
TipluJacob <j.ritterbach@tiplu.de>
2024-07-05 09:46:39 +0200

    refactor: uses `CanMsgQueue` in the CanUtils as well

--- src/Hardware/cpp/Partial_Drawer_Controller/include/can/can_utils.hpp
+++ src/Hardware/cpp/Partial_Drawer_Controller/include/can/can_utils.hpp
@@ -8,6 +8,7 @@
 
 #include "can/can_db.hpp"
 #include "can/can_message.h"
+#include "can/can_msg_queue.hpp"
 #include "debug/debug.hpp"
 
 namespace drawer_controller
@@ -16,8 +17,6 @@ namespace drawer_controller
   {
    public:
     CanUtils(std::shared_ptr<robast_can_msgs::CanDb> can_db);
-
-    void add_element_to_feedback_msg_queue(robast_can_msgs::CanMessage feedback_msg);
 
     std::optional<robast_can_msgs::CanMessage> get_element_from_feedback_msg_queue();
 
@@ -36,16 +35,7 @@ namespace drawer_controller
                                                uint8_t normed_current_position);
 
    private:
-    // Please mind: Normaly you would not built a queue yourself and use xQueue from FreeRTOS or std::queue
-    // But: std:queue did not work and just permanently threw expections that made the ESP32 reboot
-    // To use xQueue you need to use xQueueCreate to create a queue and you need to specify the size, in bytes, required
-    // to hold each item in the queue, which is not possible for the CanMessage class because it contains a vector of
-    // CanSignals which has a different length depending on the CanMessage.
-    // Therefore we built a queue with a vector, which should be fine in this case as the queue usually only contains
-    // one or two feedback messages and is rarely used. Furthermore we try to keep it as efficient as possible and try
-    // to follow what is explained here: https://youtu.be/fHNmRkzxHWs?t=2541
-    std::vector<robast_can_msgs::CanMessage> _feedback_msg_queue;
-    uint8_t _head_of_feedback_msg_queue;
+    std::unique_ptr<CanMsgQueue> _feedback_can_msg_queue;
 
     std::shared_ptr<robast_can_msgs::CanDb> _can_db;
 
