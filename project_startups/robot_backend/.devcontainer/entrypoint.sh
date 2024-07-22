3e2447c966c4f9a2764bb58e396849087151ff2a
sagarvl96 <s.lingaraj@robast.de>
2024-07-22 09:49:06 +0200

    removed unrelated changes from PR

--- project_startups/robot_backend/.devcontainer/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt-get update && sudo apt-get upgrade -y
-rosdep update
-rosdep install --from-paths /workspace/src/ --ignore-src -r -y
-pip3 install -r /workspace/src/robot_backend/requirements.txt
