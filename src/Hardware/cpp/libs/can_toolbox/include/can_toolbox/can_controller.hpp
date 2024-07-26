2c13c7bd80e33ffb7b8e4b38c8d707a8a4a25974
TipluJacob <j.ritterbach@tiplu.de>
2024-07-26 15:20:10 +0200

    fix: increases `_RX_QUEUE_SIZE` for CanController

--- src/Hardware/cpp/libs/can_toolbox/include/can_toolbox/can_controller.hpp
+++ src/Hardware/cpp/libs/can_toolbox/include/can_toolbox/can_controller.hpp
@@ -16,7 +16,7 @@ namespace drawer_controller
 {
   class CanController
   {
-  public:
+   public:
     CanController(const uint32_t module_id,
                   const std::shared_ptr<robast_can_msgs::CanDb> can_db,
                   const gpio_num_t twai_tx_pin,
@@ -28,13 +28,13 @@ namespace drawer_controller
 
     void send_can_message(robast_can_msgs::CanMessage can_msg);
 
-  private:
+   private:
     const uint32_t _module_id;
     const std::shared_ptr<robast_can_msgs::CanDb> _can_db;
     const gpio_num_t _twai_tx_pin;
     const gpio_num_t _twai_rx_pin;
 
-    static const uint8_t _RX_QUEUE_SIZE = 10; // Receive Queue size
+    static const uint8_t _RX_QUEUE_SIZE = 150;   // Receive Queue size
   };
-} // namespace drawer_controller
-#endif // DRAWER_CONTROLLER_CAN_CONTROLLER_HPP
+}   // namespace drawer_controller
+#endif   // DRAWER_CONTROLLER_CAN_CONTROLLER_HPP
