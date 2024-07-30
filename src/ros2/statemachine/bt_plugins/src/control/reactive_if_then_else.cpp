547f8b1378541897a27701a676556014131143ac
TAlscher <t.alscher@tiplu.de>
2024-07-30 11:25:42 +0200

    wrong namespace for reactive if then else

--- src/ros2/statemachine/bt_plugins/src/control/reactive_if_then_else.cpp
+++ src/ros2/statemachine/bt_plugins/src/control/reactive_if_then_else.cpp
@@ -1,41 +1,41 @@
-#include "behaviortree_cpp/controls/if_then_else_node.h"
+#include "bt_plugins/control/reactive_if_then_else.hpp"
 
-namespace BT
+namespace statemachine
 {
-  IfThenElseNode::IfThenElseNode(const std::string &name)
+  ReactiveIfThenElseNode::ReactiveIfThenElseNode(const std::string &name)
       : ControlNode::ControlNode(name, {})
   {
     setRegistrationID("ReactiveIfThenElse");
   }
 
-  void IfThenElseNode::halt()
+  void ReactiveIfThenElseNode::halt()
   {
     ControlNode::halt();
   }
 
-  NodeStatus IfThenElseNode::tick()
+  BT::NodeStatus ReactiveIfThenElseNode::tick()
   {
     const size_t children_count = children_nodes_.size();
 
     if (children_count != 2 && children_count != 3)
     {
-      throw std::logic_error("IfThenElseNode must have either 2 or 3 children");
+      throw std::logic_error("ReactiveIfThenElseNode must have either 2 or 3 children");
     }
 
-    setStatus(NodeStatus::RUNNING);
+    setStatus(BT::NodeStatus::RUNNING);
 
     uint8_t child_idx = 0;
-    NodeStatus condition_status = children_nodes_[0]->executeTick();
+    BT::NodeStatus condition_status = children_nodes_[0]->executeTick();
 
-    if (condition_status == NodeStatus::RUNNING)
+    if (condition_status == BT::NodeStatus::RUNNING)
     {
       return condition_status;
     }
-    else if (condition_status == NodeStatus::SUCCESS)
+    else if (condition_status == BT::NodeStatus::SUCCESS)
     {
       child_idx = 1;
     }
-    else if (condition_status == NodeStatus::FAILURE)
+    else if (condition_status == BT::NodeStatus::FAILURE)
     {
       if (children_count == 3)
       {
@@ -50,10 +50,10 @@ namespace BT
     // not an else
     if (child_idx > 0)
     {
-      NodeStatus status = children_nodes_[child_idx]->executeTick();
-      if (status == NodeStatus::RUNNING)
+      BT::NodeStatus status = children_nodes_[child_idx]->executeTick();
+      if (status == BT::NodeStatus::RUNNING)
       {
-        return NodeStatus::RUNNING;
+        return BT::NodeStatus::RUNNING;
       }
       else
       {
