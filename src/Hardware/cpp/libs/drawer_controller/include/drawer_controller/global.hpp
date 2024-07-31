cf8a07c55b611f74bb762444cd7c02cf90c41a08
TipluJacob <j.ritterbach@tiplu.de>
2024-07-31 17:08:21 +0200

    refactor: adds global.hpp to reduce doubled code between partial and normal drawer

---
+++ src/Hardware/cpp/libs/drawer_controller/include/drawer_controller/global.hpp
@@ -0,0 +1,83 @@
+#ifndef DRAWER_CONTROLLER_GLOBAL_HPP
+#define DRAWER_CONTROLLER_GLOBAL_HPP
+
+#include <memory>
+
+#include "can_toolbox/can_controller.hpp"
+#include "debug/debug.hpp"
+#include "drawer/electrical_drawer.hpp"
+#include "gpio/gpio_wrapper_pca9535.hpp"
+#include "interfaces/i_gpio_wrapper.hpp"
+#include "led/led_strip.hpp"
+#include "motor/motor_monitor_configs.hpp"
+#include "peripherals/gpio_defines.hpp"
+#include "switch/switch.hpp"
+#include "utils/config_manager.hpp"
+#include "utils/data_mapper.hpp"
+#include "utils/queue.hpp"
+
+#define SWITCH_PRESSED_THRESHOLD 0.9
+#define SWITCH_WEIGHT_NEW_VALUES 0.2
+
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+SemaphoreHandle_t can_queue_mutex = NULL;
+
+using drawer_ptr = std::shared_ptr<drawer_controller::IDrawer>;
+
+std::shared_ptr<robast_can_msgs::CanDb> can_db;
+
+std::shared_ptr<drawer_controller::IGpioWrapper> gpio_wrapper;
+
+std::shared_ptr<drawer_controller::ElectricalDrawerLock> drawer_lock;
+
+std::shared_ptr<drawer_controller::ElectricalDrawer> drawer;
+
+std::shared_ptr<drawer_controller::ElectricalDrawerConfigs> drawer_configs;
+std::shared_ptr<drawer_controller::EncoderConfigs> encoder_configs;
+std::shared_ptr<drawer_controller::MotorConfigs> motor_configs;
+std::shared_ptr<drawer_controller::MotorMonitorConfigs> motor_monitor_configs;
+
+stepper_motor::StepperPinIdConfig stepper_1_pin_id_config = {
+  .stepper_enn_tmc2209_pin_id = STEPPER_1_ENN_TMC2209_PIN_ID,
+  .stepper_stdby_tmc2209_pin_id = STEPPER_1_STDBY_TMC2209_PIN_ID,
+  .stepper_spread_pin_id = STEPPER_1_SPREAD_PIN_ID,
+  .stepper_dir_pin_id = STEPPER_1_DIR_PIN_ID,
+  .stepper_diag_pin_id = STEPPER_1_DIAG_PIN_ID,
+  .stepper_index_pin_id = STEPPER_1_INDEX_PIN_ID,
+  .stepper_step_pin_id = STEPPER_1_STEP_PIN_ID};
+
+std::shared_ptr<drawer_controller::Switch> endstop_switch;
+
+std::unique_ptr<drawer_controller::DataMapper> data_mapper;
+
+std::unique_ptr<drawer_controller::ConfigManager> config_manager;
+
+std::unique_ptr<drawer_controller::CanController> can_controller;
+
+// shared resource, so we need a mutex for this
+std::unique_ptr<drawer_controller::Queue<robast_can_msgs::CanMessage>> can_msg_queue;
+
+void receive_can_msg_task_loop(void* pvParameters)
+{
+  for (;;)
+  {
+    std::optional<robast_can_msgs::CanMessage> received_message = can_controller->handle_receiving_can_msg();
+    if (received_message.has_value())
+    {
+      if (xSemaphoreTake(can_queue_mutex, pdMS_TO_TICKS(500)) == pdTRUE)
+      {
+        debug_println("[Main]: Received CAN message and adding it to the queue.");
+        can_msg_queue->add_element_to_queue(received_message.value());
+        xSemaphoreGive(can_queue_mutex);
+      }
+      else
+      {
+        Serial.println("[Main]: Error: Could not take the mutex. This should not occur.");
+      }
+    }
+  }
+}
+
+#endif   // DRAWER_CONTROLLER_GLOBAL_HPP
--- src/Hardware/cpp/projects/Partial_Drawer_Controller/src/main.cpp
+++ src/Hardware/cpp/projects/Partial_Drawer_Controller/src/main.cpp
@@ -1,18 +1,7 @@
 #include <memory>
 
-#include "can_toolbox/can_controller.hpp"
-#include "debug/debug.hpp"
-#include "drawer/electrical_drawer.hpp"
-#include "gpio/gpio_wrapper_pca9535.hpp"
-#include "interfaces/i_gpio_wrapper.hpp"
-#include "led/led_strip.hpp"
+#include "drawer_controller/global.hpp"
 #include "lock/tray_manager.hpp"
-#include "motor/motor_monitor_configs.hpp"
-#include "peripherals/gpio_defines.hpp"
-#include "switch/switch.hpp"
-#include "utils/config_manager.hpp"
-#include "utils/data_mapper.hpp"
-#include "utils/queue.hpp"
 
 #define MODULE_ID               6
 #define LOCK_ID                 0
@@ -21,51 +10,9 @@
 
 #define NUM_OF_LEDS 21
 
-#define SWITCH_PRESSED_THRESHOLD 0.9
-#define SWITCH_WEIGHT_NEW_VALUES 0.2
-
-TaskHandle_t Task1;
-TaskHandle_t Task2;
-
-SemaphoreHandle_t can_queue_mutex = NULL;
-
-using drawer_ptr = std::shared_ptr<drawer_controller::IDrawer>;
-
-std::shared_ptr<robast_can_msgs::CanDb> can_db;
-
-std::shared_ptr<drawer_controller::IGpioWrapper> gpio_wrapper;
-
-std::shared_ptr<drawer_controller::ElectricalDrawer> drawer;
-
-std::shared_ptr<drawer_controller::ElectricalDrawerConfigs> drawer_configs;
-std::shared_ptr<drawer_controller::EncoderConfigs> encoder_configs;
-std::shared_ptr<drawer_controller::MotorConfigs> motor_configs;
-std::shared_ptr<drawer_controller::MotorMonitorConfigs> motor_monitor_configs;
-
-stepper_motor::StepperPinIdConfig stepper_1_pin_id_config = {
-  .stepper_enn_tmc2209_pin_id = STEPPER_1_ENN_TMC2209_PIN_ID,
-  .stepper_stdby_tmc2209_pin_id = STEPPER_1_STDBY_TMC2209_PIN_ID,
-  .stepper_spread_pin_id = STEPPER_1_SPREAD_PIN_ID,
-  .stepper_dir_pin_id = STEPPER_1_DIR_PIN_ID,
-  .stepper_diag_pin_id = STEPPER_1_DIAG_PIN_ID,
-  .stepper_index_pin_id = STEPPER_1_INDEX_PIN_ID,
-  .stepper_step_pin_id = STEPPER_1_STEP_PIN_ID,
-  .port_expander_ninterrupt_pin_id = PE_0_NINTERRUPT_PIN_ID};
-
-std::shared_ptr<drawer_controller::Switch> endstop_switch;
-
-std::shared_ptr<partial_drawer_controller::TrayManager> tray_manager;
-
 std::unique_ptr<drawer_controller::LedStrip<LED_PIXEL_PIN, NUM_OF_LEDS>> led_strip;
 
-std::unique_ptr<drawer_controller::DataMapper> data_mapper;
-
-std::unique_ptr<drawer_controller::ConfigManager> config_manager;
-
-std::unique_ptr<drawer_controller::CanController> can_controller;
-
-// shared resource, so we need a mutex for this
-std::unique_ptr<drawer_controller::Queue<robast_can_msgs::CanMessage>> can_msg_queue;
+std::shared_ptr<partial_drawer_controller::TrayManager> tray_manager;
 
 /**********************************************************************************************************************
  * The program flow is organized as follows:
@@ -74,26 +21,6 @@ std::unique_ptr<drawer_controller::Queue<robast_can_msgs::CanMessage>> can_msg_q
  *    - receive_can_msg_task_loop() is responsible for receiving CAN messages and adding them to the queue
  *    - process_can_msgs_task_loop() is responsible for processing the CAN messages from the queue
  **********************************************************************************************************************/
-
-void receive_can_msg_task_loop(void* pvParameters)
-{
-  for (;;)
-  {
-    std::optional<robast_can_msgs::CanMessage> received_message = can_controller->handle_receiving_can_msg();
-    if (received_message.has_value())
-    {
-      if (xSemaphoreTake(can_queue_mutex, pdMS_TO_TICKS(500)) == pdTRUE)
-      {
-        can_msg_queue->add_element_to_queue(received_message.value());
-        xSemaphoreGive(can_queue_mutex);
-      }
-      else
-      {
-        Serial.println("[Main]: Error: Could not take the mutex. This should not occur.");
-      }
-    }
-  }
-}
 
 void process_can_msgs_task_loop(void* pvParameters)
 {
@@ -181,6 +108,11 @@ void process_can_msgs_task_loop(void* pvParameters)
     }
 
     tray_manager->update_states();
+
+    // end timer to track time for one loop iteration
+    // unsigned long end_time = millis();
+    // unsigned long duration = end_time - start_time;
+    // debug_printf("[Main]: Duration of one loop iteration: %lu ms. Free heap: %u\n", duration, ESP.getFreeHeap());
   }
 }
 
