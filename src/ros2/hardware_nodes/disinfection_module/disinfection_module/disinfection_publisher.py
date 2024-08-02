4953e89ae99590d24441a662108e7c72b964781a
TipluJacob <j.ritterbach@tiplu.de>
2024-08-02 15:55:29 +0200

    chore: fixes formatting

--- src/ros2/hardware_nodes/disinfection_module/disinfection_module/disinfection_publisher.py
+++ src/ros2/hardware_nodes/disinfection_module/disinfection_module/disinfection_publisher.py
@@ -9,13 +9,13 @@ import Jetson.GPIO as GPIO
 DISINFECTION_PIN = 7
 BOUNCE_TIME_IN_MS = 300
 
+
 class DisinfectionPublisher(Node):
 
     def __init__(self):
-        super().__init__('disinfection_publisher')
-        self.__publisher = self.create_publisher(Time, 'disinfection_triggered', 10)
+        super().__init__("disinfection_publisher")
+        self.__publisher = self.create_publisher(Time, "disinfection_triggered", 10)
         self.setup_gpio()
-
 
     def pub_disinfection_triggered(self, channel):
         msg = Time()
@@ -23,16 +23,22 @@ class DisinfectionPublisher(Node):
         current_time = time.time()
         msg.sec = int(current_time)
         msg.nanosec = int((current_time - msg.sec) * 1e9)
-        self.get_logger().info('Publishing disinfection triggered at: %d.%d' % (msg.sec, msg.nanosec))
+        self.get_logger().info(
+            "Publishing disinfection triggered at: %d.%d" % (msg.sec, msg.nanosec)
+        )
         self.__publisher.publish(msg)
 
-
     def setup_gpio(self):
-        self.get_logger().info('Setting up GPIO')
-        self.get_logger().info('GPIO.JETSON_INFO = %s' % GPIO.JETSON_INFO)
+        self.get_logger().info("Setting up GPIO")
+        self.get_logger().info("GPIO.JETSON_INFO = %s" % GPIO.JETSON_INFO)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(DISINFECTION_PIN, GPIO.IN)
-        GPIO.add_event_detect(DISINFECTION_PIN, GPIO.FALLING, callback=self.pub_disinfection_triggered, bouncetime=BOUNCE_TIME_IN_MS)
+        GPIO.add_event_detect(
+            DISINFECTION_PIN,
+            GPIO.FALLING,
+            callback=self.pub_disinfection_triggered,
+            bouncetime=BOUNCE_TIME_IN_MS,
+        )
 
 
 def main(args=None):
@@ -48,5 +54,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/ros2/hardware_nodes/disinfection_module/launch/disinfection_module_launch.py
+++ src/ros2/hardware_nodes/disinfection_module/launch/disinfection_module_launch.py
@@ -1,11 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='disinfection_module',
-            executable='disinfection_publisher',
-            name='disinfection_publisher'
-        ),
-    ])
+    return LaunchDescription(
+        [
+            Node(
+                package="disinfection_module",
+                executable="disinfection_publisher",
+                name="disinfection_publisher",
+            ),
+        ]
+    )
