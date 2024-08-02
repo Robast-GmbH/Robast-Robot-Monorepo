5785654e504c1f3726467e980a93adb77d0901be
TipluJacob <j.ritterbach@tiplu.de>
2024-08-02 16:49:36 +0200

    refactor: moves `disinfection_module` project

--- src/ros2/nvidia_hardware_nodes/disinfection_module/disinfection_module/disinfection_publisher.py
+++ src/ros2/nvidia_hardware_nodes/disinfection_module/disinfection_module/disinfection_publisher.py
@@ -1,5 +1,4 @@
 import rclpy
-import time
 from rclpy.node import Node
 
 from builtin_interfaces.msg import Time
@@ -11,32 +10,26 @@ BOUNCE_TIME_IN_MS = 300
 
 
 class DisinfectionPublisher(Node):
-
     def __init__(self):
         super().__init__("disinfection_publisher")
         self.__publisher = self.create_publisher(Time, "disinfection_triggered", 10)
-        self.setup_gpio()
+        self.__setup_gpio()
 
-    def pub_disinfection_triggered(self, channel):
-        msg = Time()
-        # fill time message with current timestamp
-        current_time = time.time()
-        msg.sec = int(current_time)
-        msg.nanosec = int((current_time - msg.sec) * 1e9)
+    def __pub_disinfection_triggered(self, channel: int):
+        msg = self.get_clock().now().to_msg()
         self.get_logger().info(
-            "Publishing disinfection triggered at: %d.%d" % (msg.sec, msg.nanosec)
+            f"Disinfection triggered at {msg.sec}.{msg.nanosec} seconds."
         )
         self.__publisher.publish(msg)
 
-    def setup_gpio(self):
+    def __setup_gpio(self):
         self.get_logger().info("Setting up GPIO")
-        self.get_logger().info("GPIO.JETSON_INFO = %s" % GPIO.JETSON_INFO)
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(DISINFECTION_PIN, GPIO.IN)
         GPIO.add_event_detect(
             DISINFECTION_PIN,
             GPIO.FALLING,
-            callback=self.pub_disinfection_triggered,
+            callback=self.__pub_disinfection_triggered,
             bouncetime=BOUNCE_TIME_IN_MS,
         )
 
@@ -51,6 +44,7 @@ def main(args=None):
     GPIO.cleanup()
 
     disinfection_publisher.destroy_node()
+
     rclpy.shutdown()
 
 
