ab47f79e0a4a8e73f746d742261576c66e57c5bd
TipluJacob <j.ritterbach@tiplu.de>
2024-08-05 10:00:11 +0200

    docs: adds comment to explain `BOUNCE_TIME_IN_MS` value

--- src/ros2/nvidia_hardware_nodes/disinfection_module/disinfection_module/disinfection_publisher.py
+++ src/ros2/nvidia_hardware_nodes/disinfection_module/disinfection_module/disinfection_publisher.py
@@ -6,7 +6,7 @@ from builtin_interfaces.msg import Time
 import Jetson.GPIO as GPIO
 
 SENSE_DISINFECTION_SWITCH_PIN = 7
-BOUNCE_TIME_IN_MS = 300
+BOUNCE_TIME_IN_MS = 300 # the switch of the disinfection module closes for 300ms when triggered
 
 
 class DisinfectionPublisher(Node):
