eabfd987d909e233e03f08c8406ea43e3477f46e
TipluJacob <78360006+TipluJacob@users.noreply.github.com>
2024-08-07 15:40:21 +0200

    fix: false detections for disinfection module (#169)
    We could observe some false detections happening for the disinfection module. This PR should fix that.

--- src/ros2/nvidia_hardware_nodes/disinfection_module/launch/disinfection_module_launch.py
+++ src/ros2/nvidia_hardware_nodes/disinfection_module/launch/disinfection_module_launch.py
@@ -9,6 +9,13 @@ def generate_launch_description():
                 package="disinfection_module",
                 executable="disinfection_publisher",
                 name="disinfection_publisher",
+                parameters=[
+                    {"read_counter_limit": 15},
+                    {"switch_threshold": 0.35},
+                    {"is_pulldown": True},
+                    {"timer_period_in_sec": 0.01},
+                    {"num_of_first_readings_to_ignore": 2},
+                ],
             ),
         ]
     )
