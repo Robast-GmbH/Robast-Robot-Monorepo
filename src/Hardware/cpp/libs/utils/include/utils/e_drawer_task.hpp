f56a4a8b42b22a840bf45713d4d733d74af59aee
TipluJacob <j.ritterbach@tiplu.de>
2024-08-08 17:46:32 +0200

    refactor: renames struct args suffix to `arg`

--- src/Hardware/cpp/libs/utils/include/utils/e_drawer_task.hpp
+++ src/Hardware/cpp/libs/utils/include/utils/e_drawer_task.hpp
@@ -18,26 +18,26 @@ namespace utils
     }
 
     // Parameterized Constructor
-    EDrawerTask(const uint8_t target_position_input,
-                const uint8_t target_speed_input,
-                const uint8_t stall_guard_value_input)
-        : target_position(target_position_input),
-          target_speed(target_speed_input),
-          stall_guard_value(stall_guard_value_input),
+    EDrawerTask(const uint8_t target_position_arg,
+                const uint8_t target_speed_arg,
+                const uint8_t stall_guard_value_arg)
+        : target_position(target_position_arg),
+          target_speed(target_speed_arg),
+          stall_guard_value(stall_guard_value_arg),
           is_homing(false),
           use_acceleration_ramp(true)
     {
     }
 
     // Parameterized Constructor
-    EDrawerTask(const uint8_t target_position_input,
-                const uint8_t target_speed_input,
-                const uint8_t stall_guard_value_input,
+    EDrawerTask(const uint8_t target_position_arg,
+                const uint8_t target_speed_arg,
+                const uint8_t stall_guard_value_arg,
                 const bool is_homing_input,
                 const bool use_acceleration_ramp_input)
-        : target_position(target_position_input),
-          target_speed(target_speed_input),
-          stall_guard_value(stall_guard_value_input),
+        : target_position(target_position_arg),
+          target_speed(target_speed_arg),
+          stall_guard_value(stall_guard_value_arg),
           is_homing(is_homing_input),
           use_acceleration_ramp(use_acceleration_ramp_input)
     {
