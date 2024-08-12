79fa8789cbc28669f6e030ea0a4bae0c75887d15
TipluJacob <j.ritterbach@tiplu.de>
2024-08-12 09:23:20 +0200

    chore: removes unused enum

--- libs/can/include/can/can_helper.hpp
+++ libs/can/include/can/can_helper.hpp
@@ -14,20 +14,6 @@
 
 namespace robast_can_msgs
 {
-
-  enum can_baudrate_usb_to_can_interface
-  {
-    can_baud_10kbps = 0,
-    can_baud_20kbps = 1,
-    can_baud_50kbps = 2,
-    can_baud_100kbps = 3,
-    can_baud_125kbps = 4,
-    can_baud_250kbps = 5,
-    can_baud_500kbps = 6,
-    can_baud_800kbps = 7,
-    can_baud_1000kbps = 8
-  };
-
   /**
    * @brief Decodes CAN message from a 8 Byte bitstream
    *
