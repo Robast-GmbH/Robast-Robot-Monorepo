42268c73c39318b5ac91c94838e85f4aca195a16
ane-robast <a.neumann@robast.de>
2024-08-13 17:43:21 +0200

    Add return types to DisinfectionModuleBridge methods

--- src/robot_backend/sub_bridges/disinfection_module_bridge.py
+++ src/robot_backend/sub_bridges/disinfection_module_bridge.py
@@ -1,7 +1,7 @@
 from sub_bridges.base_bridge import BaseBridge
 from roslibpy import Ros
 import threading
-from typing import Any
+from typing import Any, Dict
 
 
 disinfection_triggered = threading.Event()
@@ -16,10 +16,10 @@ class DisinfectionModuleBridge(BaseBridge):
             on_msg_callback=self.__on_disinfection_triggered,
         )
 
-    def wait_for_disinfection_triggered(self, time_out: int):
+    def wait_for_disinfection_triggered(self, time_out: int) -> Dict[str, str]:
         disinfection_triggered.clear()
         was_successful = disinfection_triggered.wait(timeout=float(time_out))
         return {"status": "success" if was_successful else "failure"}
 
-    def __on_disinfection_triggered(self, msg: dict[str, Any]):
+    def __on_disinfection_triggered(self, msg: dict[str, Any]) -> None:
         disinfection_triggered.set()
