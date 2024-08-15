68dbd80c5de84544d8b413ee0a260d58546503fd
ane-robast <a.neumann@robast.de>
2024-08-15 12:27:36 +0200

    Apply backend changes for nfc timeout

--- src/robot_backend/robot_api.py
+++ src/robot_backend/robot_api.py
@@ -127,13 +127,15 @@ def get_nfc_tag():
 
 
 @app.post("/write_nfc_tag", tags=["NFC"])
-def write_nfc_tag(nfc_tag_id: str):
-    return ros_bridge.nfc_bridge.write_nfc_tag(nfc_tag_id=nfc_tag_id)
+def write_nfc_tag(nfc_tag_id: str, timeout_in_s: int):
+    return ros_bridge.nfc_bridge.write_nfc_tag(
+        nfc_tag_id=nfc_tag_id, timeout_in_s=timeout_in_s
+    )
 
 
 @app.get("/read_nfc_tag", tags=["NFC"])
-def read_nfc_tag():
-    return {"nfc_tag": ros_bridge.nfc_bridge.read_nfc_tag()}
+def read_nfc_tag(timeout_in_s: int):
+    return ros_bridge.nfc_bridge.read_nfc_tag(timeout_in_s=timeout_in_s)
 
 
 """
--- src/robot_backend/sub_bridges/nfc_bridge.py
+++ src/robot_backend/sub_bridges/nfc_bridge.py
@@ -7,24 +7,29 @@ class NfcBridge(BaseBridge):
     def __init__(self, ros: Ros) -> None:
         super().__init__(ros)
 
-    def write_nfc_tag(self, nfc_tag_id: str) -> dict[str, str]:
+    def write_nfc_tag(self, nfc_tag_id: str, timeout_in_s: int) -> dict[str, str]:
+        if not self.__validate_uint16(timeout_in_s):
+            return {"status": "failure", "message": "timeout_in_s must be a uint16"}
         raw_nfc_tag_id = nfc_tag_id.replace("-", "").lower()
         self.start_service(
             name="/write_nfc",
             service_type="communication_interfaces/srv/WriteNfcTag",
-            values={"nfc_tag_id": raw_nfc_tag_id},
+            values={"nfc_tag_id": raw_nfc_tag_id, "timeout_in_s": timeout_in_s},
         )
         return {
             "status": "success" if self.context["/write_nfc"]["success"] else "failure"
         }
 
-    def read_nfc_tag(self) -> dict[str, str]:
+    def read_nfc_tag(self, timeout_in_s: int) -> dict[str, str]:
+        if not self.__validate_uint16(timeout_in_s):
+            return {"status": "failure", "message": "timeout_in_s must be a uint16"}
         self.start_service(
             name="/read_nfc",
             service_type="communication_interfaces/srv/ReadNfcTag",
+            values={"timeout_in_s": timeout_in_s},
         ),
         Timer(5, self.__clear_nfc).start()
-        return self.get_nfc_tag()
+        return {"nfc_tag": self.get_nfc_tag()}
 
     def get_nfc_tag(self) -> dict[str, str]:
         try:
@@ -45,6 +50,9 @@ class NfcBridge(BaseBridge):
         uuid_string = f"{hex_string[:8]}-{hex_string[8:12]}-{hex_string[12:16]}-{hex_string[16:20]}-{hex_string[20:]}"
 
         return uuid_string
+
+    def __validate_uint16(self, value: int) -> bool:
+        return 0 <= value <= 65535
 
     def __clear_nfc(self) -> None:
         self.context["/read_nfc"].clear()
