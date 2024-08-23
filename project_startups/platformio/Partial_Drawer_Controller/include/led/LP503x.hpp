7a710cd502b9a9955ba0a4398291230b75cc3e6a
TipluJacob <j.ritterbach@tiplu.de>
2024-08-23 12:22:43 +0200

    refactor: renames constants in LP503x.hpp

--- project_startups/platformio/Partial_Drawer_Controller/include/led/LP503x.hpp
+++ project_startups/platformio/Partial_Drawer_Controller/include/led/LP503x.hpp
@@ -20,55 +20,55 @@ namespace lp503x
     constexpr byte BANK_B_COLOR = 0x06;      // Contains the BANK B color value
     constexpr byte BANK_C_COLOR = 0x07;      // Contains the BANK C color value
 
-    constexpr byte LED0_BRIGHTNESS = 0x08;    // Contains the brightness level for LED 0
-    constexpr byte LED1_BRIGHTNESS = 0x09;    // Contains the brightness level for LED 1
-    constexpr byte LED2_BRIGHTNESS = 0x0A;    // Contains the brightness level for LED 2
-    constexpr byte LED3_BRIGHTNESS = 0x0B;    // Contains the brightness level for LED 3
-    constexpr byte LED4_BRIGHTNESS = 0x0C;    // Contains the brightness level for LED 4
-    constexpr byte LED5_BRIGHTNESS = 0x0D;    // Contains the brightness level for LED 5
-    constexpr byte LED6_BRIGHTNESS = 0x0E;    // Contains the brightness level for LED 6
-    constexpr byte LED7_BRIGHTNESS = 0x0F;    // Contains the brightness level for LED 7
-    constexpr byte LED8_BRIGHTNESS = 0x10;    // Contains the brightness level for LED 8
-    constexpr byte LED9_BRIGHTNESS = 0x11;    // Contains the brightness level for LED 9
-    constexpr byte LED10_BRIGHTNESS = 0x12;   // Contains the brightness level for LED 10
-    constexpr byte LED11_BRIGHTNESS = 0x13;   // Contains the brightness level for LED 11
+    constexpr byte LED_0_BRIGHTNESS = 0x08;
+    constexpr byte LED_1_BRIGHTNESS = 0x09;
+    constexpr byte LED_2_BRIGHTNESS = 0x0A;
+    constexpr byte LED_3_BRIGHTNESS = 0x0B;
+    constexpr byte LED_4_BRIGHTNESS = 0x0C;
+    constexpr byte LED_5_BRIGHTNESS = 0x0D;
+    constexpr byte LED_6_BRIGHTNESS = 0x0E;
+    constexpr byte LED_7_BRIGHTNESS = 0x0F;
+    constexpr byte LED_8_BRIGHTNESS = 0x10;
+    constexpr byte LED_9_BRIGHTNESS = 0x11;
+    constexpr byte LED_10_BRIGHTNESS = 0x12;
+    constexpr byte LED_11_BRIGHTNESS = 0x13;
 
-    constexpr byte OUT0_COLOR = 0x14;    // Contains the color value for output 0
-    constexpr byte OUT1_COLOR = 0x15;    // Contains the color value for output 1
-    constexpr byte OUT2_COLOR = 0x16;    // Contains the color value for output 2
-    constexpr byte OUT3_COLOR = 0x17;    // Contains the color value for output 3
-    constexpr byte OUT4_COLOR = 0x18;    // Contains the color value for output 4
-    constexpr byte OUT5_COLOR = 0x19;    // Contains the color value for output 5
-    constexpr byte OUT6_COLOR = 0x1A;    // Contains the color value for output 6
-    constexpr byte OUT7_COLOR = 0x1B;    // Contains the color value for output 7
-    constexpr byte OUT8_COLOR = 0x1C;    // Contains the color value for output 8
-    constexpr byte OUT9_COLOR = 0x1D;    // Contains the color value for output 9
-    constexpr byte OUT10_COLOR = 0x1E;   // Contains the color value for output 10
-    constexpr byte OUT11_COLOR = 0x1F;   // Contains the color value for output 11
-    constexpr byte OUT12_COLOR = 0x20;   // Contains the color value for output 12
-    constexpr byte OUT13_COLOR = 0x21;   // Contains the color value for output 13
-    constexpr byte OUT14_COLOR = 0x22;   // Contains the color value for output 14
-    constexpr byte OUT15_COLOR = 0x23;   // Contains the color value for output 15
-    constexpr byte OUT16_COLOR = 0x24;   // Contains the color value for output 16
-    constexpr byte OUT17_COLOR = 0x25;   // Contains the color value for output 17
-    constexpr byte OUT18_COLOR = 0x26;   // Contains the color value for output 18
-    constexpr byte OUT19_COLOR = 0x27;   // Contains the color value for output 19
-    constexpr byte OUT20_COLOR = 0x28;   // Contains the color value for output 20
-    constexpr byte OUT21_COLOR = 0x29;   // Contains the color value for output 21
-    constexpr byte OUT22_COLOR = 0x2A;   // Contains the color value for output 22
-    constexpr byte OUT23_COLOR = 0x2B;   // Contains the color value for output 23
-    constexpr byte OUT24_COLOR = 0x2C;   // Contains the color value for output 24
-    constexpr byte OUT25_COLOR = 0x2D;   // Contains the color value for output 25
-    constexpr byte OUT26_COLOR = 0x2E;   // Contains the color value for output 26
-    constexpr byte OUT27_COLOR = 0x2F;   // Contains the color value for output 27
-    constexpr byte OUT28_COLOR = 0x30;   // Contains the color value for output 28
-    constexpr byte OUT29_COLOR = 0x31;   // Contains the color value for output 29
-    constexpr byte OUT30_COLOR = 0x32;   // Contains the color value for output 30
-    constexpr byte OUT31_COLOR = 0x33;   // Contains the color value for output 31
-    constexpr byte OUT32_COLOR = 0x34;   // Contains the color value for output 32
-    constexpr byte OUT33_COLOR = 0x35;   // Contains the color value for output 33
-    constexpr byte OUT34_COLOR = 0x36;   // Contains the color value for output 34
-    constexpr byte OUT35_COLOR = 0x37;   // Contains the color value for output 35
+    constexpr byte OUT_0_COLOR = 0x14;
+    constexpr byte OUT_1_COLOR = 0x15;
+    constexpr byte OUT_2_COLOR = 0x16;
+    constexpr byte OUT_3_COLOR = 0x17;
+    constexpr byte OUT_4_COLOR = 0x18;
+    constexpr byte OUT_5_COLOR = 0x19;
+    constexpr byte OUT_6_COLOR = 0x1A;
+    constexpr byte OUT_7_COLOR = 0x1B;
+    constexpr byte OUT_8_COLOR = 0x1C;
+    constexpr byte OUT_9_COLOR = 0x1D;
+    constexpr byte OUT_10_COLOR = 0x1E;
+    constexpr byte OUT_11_COLOR = 0x1F;
+    constexpr byte OUT_12_COLOR = 0x20;
+    constexpr byte OUT_13_COLOR = 0x21;
+    constexpr byte OUT_14_COLOR = 0x22;
+    constexpr byte OUT_15_COLOR = 0x23;
+    constexpr byte OUT_16_COLOR = 0x24;
+    constexpr byte OUT_17_COLOR = 0x25;
+    constexpr byte OUT_18_COLOR = 0x26;
+    constexpr byte OUT_19_COLOR = 0x27;
+    constexpr byte OUT_20_COLOR = 0x28;
+    constexpr byte OUT_21_COLOR = 0x29;
+    constexpr byte OUT_22_COLOR = 0x2A;
+    constexpr byte OUT_23_COLOR = 0x2B;
+    constexpr byte OUT_24_COLOR = 0x2C;
+    constexpr byte OUT_25_COLOR = 0x2D;
+    constexpr byte OUT_26_COLOR = 0x2E;
+    constexpr byte OUT_27_COLOR = 0x2F;
+    constexpr byte OUT_28_COLOR = 0x30;
+    constexpr byte OUT_29_COLOR = 0x31;
+    constexpr byte OUT_30_COLOR = 0x32;
+    constexpr byte OUT_31_COLOR = 0x33;
+    constexpr byte OUT_32_COLOR = 0x34;
+    constexpr byte OUT_33_COLOR = 0x35;
+    constexpr byte OUT_34_COLOR = 0x36;
+    constexpr byte OUT_35_COLOR = 0x37;
 
     constexpr byte RESET_REGISTERS = 0x38;   // Resets all the registers to their default values
   }   // namespace register_address
@@ -109,13 +109,13 @@ namespace lp503x
 
     bool set_led_brigthness(byte led_number, byte brightness)
     {
-      byte register_address = lp503x::register_address::LED0_BRIGHTNESS + led_number;
+      byte register_address = lp503x::register_address::LED_0_BRIGHTNESS + led_number;
       return twi_write(register_address, brightness);
     }
 
     bool set_led_output_color_by_number(byte led_number, byte color)
     {
-      byte register_address = lp503x::register_address::OUT0_COLOR + led_number;
+      byte register_address = lp503x::register_address::OUT_0_COLOR + led_number;
       return twi_write(register_address, color);
     }
 
