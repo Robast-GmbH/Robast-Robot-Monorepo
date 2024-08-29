eec46d847f88dfe77f83a4b7157dbd1ed9db7803
ane-robast <a.neumann@robast.de>
2024-08-29 14:18:43 +0200

    Add custom scroll behaviour back in

--- src/web/frontend/robot_apps_flutter/apps/robot_frontend/lib/main.dart
+++ src/web/frontend/robot_apps_flutter/apps/robot_frontend/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -9,6 +10,15 @@ import 'package:robot_frontend/models/provider/robot_provider.dart';
 import 'package:robot_frontend/models/provider/task_provider.dart';
 import 'package:robot_frontend/models/provider/user_provider.dart';
 import 'package:robot_frontend/pages/config_page.dart';
+
+class CustomScrollBehavior extends MaterialScrollBehavior {
+  // Override behavior methods and getters like dragDevices
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+      };
+}
 
 void main() {
   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
@@ -48,6 +58,7 @@ class RobotFrontend extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       navigatorKey: navigatorKey,
+      scrollBehavior: CustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(useMaterial3: true).copyWith(
         textTheme: GoogleFonts.montserratTextTheme(),
