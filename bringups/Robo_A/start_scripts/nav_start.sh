47ed64ec02e51de7cae1c379cf168dba01384422
TAlscher <t.alscher@tiplu.de>
2024-09-01 20:54:29 +0200

    autostart pose saver

--- bringups/Robo_A/start_scripts/nav_start.sh
+++ bringups/Robo_A/start_scripts/nav_start.sh
@@ -9,4 +9,5 @@ ros2 launch nav_bringup nav_without_localization_launch.py &
 ros2 launch nav_bringup mask_server_launch.py &
 ros2 launch nav_action_node nav_action_node.launch.py &
 ros2 launch robot_pose_publisher robot_pose_publisher.launch.py &
-ros2 launch clicked_point_to_nav_pose clicked_point_to_nav_pose_launch.py
+ros2 launch clicked_point_to_nav_pose clicked_point_to_nav_pose_launch.py &
+ros2 run save_pose save_map --ros-args -p file_path:="/logs/last_pose.yaml"
