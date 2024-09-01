e042d44421dd568385cdd94ab6f5d3ee18b08716
TAlscher <t.alscher@tiplu.de>
2024-09-01 20:49:22 +0200

    frame und filepath konfigurierbar

--- src/ros2/navigation/robast_utils/save_pose/src/save_map_pose.cpp
+++ src/ros2/navigation/robast_utils/save_pose/src/save_map_pose.cpp
@@ -21,13 +21,15 @@ class SaveMapPose : public rclcpp::Node
     _tf->setUsingDedicatedThread(true);
     _transform_listener = std::make_shared<tf2_ros::TransformListener>(*_tf, this, false);
     _timer = this->create_wall_timer(1s, std::bind(&SaveMapPose::timer_callback, this));
+    _base_frame = this->declare_parameter<std::string>("base_frame", "robot/base_link");
+    _file_path = this->declare_parameter<std::string>("file_path", "/workspace/last_pose.yaml");
   }
 
  private:
   void timer_callback()
   {
     geometry_msgs::msg::PoseStamped global_pose;
-    if (nav2_util::getCurrentPose(global_pose, *_tf, "map", "base_link", 0.2))
+    if (nav2_util::getCurrentPose(global_pose, *_tf, "map", _base_frame, 0.2))
     {
       RCLCPP_INFO(this->get_logger(),
                   "Save map pose: %f %f %f",
@@ -44,12 +46,11 @@ class SaveMapPose : public rclcpp::Node
       tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
 
       YAML::Node config;
-      std::string file_path = "/workspace/last_pose.yaml";
 
       // Check if the file exists
-      if (std::filesystem::exists(file_path))
+      if (std::filesystem::exists(_file_path))
       {
-        config = YAML::LoadFile(file_path);
+        config = YAML::LoadFile(_file_path);
       }
       else
       {
@@ -61,13 +62,15 @@ class SaveMapPose : public rclcpp::Node
       config["map_pose"]["position"]["z"] = global_pose.pose.position.z;
       config["map_pose"]["orientation"]["yaw"] = yaw;
 
-      std::ofstream fout(file_path);
+      std::ofstream fout(_file_path);
       fout << config;
     }
   }
   std::shared_ptr<tf2_ros::Buffer> _tf;
   std::shared_ptr<tf2_ros::TransformListener> _transform_listener;
   rclcpp::TimerBase::SharedPtr _timer;
+  std::string _file_path = "/workspace/last_pose.yaml";
+  std::string _base_frame = "base_link";
 };
 
 int main(int argc, char* argv[])
