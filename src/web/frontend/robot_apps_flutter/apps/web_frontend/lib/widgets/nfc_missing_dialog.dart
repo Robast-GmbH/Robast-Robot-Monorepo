efc69297d47ea9ab4c452272909cdf5cb4499982
ane-robast <a.neumann@robast.de>
2024-09-03 16:17:15 +0200

    Adjust nfc_mssing_dialog UI for web-frontend

--- src/web/frontend/robot_apps_flutter/apps/web_frontend/lib/widgets/nfc_missing_dialog.dart
+++ src/web/frontend/robot_apps_flutter/apps/web_frontend/lib/widgets/nfc_missing_dialog.dart
@@ -9,23 +9,22 @@ class NfcMissingDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      backgroundColor: WebColors.secondaryBackground,
       title: Text(
         'Nutzer ${identifiers.length != 1 ? 'haben' : 'hat'} keine NFC-ID',
-        style: const TextStyle(color: WebColors.primaryText, fontSize: 28),
+        style: const TextStyle(color: WebColors.primaryText, fontSize: 24),
       ),
       content: Text(
         identifiers.length != 1
             ? '${identifiers.sublist(0, identifiers.length - 1).join(', ')} und ${identifiers.last} haben keine NFC-ID. Bitte wenden Sie sich für die Zuweisung einer NFC-ID an einen Admin.'
             : 'Der ${identifiers.first} hat keine NFC-ID. Bitte wenden Sie sich für die Zuweisung einer NFC-ID an einen Admin.',
-        style: const TextStyle(color: WebColors.secondaryText, fontSize: 24),
+        style: const TextStyle(color: WebColors.secondaryText, fontSize: 18),
       ),
       actions: [
         TextButton(
           onPressed: () {
             Navigator.of(context).pop();
           },
-          child: const Text('OK', style: TextStyle(color: WebColors.secondaryText, fontSize: 24)),
+          child: const Text('OK', style: TextStyle(color: WebColors.secondaryText, fontSize: 18)),
         ),
       ],
     );
