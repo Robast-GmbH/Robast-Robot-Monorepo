38c7a7e3fc4dfa7db15e0833f8236e775f3f5952
ane-robast <a.neumann@robast.de>
2024-09-11 09:44:25 +0200

    Make requested changes

--- src/robot_backend/sub_bridges/module_bridge.py
+++ src/robot_backend/sub_bridges/module_bridge.py
@@ -7,40 +7,38 @@ class ModuleBridge(BaseBridge):
     DRAWER_ADDRESS_MSG = "communication_interfaces/msg/DrawerAddress"
 
     MODULE_UNIQUE_ID_LENGTH = 16
-    ELECTRIC_DRAWER_MODULE_ID_PREFIX = 0b00010100
-    PARTIAL_DRAWER_MODULE_ID_PREFIX = 0b00010101
-    MODULE_TYPES = [
-        0b00010001,  # MANUAL_DRAWER_10x40
-        0b00010010,  # MANUAL_DRAWER_20x40
-        0b00010011,  # MANUAL_DRAWER_30x40
-        0b00010100,  # E_DRAWER_10x40
-        0b00010101,  # PARTIAL_DRAWER_10x40x8
-        0b00010110,  # DINNER_TRAYS
-        0b00010111,  # SURGERY_TOOLS
-    ]
+    MODULE_TYPES = {
+        "MANUAL_DRAWER_10x40": 0b00010001,
+        "MANUAL_DRAWER_20x40": 0b00010010,
+        "MANUAL_DRAWER_30x40": 0b00010011,
+        "E_DRAWER_10x40": 0b00010100,
+        "PARTIAL_DRAWER_10x40x8": 0b00010101,
+        "DINNER_TRAYS": 0b00010110,
+        "SURGERY_TOOLS": 0b00010111,
+    }
 
     def __init__(self, ros: Ros) -> None:
         super().__init__(ros)
         self.__drawer_open_subscriber = self.start_subscriber(
             "/bt_drawer_open",
-            self.DRAWER_STATUS_MSG,
+            ModuleBridge.DRAWER_STATUS_MSG,
             on_msg_callback=self.__on_submodule_is_open_msg_callback,
         )
         self.__drawer_tree_publisher = self.start_publisher(
             "/trigger_drawer_tree",
-            self.DRAWER_ADDRESS_MSG,
+            ModuleBridge.DRAWER_ADDRESS_MSG,
         )
         self.__electric_drawer_tree_publisher = self.start_publisher(
             "/trigger_electric_drawer_tree",
-            self.DRAWER_ADDRESS_MSG,
+            ModuleBridge.DRAWER_ADDRESS_MSG,
         )
         self.__partial_drawer_tree_publisher = self.start_publisher(
             "/trigger_partial_drawer_tree",
-            self.DRAWER_ADDRESS_MSG,
+            ModuleBridge.DRAWER_ADDRESS_MSG,
         )
         self.__close_drawer_publisher = self.start_publisher(
             "/close_drawer",
-            self.DRAWER_ADDRESS_MSG,
+            ModuleBridge.DRAWER_ADDRESS_MSG,
         )
 
     def open_submodule(self, module_id: int, submodule_id: int) -> bool:
@@ -49,14 +47,15 @@ class ModuleBridge(BaseBridge):
             return False
 
         if self.__is_module_type(
-            module_type=self.ELECTRIC_DRAWER_MODULE_ID_PREFIX,
+            module_type=ModuleBridge.MODULE_TYPES["E_DRAWER_10x40"],
             module_id=module_id,
         ):
             self.__electric_drawer_tree_publisher.publish(
                 {"module_id": module_id, "drawer_id": submodule_id}
             )
         elif self.__is_module_type(
-            module_type=self.PARTIAL_DRAWER_MODULE_ID_PREFIX, module_id=module_id
+            module_type=ModuleBridge.MODULE_TYPES["PARTIAL_DRAWER_10x40x8"],
+            module_id=module_id,
         ):
             self.__partial_drawer_tree_publisher.publish(
                 {"module_id": module_id, "drawer_id": submodule_id}
@@ -72,10 +71,11 @@ class ModuleBridge(BaseBridge):
             print("Invalid module id")
             return False
         if self.__is_module_type(
-            module_type=self.ELECTRIC_DRAWER_MODULE_ID_PREFIX,
+            module_type=ModuleBridge.MODULE_TYPES["E_DRAWER_10x40"],
             module_id=module_id,
         ) or self.__is_module_type(
-            module_type=self.PARTIAL_DRAWER_MODULE_ID_PREFIX, module_id=module_id
+            module_type=ModuleBridge.MODULE_TYPES["PARTIAL_DRAWER_10x40x8"],
+            module_id=module_id,
         ):
             self.__close_drawer_publisher.publish(
                 {"module_id": module_id, "drawer_id": submodule_id}
@@ -98,7 +98,9 @@ class ModuleBridge(BaseBridge):
         self.context[f"{str(module_id)}_{str(submodule_id)}"] = {"is_open": is_open}
 
     def __is_module_type(self, module_type: int, module_id: int) -> bool:
-        return module_id >> self.MODULE_UNIQUE_ID_LENGTH == module_type
+        return module_id >> ModuleBridge.MODULE_UNIQUE_ID_LENGTH == module_type
 
     def __validate_module_id(self, module_id: int) -> bool:
-        return (module_id >> self.MODULE_UNIQUE_ID_LENGTH) in self.MODULE_TYPES
+        return (
+            module_id >> ModuleBridge.MODULE_UNIQUE_ID_LENGTH
+        ) in ModuleBridge.MODULE_TYPES.values()
--- src/web/frontend/robot_apps_flutter/apps/robot_frontend/lib/widgets/dialogs/nfc_missing_dialog.dart
+++ src/web/frontend/robot_apps_flutter/apps/robot_frontend/lib/widgets/dialogs/nfc_missing_dialog.dart
@@ -16,8 +16,8 @@ class NfcMissingDialog extends StatelessWidget {
       ),
       content: Text(
         identifiers.length != 1
-            ? '${identifiers.sublist(0, identifiers.length - 1).join(', ')} und ${identifiers.last} haben keine NFC-ID. Bitte wenden Sie sich für die Zuweisung einer NFC-ID an einen Admin.'
-            : 'Der ${identifiers.first} hat keine NFC-ID. Bitte wenden Sie sich für die Zuweisung einer NFC-ID an einen Admin.',
+            ? '${identifiers.sublist(0, identifiers.length - 1).join(', ')} und ${identifiers.last} haben keine eingerichtete Authentifizierung. Bitte wenden Sie sich für die Einrichtung an einen Admin.'
+            : 'Der ${identifiers.first} hat keine eingerichtete Authentifizierung. Bitte wenden Sie sich für die Einrichtung an einen Admin.',
         style: const TextStyle(color: RobotColors.secondaryText, fontSize: 24),
       ),
       actions: [
