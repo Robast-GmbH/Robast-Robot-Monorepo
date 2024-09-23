8b3fe65664ac68b24ccc1d266a4579b1bd4b30a6
TAlscher <t.alscher@tiplu.de>
2024-09-23 10:14:02 +0200

    bt adjustments for new robot

--- src/ros2/statemachine/bt_plugins/src/action/import_environment_led_color_action.cpp
+++ src/ros2/statemachine/bt_plugins/src/action/import_environment_led_color_action.cpp
@@ -15,26 +15,25 @@ namespace bt_plugins
 
   BT::NodeStatus ImportEnvironmentLEDColor::tick()
   {
-    auto getEnvAsInt = [](const char *varName, int defaultValue) -> int
+    auto getEnvAsInt = [](const char *varName, int defaultValue) -> uint8_t
     {
       const char *value = std::getenv(varName);
       if (value)
       {
-        std::istringstream iss(value);
-        int intValue;
-        if (iss >> intValue)
+        int intValue = std::stoi(value);
+        if (intValue >= 0 && intValue <= 255)
         {
-          return intValue;
+          return static_cast<uint8_t>(intValue);
         }
       }
       return defaultValue;
     };
 
     // Get the values from the environment variables
-    int red = getEnvAsInt("RED", 0);
-    int green = getEnvAsInt("GREEN", 0);
-    int blue = getEnvAsInt("BLUE", 0);
-    int brightness = getEnvAsInt("BRIGHTNESS", 0);
+    uint8_t red = getEnvAsInt("RED", 0);
+    uint8_t green = getEnvAsInt("GREEN", 0);
+    uint8_t blue = getEnvAsInt("BLUE", 0);
+    uint8_t brightness = getEnvAsInt("BRIGHTNESS", 0);
 
     setOutput("blue", blue);
     setOutput("red", red);
