bf42af5cda194384d44c395a58e8e30f3c2a6692
ane-robast <a.neumann@robast.de>
2024-09-30 15:37:34 +0200

    Refactor load_frontend script

--- bringups/jetson/start_scripts/load_frontend.sh
+++ bringups/jetson/start_scripts/load_frontend.sh
@@ -1,7 +1,10 @@
 bash ping_and_wait.sh
 docker pull ghcr.io/robast-gmbh/monorepo/robot_frontend:release
+
+CONTAINER_NAME="frontend_copier"
 CURRENT_DIR=$(dirname "$(realpath "$0")")
-docker run -it --name frontend_copier -v $CURRENT_DIR/:/app ghcr.io/robast-gmbh/monorepo/robot_frontend:release sh -c "cp -r /robot_frontend /app" 
-docker container rm frontend_copier
+TARGET_DIR="/app"
+docker run -it --name $CONTAINER_NAME -v $CURRENT_DIR/:$TARGET_DIR ghcr.io/robast-gmbh/monorepo/robot_frontend:release sh -c "cp -r /robot_frontend $TARGET_DIR" 
+docker container rm $CONTAINER_NAME
 
 robot_frontend/robot_frontend
