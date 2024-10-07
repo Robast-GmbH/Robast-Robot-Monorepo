4cc1705fdf00475491bcf4e718da9d84faf32d42
sagarvl96 <s.lingaraj@robast.de>
2024-10-07 16:17:18 +0200

    changed according to PR comments

--- src/ros2/navigation/self_mapping/include/self_mapping/nav_action.hpp
+++ src/ros2/navigation/self_mapping/include/self_mapping/nav_action.hpp
@@ -4,41 +4,51 @@
 #include <rclcpp/rclcpp.hpp>
 #include <nav2_msgs/action/navigate_to_pose.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
-#include <geometry_msgs/msg/pose.hpp>
 #include <nav_msgs/msg/occupancy_grid.hpp>
-#include <action_msgs/msg/goal_status_array.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 #include <visualization_msgs/msg/marker_array.hpp>
 
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <unordered_set>
+#include <boost/functional/hash.hpp>
 
 class NavAction : public rclcpp::Node
 {
 public:
   NavAction();
-
   void navigate_to_frontier();
   
 private:
-  void send_goal(const geometry_msgs::msg::PoseStamped &goal);
-  geometry_msgs::msg::PoseStamped choose_closest_point();
   void costmap_callback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
   void find_new_frontiers();
   void send_to_a_frontier();
   void update_current_position();  
   void publish_frontier_markers();
-  rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr action_client_;
-  rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_sub_;
-  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_pub_;
-  tf2_ros::Buffer tf_buffer_;
-  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-  nav_msgs::msg::OccupancyGrid costmap_;
-  std::vector<geometry_msgs::msg::PoseStamped> frontiers_;
-  std::vector<geometry_msgs::msg::PoseStamped> blacklist_;
-  geometry_msgs::msg::PoseStamped current_position_;
-  bool is_navigating_;
+  void send_goal(const geometry_msgs::msg::PoseStamped &goal);
+  void handle_goal_result(const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::WrappedResult &result, const geometry_msgs::msg::PoseStamped &goal);
+  nav2_msgs::action::NavigateToPose::Goal create_goal_message(const geometry_msgs::msg::PoseStamped &goal);
+  geometry_msgs::msg::PoseStamped choose_closest_point();
+  geometry_msgs::msg::PoseStamped index_to_pose(int index, int width, float resolution);
+  bool is_cell_near_obstacle(int index, int width, int height, int occupied_threshold);
+  bool is_free_cell(int index,int free_threshold_min, int free_threshold_max);
+  bool is_point_close(const geometry_msgs::msg::PoseStamped &point, float threshold);
+  bool is_in_blacklist(const geometry_msgs::msg::PoseStamped &point, const std::unordered_set<std::pair<float, float>, boost::hash<std::pair<float, float>>> &blacklist_set);
+
+  std::string _map_frame;
+  std::string _base_frame;
+  std::string _costmap_topic;
+  rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr _action_client;
+  rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr _costmap_sub;
+  rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr _marker_pub;
+  tf2_ros::Buffer _tf_buffer;
+  std::shared_ptr<tf2_ros::TransformListener> _tf_listener;
+  nav_msgs::msg::OccupancyGrid _costmap;
+  std::vector<geometry_msgs::msg::PoseStamped> _frontiers;
+  std::vector<geometry_msgs::msg::PoseStamped> _blacklist;
+  geometry_msgs::msg::PoseStamped _current_position;
+  bool _is_navigating;
 };
 
 #endif  // SELF_MAPPING__NAV_ACTION_HPP_
--- src/ros2/navigation/self_mapping/src/nav_action.cpp
+++ src/ros2/navigation/self_mapping/src/nav_action.cpp
@@ -1,57 +1,60 @@
-#include "self_mapping/nav_action.hpp"
-#include <tf2_ros/transform_listener.h>
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp" 
 #include <random>
 #include <cmath>
+#include "self_mapping/nav_action.hpp"
 
-NavAction::NavAction() : Node("self_mapping_node"), tf_buffer_(get_clock()), is_navigating_(false)
+NavAction::NavAction() : Node("self_mapping_node"), _tf_buffer(get_clock()), _is_navigating(false)
 {
-  action_client_ = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(this, "navigate_to_pose");
+  //parameters
+  this->declare_parameter<std::string>("map_frame", "map");
+  this->declare_parameter<std::string>("costmap_topic", "global_costmap/costmap");
+  this->declare_parameter<std::string>("base_frame", "base_link");
 
-  rclcpp::QoS costmap_qos_profile(1);  
-  costmap_qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
-  costmap_qos_profile.durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL); 
+  this->get_parameter("map_frame", _map_frame);
+  this->get_parameter("costmap_topic", _costmap_topic);
+  this->get_parameter("base_frame", _base_frame);
+  _action_client = rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(this, "navigate_to_pose");
 
-  costmap_sub_ = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
-      "global_costmap/costmap", costmap_qos_profile, std::bind(&NavAction::costmap_callback, this, std::placeholders::_1));
+  rclcpp::QoS _costmap_qos_profile(1);  
+  _costmap_qos_profile.reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE);
+  _costmap_qos_profile.durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL); 
+
+  _costmap_sub = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
+      _costmap_topic, _costmap_qos_profile, std::bind(&NavAction::costmap_callback, this, std::placeholders::_1));
 
   RCLCPP_DEBUG(this->get_logger(), "NavAction Node Initialized");
   
-  tf_listener_ = std::make_shared<tf2_ros::TransformListener>(tf_buffer_);
-  marker_pub_ = this->create_publisher<visualization_msgs::msg::MarkerArray>("visualization_marker_array", 10);
+  _tf_listener = std::make_shared<tf2_ros::TransformListener>(_tf_buffer);
+  _marker_pub = this->create_publisher<visualization_msgs::msg::MarkerArray>("visualization_marker_array", 10);
+}
+
+nav2_msgs::action::NavigateToPose::Goal NavAction::create_goal_message(const geometry_msgs::msg::PoseStamped &goal)
+{
+  auto goal_msg = nav2_msgs::action::NavigateToPose::Goal();
+  goal_msg.pose.pose.position = goal.pose.position;
+  goal_msg.pose.pose.orientation.w = 1.;
+  goal_msg.pose.header.frame_id = _map_frame;
+  goal_msg.pose.header.stamp = this->now();
+  goal_msg.behavior_tree = "/workspace/src/navigation/nav_bringup/behavior_trees/humble/navigate_w_recovery_and_replanning_only_if_path_becomes_invalid.xml";
+  return goal_msg;
 }
 
 void NavAction::send_goal(const geometry_msgs::msg::PoseStamped &goal)
 {
-  if (!action_client_->wait_for_action_server()) {
+  if (!_action_client->wait_for_action_server()) {
     RCLCPP_ERROR(this->get_logger(), "Action server not available!");
     return ;
   }
 
-  auto goal_msg = nav2_msgs::action::NavigateToPose::Goal();
-  goal_msg.pose.pose.position = goal.pose.position;
-  goal_msg.pose.pose.orientation.w = 1.;
-  goal_msg.pose.header.frame_id = "map";
-  goal_msg.pose.header.stamp = this->now();
-  goal_msg.behavior_tree = "/workspace/src/navigation/nav_bringup/behavior_trees/humble/navigate_w_recovery_and_replanning_only_if_path_becomes_invalid.xml";
+  auto goal_msg = create_goal_message(goal);
 
   RCLCPP_DEBUG(this->get_logger(), "Sending goal to point: (%f, %f)", goal.pose.position.x, goal.pose.position.y);
   auto send_goal_options = rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions();
 
   send_goal_options.result_callback = [this, goal](const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::WrappedResult &result) {
-    if (result.code == rclcpp_action::ResultCode::SUCCEEDED) {
-        RCLCPP_INFO(this->get_logger(), "Goal succeeded!");
-    } else if (result.code == rclcpp_action::ResultCode::ABORTED) {
-        RCLCPP_WARN(this->get_logger(), "Goal was aborted");
-        blacklist_.push_back(goal);
-    } else if (result.code == rclcpp_action::ResultCode::CANCELED) {
-        RCLCPP_WARN(this->get_logger(), "Goal was canceled");
-        blacklist_.push_back(goal);
-    }
+    handle_goal_result(result, goal);
     update_current_position();
-    is_navigating_ = false;
-    
-    if (!frontiers_.empty()) {
+    _is_navigating = false;
+    if (!_frontiers.empty()) {
             RCLCPP_INFO(this->get_logger(), "Sending to a frontier...");
             auto next_goal = choose_closest_point();
             send_goal(next_goal);
@@ -61,15 +64,37 @@ void NavAction::send_goal(const geometry_msgs::msg::PoseStamped &goal)
 
     };
 
-    action_client_->async_send_goal(goal_msg, send_goal_options);
-    is_navigating_ = true;
+    _action_client->async_send_goal(goal_msg, send_goal_options);
+    _is_navigating = true;
 }
+
+void NavAction::handle_goal_result(const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::WrappedResult &result, const geometry_msgs::msg::PoseStamped &goal)
+{
+  if (result.code == rclcpp_action::ResultCode::SUCCEEDED) {
+    RCLCPP_INFO(this->get_logger(), "Goal succeeded!");
+  } else if (result.code == rclcpp_action::ResultCode::ABORTED) {
+    RCLCPP_WARN(this->get_logger(), "Goal was aborted");
+    _blacklist.push_back(goal);
+  } else if (result.code == rclcpp_action::ResultCode::CANCELED) {
+    RCLCPP_WARN(this->get_logger(), "Goal was canceled");
+    _blacklist.push_back(goal);
+  }
+}
+
+void NavAction::navigate_to_frontier()
+{
+  if (_frontiers.empty()) {
+    RCLCPP_INFO(this->get_logger(), "No frontiers found. Waiting for new costmap...");
+  } else {
+    send_to_a_frontier();
+  }
+} 
 
 void NavAction::costmap_callback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg)
 {
   RCLCPP_DEBUG(this->get_logger(), "Costmap callback triggered");
-  costmap_ = *msg;
-  if (frontiers_.empty()) {
+  _costmap = *msg;
+  if (_frontiers.empty()) {
     find_new_frontiers();
   }
   else {
@@ -77,73 +102,82 @@ void NavAction::costmap_callback(const nav_msgs::msg::OccupancyGrid::SharedPtr m
   }
 }
 
-void NavAction::find_new_frontiers()
-{
-  RCLCPP_INFO(this->get_logger(), "Finding new frontiers...");
-  frontiers_.clear();
-  int width = costmap_.info.width;
-  int height = costmap_.info.height;
-  float resolution = costmap_.info.resolution;
+void NavAction::find_new_frontiers()  
+{  
+  RCLCPP_INFO(this->get_logger(), "Finding new frontiers...");  
+  _frontiers.clear();  
+  int width = _costmap.info.width;  
+  int height = _costmap.info.height;  
+  float resolution = _costmap.info.resolution;  
   int free_threshold_min = 0;    
   int free_threshold_max = 50; 
-  int occupied_threshold = 80;
+  int occupied_threshold = 80;  
 
-  for (int i = 0; i < width * height; ++i) {
-    
-     if (costmap_.data[i] >= free_threshold_min && costmap_.data[i] <= free_threshold_max) {
-      bool is_near_obstacle = false;
+  // Convert blacklist to a set for faster lookups  
+  std::unordered_set<std::pair<float, float>, boost::hash<std::pair<float, float>>> blacklist_set;  
+  for (const auto &blacklisted_point : _blacklist) {  
+    blacklist_set.emplace(blacklisted_point.pose.position.x, blacklisted_point.pose.position.y);  
+  }  
 
-      for (int dx = -1; dx <= 1; ++dx) {
-        for (int dy = -1; dy <= 1; ++dy) {
-          int neighbor_x = (i % width) + dx;
-          int neighbor_y = (i / width) + dy;
+  for (int i = 0; i < width * height; ++i) {  
+    if (is_free_cell(i, free_threshold_min, free_threshold_max) && !is_cell_near_obstacle(i, width, height, occupied_threshold)) {  
+      auto point = index_to_pose(i, width, resolution);  
+      if (!is_in_blacklist(point, blacklist_set)) {  
+        _frontiers.push_back(point);  
+      }  
+    }  
+  }  
 
-          if (neighbor_x >= 0 && neighbor_x < width && neighbor_y >= 0 && neighbor_y < height) {
-            int neighbor_idx = neighbor_x + neighbor_y * width;
-            if (costmap_.data[neighbor_idx] >= occupied_threshold) {
-              is_near_obstacle = true;
-              break;
-            }
-          }
-        }
-        if (is_near_obstacle) {
-          break;
-        }
-      }
+  RCLCPP_DEBUG(this->get_logger(), "Number of frontiers found: %zu", _frontiers.size());  
+}  
 
-      if (!is_near_obstacle) {
-        geometry_msgs::msg::PoseStamped point;
-        point.pose.position.x = (i % width) * resolution + costmap_.info.origin.position.x;
-        point.pose.position.y = (i / width) * resolution + costmap_.info.origin.position.y;
-        bool in_blacklist = false;
-        for (const auto &blacklisted_point : blacklist_) {
-                    if (blacklisted_point.pose.position.x == point.pose.position.x && 
-                        blacklisted_point.pose.position.y == point.pose.position.y) {
-                        in_blacklist = true;
-                        break;
-                    }
-        }
-        if (!in_blacklist) {
-          frontiers_.push_back(point);
-        }
-      }
-    }
-  }
+geometry_msgs::msg::PoseStamped NavAction::index_to_pose(int index, int width, float resolution)  
+{  
+  geometry_msgs::msg::PoseStamped point;  
+  point.pose.position.x = (index % width) * resolution + _costmap.info.origin.position.x;  
+  point.pose.position.y = (index / width) * resolution + _costmap.info.origin.position.y;  
+  return point;  
+}  
 
-  RCLCPP_DEBUG(this->get_logger(), "Number of frontiers found: %zu", frontiers_.size());
+bool NavAction::is_in_blacklist(const geometry_msgs::msg::PoseStamped &point, const std::unordered_set<std::pair<float, float>, boost::hash<std::pair<float, float>>> &blacklist_set)  
+{  
+  return blacklist_set.find({point.pose.position.x, point.pose.position.y}) != blacklist_set.end();  
+} 
 
+bool NavAction::is_cell_near_obstacle(int index, int width, int height, int occupied_threshold)
+{
+  int x = index % width;  
+  int y = index / width;  
+  for (int dx = -1; dx <= 1; ++dx) {  
+    for (int dy = -1; dy <= 1; ++dy) {  
+      int neighbor_x = x + dx;  
+      int neighbor_y = y + dy;  
+      if (neighbor_x >= 0 && neighbor_x < width && neighbor_y >= 0 && neighbor_y < height) {  
+        int neighbor_idx = neighbor_x + neighbor_y * width;  
+        if (_costmap.data[neighbor_idx] >= occupied_threshold) {  
+          return true;  
+        }  
+      }  
+    }  
+  }  
+  return false;  
+}
+
+bool NavAction::is_free_cell(int index, int free_threshold_min, int free_threshold_max)
+{
+  return _costmap.data[index] >= free_threshold_min && _costmap.data[index] <= free_threshold_max;
 }
 
 void NavAction::send_to_a_frontier(){
-  if (!frontiers_.empty() && !is_navigating_){
+  if (!_frontiers.empty() && !_is_navigating){
     RCLCPP_INFO(this->get_logger(), "Sending to a frontier...");
     auto goal = choose_closest_point();
     send_goal(goal);
   }
-  else if (is_navigating_) {
+  else if (_is_navigating) {
     RCLCPP_DEBUG(this->get_logger(), "Waiting for current navigation to finish...");
   }
-  else if (frontiers_.empty()) {
+  else if (_frontiers.empty()) {
     RCLCPP_INFO(this->get_logger(), "No frontiers found. Waiting for new costmap...");
   }
 }
@@ -155,9 +189,9 @@ geometry_msgs::msg::PoseStamped NavAction::choose_closest_point()
   float significant_distance_threshold = 4.0;
   bool found_valid_goal = false;
 
-  for (const auto &point : frontiers_) {
-    float distance = std::hypot(point.pose.position.x - current_position_.pose.position.x,
-                                point.pose.position.y - current_position_.pose.position.y);
+  for (const auto &point : _frontiers) {
+    float distance = std::hypot(point.pose.position.x - _current_position.pose.position.x,
+                                point.pose.position.y - _current_position.pose.position.y);
 
     if (distance < significant_distance_threshold) {
       RCLCPP_DEBUG(this->get_logger(), "Skipping close frontier at (%f, %f) - Distance: %f", 
@@ -184,58 +218,58 @@ void NavAction::update_current_position()
   geometry_msgs::msg::TransformStamped transform_stamped;
 
   try {
-    transform_stamped = tf_buffer_.lookupTransform("map", "base_link", tf2::TimePointZero);
+    transform_stamped = _tf_buffer.lookupTransform(_map_frame, _base_frame, tf2::TimePointZero);
   } catch (tf2::TransformException &ex) {
-    RCLCPP_WARN(this->get_logger(), "Could not transform 'map' to 'base_link': %s", ex.what());
+    RCLCPP_WARN(this->get_logger(), "Could not transform map frame to base frame: %s", ex.what());
     return;
   }
 
-  current_position_.pose.position.x = transform_stamped.transform.translation.x;
-  current_position_.pose.position.y = transform_stamped.transform.translation.y;
-  current_position_.pose.position.z = transform_stamped.transform.translation.z;
+  _current_position.pose.position.x = transform_stamped.transform.translation.x;
+  _current_position.pose.position.y = transform_stamped.transform.translation.y;
+  _current_position.pose.position.z = transform_stamped.transform.translation.z;
 
-  current_position_.pose.orientation = transform_stamped.transform.rotation;
+  _current_position.pose.orientation = transform_stamped.transform.rotation;
 
-  RCLCPP_INFO(this->get_logger(), "Current Position updated from transform: x: %f, y: %f", 
-              current_position_.pose.position.x, current_position_.pose.position.y);
+  RCLCPP_DEBUG(this->get_logger(), "Current Position updated from transform: x: %f, y: %f", 
+              _current_position.pose.position.x, _current_position.pose.position.y);
 }
 
 void NavAction::publish_frontier_markers()
 {
-  visualization_msgs::msg::MarkerArray marker_array;
+  visualization_msgs::msg::MarkerArray _marker_array;
   
   int id = 0; 
 
-  for (const auto &frontier : frontiers_) {
-    visualization_msgs::msg::Marker marker;
+  for (const auto &frontier : _frontiers) {
+    visualization_msgs::msg::Marker _marker;
 
-    marker.header.frame_id = "map";  
-    marker.header.stamp = this->get_clock()->now();
-    marker.ns = "frontiers";
-    marker.id = id++;  
-    marker.type = visualization_msgs::msg::Marker::SPHERE;  
-    marker.action = visualization_msgs::msg::Marker::ADD;
+    _marker.header.frame_id = _map_frame;  
+    _marker.header.stamp = this->get_clock()->now();
+    _marker.ns = "frontiers";
+    _marker.id = id++;  
+    _marker.type = visualization_msgs::msg::Marker::SPHERE;  
+    _marker.action = visualization_msgs::msg::Marker::ADD;
 
-    marker.pose.position.x = frontier.pose.position.x;
-    marker.pose.position.y = frontier.pose.position.y;
-    marker.pose.position.z = 0.0; 
+    _marker.pose.position.x = frontier.pose.position.x;
+    _marker.pose.position.y = frontier.pose.position.y;
+    _marker.pose.position.z = 0.0; 
 
-    marker.scale.x = 0.2;
-    marker.scale.y = 0.2;
-    marker.scale.z = 0.2;
+    _marker.scale.x = 0.2;
+    _marker.scale.y = 0.2;
+    _marker.scale.z = 0.2;
 
-    marker.color.r = 0.0f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
+    _marker.color.r = 0.0f;
+    _marker.color.g = 1.0f;
+    _marker.color.b = 0.0f;
+    _marker.color.a = 1.0;
     
-    marker.lifetime = rclcpp::Duration::from_seconds(0);  
+    _marker.lifetime = rclcpp::Duration::from_seconds(0);  
 
-    marker_array.markers.push_back(marker);
+    _marker_array.markers.push_back(_marker);
   }
 
   // Publish the markers
-  marker_pub_->publish(marker_array);
+  _marker_pub->publish(_marker_array);
 
-  RCLCPP_DEBUG(this->get_logger(), "Published %zu frontier markers.", marker_array.markers.size());
+  RCLCPP_DEBUG(this->get_logger(), "Published %zu frontier markers.", _marker_array.markers.size());
 }
