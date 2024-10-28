bd8765aa5cda186eae0554971fc72fdf6952a765
TipluJacob <j.ritterbach@tiplu.de>
2024-10-28 17:19:09 +0100

    chore: removes old unsed `simple_fleetmanagement`

--- src/ros2/simple_fleetmanagement/deploy-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /workspace; 
-source install/setup.bash; 
-
-if [ -z ${DOCKER_MODE+x} ]; then
-    ros2 run simple_fleetmanagement fleetmanagement
-    tail -f /dev/null
- fi
--- src/ros2/simple_fleetmanagement/simple_fleetmanagement/drawer_helper_module.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-
-def get_list_of_drawer_ids(list_of_drawers) -> List[int]:
-    list_of_drawer_ids = []
-    for item in list_of_drawers:
-        drawer_id = item["drawer_controller_id"]
-        list_of_drawer_ids.append(drawer_id)
-    return list_of_drawer_ids
