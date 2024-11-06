27e0cd984a930180afd3a759fa0fb1cfd5ba4ad1
TAlscher <t.alscher@tiplu.de>
2024-11-06 16:30:27 +0100

    changed ros1 launch style to python ros2 style

---
+++ src/ros2/navigation/external/ira_laser_tools-humble/launch/laserscan_multi_merger.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+def generate_launch_description():
+    laserscan_topics_arg = DeclareLaunchArgument(
+        'laserscan_topics',
+        default_value='/robot/front_laser/scan /robot/rear_laser/scan',
+        description='List of laser scan topics to merge separated by space'
+    )
+
+    laserscan_topics = LaunchConfiguration('laserscan_topics')
+
+    return LaunchDescription([
+        laserscan_topics_arg,
+        Node(
+            package='ira_laser_tools',
+            executable='laserscan_multi_merger',
+            name='laserscan_multi_merger',
+            output='screen',
+            parameters=[
+                {'destination_frame': 'base_link'},
+                {'cloud_destination_topic': '/merged_cloud'},
+                {'scan_destination_topic': '/merged_laser/scan'},
+                {'laserscan_topics': laserscan_topics},
+                {'angle_min': -3.14},
+                {'angle_max': 3.14},
+                {'angle_increment': 0.003},
+                {'scan_time': 0.0},
+                {'range_min': 0.1},
+                {'range_max': 40.0},
+            ]
+        )
+    ])
