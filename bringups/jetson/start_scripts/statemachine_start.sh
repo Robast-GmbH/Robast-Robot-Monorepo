285634abb451a96070127b656bf11ad9a8637c51
TipluJacob <j.ritterbach@tiplu.de>
2024-11-15 14:45:01 +0100

    feat: adds `heartbeat_tree_spawner_launch` to jetson bringup

--- bringups/jetson/start_scripts/statemachine_start.sh
+++ bringups/jetson/start_scripts/statemachine_start.sh
@@ -5,3 +5,4 @@ ros2 launch drawer_sm electrical_drawer_launch.py &
 ros2 launch drawer_sm robot_base_led_bt_launch.py &
 ros2 topic pub /trigger_base_tree communication_interfaces/msg/DrawerAddress '{module_id: 0, drawer_id: 0}' --once &
 ros2 launch drawer_sm drawer_statemachine_launch.py 
+ros2 launch drawer_sm heartbeat_tree_spawner_launch.py
