3c82bb3930060062db79206ab952d2a0f23ce107
TipluJacob <j.ritterbach@tiplu.de>
2024-11-23 13:30:28 +0100

    feat: some updates to the launch file of door opening mtc

--- src/ros2/moveit/moveit_mtc/door_opening_mechanism_mtc/launch/door_opening_mechanism_mtc_launch.py
+++ src/ros2/moveit/moveit_mtc/door_opening_mechanism_mtc/launch/door_opening_mechanism_mtc_launch.py
@@ -6,10 +6,12 @@ from moveit_configs_utils import MoveItConfigsBuilder
 
 
 def generate_launch_description():
-    launch_arguments = {
+    urdf_launch_arguments = {
         "ros2_control_hardware_type": "gz_ros2_control",
         "position_joint_type": "prismatic",
         "model_door_opening_mechanism": "true",
+        "ros2_control_hardware_type": "mock_components",
+        "ros2_control_hardware_type_positon_joint": "mock_components",
     }
 
     ros_distro = os.environ["ROS_DISTRO"]
@@ -26,10 +28,12 @@ def generate_launch_description():
             package_name="moveit_door_opening_mechanism_config",
         )
         .robot_description(
-            file_path="config/rb_theron.urdf.xacro", mappings=launch_arguments
+            file_path="config/rb_theron.urdf.xacro", mappings=urdf_launch_arguments
         )
-        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .robot_description_semantic(file_path="config/rb_theron_real_world.srdf")
+        .trajectory_execution(file_path="config/moveit_controllers_real_world.yaml")
         .planning_pipelines(pipelines=planning_pipelines)
+        .joint_limits(file_path="config/joint_limits_real_world.yaml")
         .to_moveit_configs()
     )
 
