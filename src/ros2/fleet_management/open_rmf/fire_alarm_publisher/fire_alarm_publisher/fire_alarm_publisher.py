c3b61e58206012cdaeb00d72ab245c70fe067788
ane-robast <a.neumann@robast.de>
2024-11-29 17:05:39 +0100

    Add fire_plan_publisher and dispenser_ingestor_mock to rmf launch

--- src/ros2/fleet_management/open_rmf/fire_alarm_publisher/fire_alarm_publisher/fire_alarm_publisher.py
+++ src/ros2/fleet_management/open_rmf/fire_alarm_publisher/fire_alarm_publisher/fire_alarm_publisher.py
@@ -6,33 +6,45 @@ import requests
 
 
 class FireAlarmPublisher(Node):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__("fire_alarm_publisher")
 
-        qos_profile = QoSProfile(
-        reliability=ReliabilityPolicy.RELIABLE,
-        history=HistoryPolicy.KEEP_LAST,
-        depth=10,
-        durability=DurabilityPolicy.TRANSIENT_LOCAL
-)   
-  
-        self.__publisher = self.create_publisher(Bool, "fire_alarm_trigger", qos_profile)
-        self.__url = "http://10.10.13.7:8003/fire_alarm/fire_alarm_triggered?source=fire_alarm_publisher"
-        self.create_timer(1.0, self.timer_callback)
-        self.get_logger().info("node started")
+        self.declare_parameter("middleware_address", "http://10.10.13.7:8003")
+        middleware_address = self.get_parameter("middleware_address").value
+        self.__url = f"{middleware_address}/fire_alarm/fire_alarm_triggered?source=fire_alarm_publisher"
 
-    def timer_callback(self):
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.RELIABLE,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=10,
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+        )
+        self.__publisher = self.create_publisher(
+            Bool, "fire_alarm_trigger", qos_profile
+        )
+
+        self.create_timer(1.0, self.__timer_callback)
+
+    def __timer_callback(self) -> None:
+        fire_alarm_triggered = self.__get_fire_alarm_triggered()
+        if fire_alarm_triggered is not None:
+            self.__publish_fire_alarm_triggered(fire_alarm_triggered)
+
+    def __get_fire_alarm_triggered(self) -> bool | None:
         try:
             response = requests.get(self.__url, timeout=1)
             response.raise_for_status()
             data = response.json()
-            message = Bool()
-            message.data = data["fire_alarm_triggered"]
-            self.__publisher.publish(message)
-
-            self.get_logger().info(f"fire_alarm_triggered published: {message.data}")
+            return data["fire_alarm_triggered"]
         except requests.RequestException as e:
             self.get_logger().warn(f"HTTP-request failed: {e}")
+            return None
+
+    def __publish_fire_alarm_triggered(self, fire_alarm_triggered: bool) -> None:
+        message = Bool()
+        message.data = fire_alarm_triggered
+        self.__publisher.publish(message)
+        self.get_logger().debug(f"fire_alarm_triggered published: {message.data}")
 
 
 def main(args=None):
