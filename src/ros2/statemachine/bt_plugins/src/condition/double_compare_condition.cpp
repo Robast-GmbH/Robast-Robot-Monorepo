c8e72e55c0a5808a6b29ca63c8a2da7aa9666108
TAlscher <t.alscher@tiplu.de>
2024-12-13 17:47:03 +0100

    added double compare condition

---
+++ src/ros2/statemachine/bt_plugins/src/condition/double_compare_condition.cpp
@@ -0,0 +1,61 @@
+#include "bt_plugins/condition/double_compare_condition.hpp"
+
+#include <stdio.h>
+
+namespace statemachine
+{
+  DoubleCompareCondition::DoubleCompareCondition(const std::string &name, const BT::NodeConfig &config)
+      : BT::ConditionNode(name, config)
+  {
+  }
+
+  BT::NodeStatus DoubleCompareCondition::tick()
+  {
+    // Retrieve input values from the ports
+    getInput("target_value", _target_value);
+    getInput("value", _value);
+    getInput("comparison", _comparison);
+    // Perform the comparison based on the specified comparison type
+    if (_comparison == "==")
+    {
+      std::cout << "Comparing " << _value << " == " << _target_value << std::endl;
+      return (_value == _target_value) ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+    }
+    else if (_comparison == "!=")
+    {
+      std::cout << "Comparing " << _value << " != " << _target_value << std::endl;
+      return (_value != _target_value) ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+    }
+    else if (_comparison == "<")
+    {
+      std::cout << "Comparing " << _value << " < " << _target_value << std::endl;
+      return (_value < _target_value) ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+    }
+    else if (_comparison == "<=")
+    {
+      std::cout << "Comparing " << _value << " <= " << _target_value << std::endl;
+      return (_value <= _target_value) ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+    }
+    else if (_comparison == ">")
+    {
+      std::cout << "Comparing " << _value << " > " << _target_value << std::endl;
+      return (_value > _target_value) ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+    }
+    else if (_comparison == ">=")
+    {
+      std::cout << "Comparing " << _value << " >= " << _target_value << std::endl;
+      return (_value >= _target_value) ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+    }
+    else
+    {
+      std::cout << "Invalid comparison type: " << _comparison << std::endl;
+      return BT::NodeStatus::FAILURE;   // Invalid comparison type
+    }
+  }
+}   // namespace statemachine
+
+#include "behaviortree_cpp/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<statemachine::DoubleCompareCondition>("DoubleCompareCondition");
+}
