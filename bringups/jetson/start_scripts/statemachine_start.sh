a8f6d259656fdc5fa2b9f2d16caaf445ad8d4239
TipluJacob <j.ritterbach@tiplu.de>
2024-12-17 09:58:42 +0100

    fix: last command in the shell needs to be a cmd that runs forever otherwise the docker will stop

--- bringups/jetson/start_scripts/statemachine_start.sh
+++ bringups/jetson/start_scripts/statemachine_start.sh
@@ -5,6 +5,6 @@ ros2 launch drawer_sm electrical_drawer_launch.py &
 ros2 launch drawer_sm robot_base_led_bt_launch.py &
 ros2 launch drawer_sm partial_drawer_launch.py &
 ros2 topic pub /trigger_base_tree communication_interfaces/msg/DrawerAddress '{module_id: 1, drawer_id: 0}' --once &
-ros2 launch drawer_sm drawer_statemachine_launch.py &
 ros2 launch drawer_sm heartbeat_launch.py &
-ros2 topic pub /trigger_heartbeat_tree std_msgs/msg/String --once
+ros2 topic pub /trigger_heartbeat_tree std_msgs/msg/String --once &
+ros2 launch drawer_sm drawer_statemachine_launch.py
