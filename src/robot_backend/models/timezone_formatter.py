340dd9d93890127c75c36c2f0787fa9c50405f86
ane-robast <a.neumann@robast.de>
2024-12-17 10:22:02 +0100

    Change duplicated code a little bit

--- src/robot_backend/models/timezone_formatter.py
+++ src/robot_backend/models/timezone_formatter.py
@@ -4,20 +4,16 @@ from zoneinfo import ZoneInfo
 
 
 class TimezoneFormatter(Formatter):
-    """
-    Custom formatter to include timezone-aware timestamps.
-    """
-
     def __init__(self, fmt=None, datefmt=None, tz="Europe/Berlin"):
         super().__init__(fmt=fmt, datefmt=datefmt)
-        self.tz = ZoneInfo(tz)
+        self.__timezone = ZoneInfo(tz)
+
+    def format_timestamp(self, timestamp: float):
+        timestamp_with_timezone = datetime.fromtimestamp(timestamp, tz=self.__timezone)
+        return timestamp_with_timezone.strftime("%d-%m-%Y %H:%M:%S")
 
     def formatTime(self, record, datefmt=None):
-        record_time = datetime.fromtimestamp(record.created, tz=self.tz)
+        record_time = datetime.fromtimestamp(record.created, tz=self.__timezone)
         if datefmt:
             return record_time.strftime(datefmt)
         return record_time.isoformat()
-
-    def format_timestamp(self, timestamp: float):
-        timestamp_with_timezone = datetime.fromtimestamp(timestamp, tz=self.tz)
-        return timestamp_with_timezone.strftime("%d-%m-%Y %H:%M:%S")
