c809de56474254aa6669b58b1d16f3e4210eefae
TipluJacob <j.ritterbach@tiplu.de>
2025-01-06 11:21:02 +0100

    fix: wait some time before we start heartbeat_launch otherwise we might get wrong timeouts

--- bringups/jetson/start_scripts/statemachine_start.sh
+++ bringups/jetson/start_scripts/statemachine_start.sh
@@ -5,6 +5,7 @@ ros2 launch drawer_sm electrical_drawer_launch.py &
 ros2 launch drawer_sm robot_base_led_bt_launch.py &
 ros2 launch drawer_sm partial_drawer_launch.py &
 ros2 topic pub /trigger_base_tree communication_interfaces/msg/DrawerAddress '{module_id: 1, drawer_id: 0}' --once &
+sleep 20
 ros2 launch drawer_sm heartbeat_launch.py &
 ros2 topic pub /trigger_heartbeat_tree std_msgs/msg/Empty --once &
 ros2 launch drawer_sm drawer_statemachine_launch.py
