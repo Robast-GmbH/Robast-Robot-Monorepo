1428f219627d584367660916fa66c64e306be85d
TipluJacob <78360006+TipluJacob@users.noreply.github.com>
2025-01-09 15:24:47 +0100

    fix: generic error converter (RE-2500) (#263)
    * feat: rework of generic error converter including python version and tests
    
    * chore: updates version of Catch2
    
    * chore: removes license comment
    
    * docs: adds package description
    
    * chore: updates dependency to test_depend

--- src/ros2/statemachine/bt_plugins/include/bt_plugins/decorator/base_error_decorator.hpp
+++ src/ros2/statemachine/bt_plugins/include/bt_plugins/decorator/base_error_decorator.hpp
@@ -1,44 +1,39 @@
 #ifndef BT_PLUGINS__BT_BASE_ERROR_DECORATOR_HPP
 #define BT_PLUGINS__BT_BASE_ERROR_DECORATOR_HPP
 
-#include <string>
 #include <memory>
+#include <string>
 
-#include "rclcpp/rclcpp.hpp"
 #include "behaviortree_cpp/decorator_node.h"
-
-#include "error_utils/generic_error_converter.hpp"
-#include "communication_interfaces/msg/error_base_msg.hpp"
 #include "communication_interfaces/msg/drawer_address.hpp"
+#include "communication_interfaces/msg/error_base_msg.hpp"
+#include "error_utils/generic_error_converter.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 namespace statemachine
 {
   class BaseErrorDecorator : public BT::DecoratorNode
   {
-
-  public:
+   public:
     BaseErrorDecorator(const std::string &name, const BT::NodeConfig &config);
     BT::NodeStatus tick();
     static BT::PortsList providedPorts()
     {
-      return {
-          BT::InputPort<std::string>("topic", "/robast_error")};
+      return {BT::InputPort<std::string>("topic", "/robast_error")};
     }
 
-  protected:
+   protected:
     void callbackDrawerFeedback(const communication_interfaces::msg::ErrorBaseMsg::SharedPtr msg);
-    void logError();
+    void logError(const std::string &error_msg);
     typename rclcpp::Subscription<communication_interfaces::msg::ErrorBaseMsg>::SharedPtr drawer_status_sub_;
     std::string topic_name_;
     BT::Blackboard::Ptr blackboard_;
     rclcpp::executors::SingleThreadedExecutor callback_group_executor_;
-    MessageConverter<communication_interfaces::msg::DrawerAddress> converter_;
 
-  private:
+   private:
     rclcpp::Node::SharedPtr _node;
     rclcpp::CallbackGroup::SharedPtr _callback_group;
-    communication_interfaces::msg::ErrorBaseMsg::SharedPtr _error_msg;
     bool _is_error_received = false;
   };
-} // namespace statemachine
-#endif // BT_PLUGINS__BT_BASE_ERROR_DECORATOR_HPP
+}   // namespace statemachine
+#endif   // BT_PLUGINS__BT_BASE_ERROR_DECORATOR_HPP
