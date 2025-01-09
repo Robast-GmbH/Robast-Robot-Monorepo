af9cb51a13447b4996dcccca59ca51e9fab10247
TipluJacob <j.ritterbach@tiplu.de>
2025-01-09 16:50:01 +0100

    feat: first draft for `GeneratePartialPositionAction`

---
+++ src/ros2/statemachine/bt_plugins/include/bt_plugins/action/generate_partial_position_action.hpp
@@ -0,0 +1,43 @@
+#ifndef DRAWER_STATEMACHINE__BT_PLUGINS__ACTION__GENERATE_PARTIAL_POSITION_ACTION_HPP
+#define DRAWER_STATEMACHINE__BT_PLUGINS__ACTION__GENERATE_PARTIAL_POSITION_ACTION_HPP
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "behaviortree_cpp/action_node.h"
+#include "communication_interfaces/msg/drawer_address.hpp"
+
+namespace statemachine
+{
+  constexpr uint8_t FIRST_LID_POSITION_OFFSET = 30;
+
+  class GeneratePartialPosition : public BT::SyncActionNode
+  {
+   public:
+    GeneratePartialPosition(const std::string &name, const BT::NodeConfig &config);
+
+    GeneratePartialPosition() = delete;
+
+    /**
+     * @brief The main override required by a BT action
+     * @return BT::NodeStatus Status of tick execution
+     */
+    BT::NodeStatus tick() override;
+
+    /**
+     * @brief Creates list of BT ports
+     * @return BT::PortsList Containing node-specific ports
+     */
+    static BT::PortsList providedPorts()
+    {
+      return {BT::InputPort<communication_interfaces::msg::DrawerAddress>(
+                  "drawer_address", "address of the tray we want to get the position for"),
+              BT::InputPort<uint8_t>("first_tray_position", 0),
+              BT::InputPort<uint8_t>("last_tray_position", 0),
+              BT::InputPort<uint8_t>("tray_count", 0),
+              BT::OutputPort<uint8_t>("target_position")};
+    }
+  };
+
+}   // namespace statemachine
+
+#endif   // DRAWER_STATEMACHINE__BT_PLUGINS__ACTION__GENERATE_PARTIAL_POSITION_ACTION_HPP
---
+++ src/ros2/statemachine/bt_plugins/src/action/generate_partial_position_action.cpp
@@ -0,0 +1,52 @@
+#include "bt_plugins/action/generate_partial_position_action.hpp"
+
+namespace statemachine
+{
+  GeneratePartialPosition::GeneratePartialPosition(const std::string &name, const BT::NodeConfig &config)
+      : BT::SyncActionNode(name, config)
+  {
+  }
+
+  BT::NodeStatus GeneratePartialPosition::tick()
+  {
+    communication_interfaces::msg::DrawerAddress drawer_address;
+    getInput("drawer_address", drawer_address);
+    const uint8_t tray_id = drawer_address.drawer_id;
+    if (tray_id == 0)
+    {
+      RCLCPP_ERROR(rclcpp::get_logger("GeneratePartialPosition"), "Tray ID cannot be 0. First tray is 1.");
+      return BT::NodeStatus::FAILURE;
+    }
+
+    uint8_t first_tray_position;
+    getInput("first_tray_position", first_tray_position);
+
+    uint8_t last_tray_position;
+    getInput("last_tray_position", last_tray_position);
+    if (last_tray_position <= first_tray_position)
+    {
+      RCLCPP_ERROR(rclcpp::get_logger("GeneratePartialPosition"),
+                   "Last tray position must be greater than first tray position.");
+      return BT::NodeStatus::FAILURE;
+    }
+
+    uint8_t tray_count;
+    getInput("tray_count", tray_count);
+    if (tray_count == 0)
+    {
+      RCLCPP_ERROR(rclcpp::get_logger("GeneratePartialPosition"), "Tray count cannot be 0.");
+      return BT::NodeStatus::FAILURE;
+    }
+
+    uint8_t target_position =
+        first_tray_position + (tray_id - 1) * (last_tray_position - first_tray_position) / (tray_count - 1);
+    setOutput("target_position", target_position);
+    return BT::NodeStatus::SUCCESS;
+  }
+}   // namespace statemachine
+
+#include "behaviortree_cpp/bt_factory.h"
+BT_REGISTER_NODES(factory)
+{
+  factory.registerNodeType<statemachine::GeneratePartialPosition>("GeneratePartialPosition");
+}
