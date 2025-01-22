742f9357e0707d91d9ebfddf819c90e3b4aadf75
ane-robast <a.neumann@robast.de>
2025-01-22 14:13:03 +0100

    Remove unused deserializer functions

--- src/robot_backend/models/deserializer.py
+++ src/robot_backend/models/deserializer.py
@@ -21,18 +21,3 @@ class Deserializer:
             serialized_data, Deserializer.DRAWER_ADDRESS_FORMAT
         )
         return {"module_id": module_id, "submodule_id": submodule_id}
-
-    @staticmethod
-    def deserialize_string(serialized_data: bytes) -> str:
-        if b"\x00" in serialized_data:
-            return serialized_data.split(b"\x00", 1)[0].decode("utf-8")
-        return serialized_data.decode("utf-8").strip()
-
-    @staticmethod
-    def deserialize_error_msg(serialized_data: bytes, interface: str) -> Any:
-        if interface == "communication_interfaces::msg::DrawerAddress":
-            return Deserializer.deserialize_drawer_address(serialized_data)
-        elif interface == "std::string":
-            return Deserializer.deserialize_string(serialized_data)
-        else:
-            return None
