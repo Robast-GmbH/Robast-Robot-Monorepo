0a1b961a7ca5032adfec4d132ab1ef040269027e
TAlscher <60599364+TAlscher@users.noreply.github.com>
2025-01-28 17:20:54 +0100

    Dev/re 2643 (#280)
    * remove BT submodule
    
    * remove bt manual install from docker
    
    * gute pfadfinder cleant test includes
    
    * workflow include reduction

--- src/ros2/statemachine/bt_plugins/tests/actions/open_drawer_action_test.cpp
+++ src/ros2/statemachine/bt_plugins/tests/actions/open_drawer_action_test.cpp
@@ -1,38 +1,36 @@
-#include <catch2/catch_all.hpp>
-#include "bt_plugins/behavior_tree_engine.hpp"
-#include "behaviortree_cpp/tree_node.h"
-#include "behaviortree_cpp/blackboard.h"
-#include "rclcpp/rclcpp.hpp"
-#include "communication_interfaces/msg/drawer_address.hpp"
 #include "bt_plugins/action/open_drawer_action.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "behaviortree_cpp/blackboard.h"
+#include "behaviortree_cpp/tree_node.h"
+#include "bt_plugins/behavior_tree_engine.hpp"
+#include "communication_interfaces/msg/drawer_address.hpp"
+#include "rclcpp/rclcpp.hpp"
 
 namespace test2
 {
-    SCENARIO("A minimal tree gets created with just the OpenDrawer plugin")
+  SCENARIO("A minimal tree gets created with just the OpenDrawer plugin")
+  {
+    GIVEN("A minimal BT_engine with a one node opendrawer tree")
     {
-        GIVEN("A minimal BT_engine with a one node opendrawer tree")
-        {
-            std::string nodename = "OpenDrawer";
-            rclcpp::init(0, nullptr);
+      std::string nodename = "OpenDrawer";
+      rclcpp::init(0, nullptr);
 
-            const std::vector<std::string> plugins = {
-                "open_drawer_action_bt_node",
-            };
-            static rclcpp::Node::SharedPtr node_open_drawer = std::make_shared<rclcpp::Node>("test_open_drawer");
-            static BT::NodeConfig *config_;
-            config_ = new BT::NodeConfig();
-            auto blackboard = BT::Blackboard::create();
-            blackboard->set<std::chrono::milliseconds>(
-                "bt_loop_duration",
-                std::chrono::milliseconds(10));
-            communication_interfaces::msg::DrawerAddress used_drawer_address;
-            used_drawer_address.module_id = 1;
-            used_drawer_address.drawer_id = 1;
-            blackboard->set<communication_interfaces::msg::DrawerAddress>(
-                "drawer_address",
-                used_drawer_address);
-            std::string led_tree_xml =
-                R"(
+      const std::vector<std::string> plugins = {
+        "open_drawer_action_bt_node",
+      };
+      static rclcpp::Node::SharedPtr node_open_drawer = std::make_shared<rclcpp::Node>("test_open_drawer");
+      static BT::NodeConfig *config_;
+      config_ = new BT::NodeConfig();
+      auto blackboard = BT::Blackboard::create();
+      blackboard->set<std::chrono::milliseconds>("bt_loop_duration", std::chrono::milliseconds(10));
+      communication_interfaces::msg::DrawerAddress used_drawer_address;
+      used_drawer_address.module_id = 1;
+      used_drawer_address.drawer_id = 1;
+      blackboard->set<communication_interfaces::msg::DrawerAddress>("drawer_address", used_drawer_address);
+      std::string led_tree_xml =
+        R"(
                     <root BTCPP_format="4" >
                         <BehaviorTree ID="MainTree">
                             <OpenDrawer drawer_address="{drawer}"
@@ -40,37 +38,34 @@ namespace test2
                         </BehaviorTree>
                     </root>)";
 
-            WHEN("The bt engine including the open drawer plugin is created")
+      WHEN("The bt engine including the open drawer plugin is created")
+      {
+        blackboard->set<rclcpp::Node::SharedPtr>("node", node_open_drawer);
+        auto bt_engine = std::make_unique<statemachine::BehaviorTreeEngine>(plugins);
+        auto bt = bt_engine->createTreeFromText(led_tree_xml, blackboard, "MainTree");
+        THEN("A Subtree should exist")
+        {
+          REQUIRE(bt.subtrees[0]);
+          WHEN("this Tree exists")
+          {
+            THEN("the Tree should hold all added plugins, in this case OpenDrawer")
             {
-                blackboard->set<rclcpp::Node::SharedPtr>(
-                    "node",
-                    node_open_drawer);
-                auto bt_engine = std::make_unique<statemachine::BehaviorTreeEngine>(plugins);
-                auto bt = bt_engine->createTreeFromText(led_tree_xml, blackboard, "MainTree");
-                THEN("A Subtree should exist")
+              auto iter = bt.subtrees[0]->nodes.begin();
+              bool found = false;
+              for (; iter != bt.subtrees[0]->nodes.end(); iter++)
+              {
+                if ((*iter)->registrationName() == nodename)
                 {
-                    REQUIRE(bt.subtrees[0]);
-                    WHEN("this Tree exists")
-                    {
-                        THEN("the Tree should hold all added plugins, in this case OpenDrawer")
-                        {
-                            auto iter = bt.subtrees[0]->nodes.begin();
-                            bool found = false;
-                            for (; iter != bt.subtrees[0]->nodes.end(); iter++)
-                            {
-                                if ((*iter)->registrationName() == nodename)
-                                {
-                                    iter = bt.subtrees[0]->nodes.begin();
-                                    found = true;
-                                    break;
-                                }
-                            }
-                            REQUIRE(found);
-                        }
-                    }
-
+                  iter = bt.subtrees[0]->nodes.begin();
+                  found = true;
+                  break;
                 }
+              }
+              REQUIRE(found);
             }
+          }
         }
+      }
     }
-}
+  }
+}   // namespace test2
