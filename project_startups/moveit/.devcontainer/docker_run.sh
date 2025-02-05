9b70d52604eecaced5dce36b555169cc52e68492
TipluJacob <j.ritterbach@tiplu.de>
2025-02-05 16:17:07 +0100

    chore: updates mounts

--- project_startups/moveit/.devcontainer/docker_run.sh
+++ project_startups/moveit/.devcontainer/docker_run.sh
@@ -11,8 +11,10 @@ docker run \
 	--volume=/home/robot/robast/Monorepo/src/ros2/ros2_control/hardware_interfaces/ros2_control_plugin_dryve_d1:/workspace/src/ros2_control/hardware_interfaces/ros2_control_plugin_dryve_d1 \
 	--volume=/home/robot/robast/Monorepo/src/ros2/ros2_control/hardware_interfaces/ros2_control_base_movement:/workspace/src/ros2_control/hardware_interfaces/ros2_control_base_movement \
 	--volume=/home/robot/robast/Monorepo/src/ros2/ros2_control/hardware_interfaces/hardware_interface_utils:/workspace/src/ros2_control/hardware_interfaces/hardware_interface_utils \
-	--volume=/home/robot/robast/Monorepo/src/ros2/moveit:/workspace/src/moveit \
 	--volume=/home/robot/robast/Monorepo/src/ros2/ros2_control/ros2_controllers:/workspace/src/ros2_control/ros2_controllers \
+	--volume=/home/robot/robast/Monorepo/src/ros2/moveit/moveit_mtc:/workspace/src/moveit/moveit_mtc \
+	--volume=/home/robot/robast/Monorepo/src/ros2/moveit/moveit_resources:/workspace/src/moveit/moveit_resources \
+	--volume=/home/robot/robast/Monorepo/src/ros2/moveit/perception:/workspace/src/moveit/perception \
 	--volume=/home/robot/robast/Monorepo/src/ros2/dds_configs:/workspace/dds_configs \
 	--volume=/home/robot/robast/Monorepo/src/ros2/utils/launch_manager:/workspace/src/utils/launch_manager \
 	-e DISPLAY=:0 \
@@ -21,5 +23,5 @@ docker run \
 	--env prefix="" \
 	--user robast \
 	ghcr.io/robast-gmbh/monorepo/moveit:devel-humble \
-	bash -c "cd ~/../../workspace && sudo apt-get update && sudo apt-get upgrade -y && rosdep update && rosdep install --from-paths src --ignore-src -r -y && colcon build --packages-skip get_spatial_detections_from_topic create_pose_from_spatial_detections publish_pose_to_topic setup_mtc_move_to_pose_with_constraints && bash"
+	bash -c "cd ~/../../workspace && sudo apt-get update && sudo apt-get upgrade -y && rosdep update && rosdep install --from-paths src --ignore-src -r -y && colcon build && bash"
 
