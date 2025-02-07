6a0a4b26a42af78e542cd415ff49701b5ee3d3b9
ane-robast <a.neumann@robast.de>
2025-02-07 12:56:50 +0100

    Add toggle function to enable/disable image_subscription

--- src/ros2/hardware_nodes/qr_code_scanner/include/qr_code_scanner/qr_code_scanner.hpp
+++ src/ros2/hardware_nodes/qr_code_scanner/include/qr_code_scanner/qr_code_scanner.hpp
@@ -18,15 +18,17 @@ class QrCodeScanner : public rclcpp::Node
     ~QrCodeScanner();
 
   private:
-    std::vector<std::string> read_qr_codes_from_image(const cv::Mat &image);
+    void toggle_image_subscription(bool enable);
     void image_callback(const sensor_msgs::msg::CompressedImage::SharedPtr msg);
     void read_qr_code_service_callback(
       const std::shared_ptr<communication_interfaces::srv::ReadQrCode::Request> request,
       std::shared_ptr<communication_interfaces::srv::ReadQrCode::Response> response);
+    std::vector<std::string> read_qr_codes_from_image(const cv::Mat &image);
 
-    int _active_service_calls_counter = 1;
+    int _active_service_calls_counter = 0;
     struct quirc *_qr_decoder = quirc_new();
     std::vector<std::string> _visible_qr_codes;
+    rclcpp::SubscriptionOptions image_subscription_options;
     rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr _zed_image_subscriber;
     rclcpp::Service<communication_interfaces::srv::ReadQrCode>::SharedPtr _read_qr_code_service;
 };
--- src/ros2/hardware_nodes/qr_code_scanner/src/qr_code_scanner.cpp
+++ src/ros2/hardware_nodes/qr_code_scanner/src/qr_code_scanner.cpp
@@ -2,13 +2,7 @@
 
 QrCodeScanner::QrCodeScanner() : Node("qr_code_scanner")
 {
-  auto image_subscription_options = rclcpp::SubscriptionOptions();
   image_subscription_options.callback_group = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
-  _zed_image_subscriber = this->create_subscription<sensor_msgs::msg::CompressedImage>(
-    "/robot/zed/zed_node/left_gray/image_rect_gray/compressed",
-    10,
-    bind(&QrCodeScanner::image_callback, this, std::placeholders::_1),
-    image_subscription_options);
 
   _read_qr_code_service = this->create_service<communication_interfaces::srv::ReadQrCode>(
     "read_qr_code",
@@ -20,13 +14,25 @@ QrCodeScanner::~QrCodeScanner()
   quirc_destroy(_qr_decoder);
 }
 
+void QrCodeScanner::toggle_image_subscription(bool enable)
+{
+  if (enable)
+  {
+    _zed_image_subscriber = this->create_subscription<sensor_msgs::msg::CompressedImage>(
+      "/robot/zed/zed_node/left_gray/image_rect_gray/compressed",
+      10,
+      bind(&QrCodeScanner::image_callback, this, std::placeholders::_1),
+      image_subscription_options);
+  }
+  else
+  {
+    _zed_image_subscriber.reset();
+  }
+}
+
 void QrCodeScanner::image_callback(const sensor_msgs::msg::CompressedImage::SharedPtr msg)
 {
   RCLCPP_DEBUG(this->get_logger(), "image received");
-  if (_active_service_calls_counter <= 0)
-  {
-    return;
-  }
   const cv::Mat image = cv::imdecode(cv::Mat(msg->data), cv::IMREAD_COLOR);
   _visible_qr_codes = read_qr_codes_from_image(image);
   for (const auto &qrCode : _visible_qr_codes)
@@ -39,12 +45,17 @@ void QrCodeScanner::read_qr_code_service_callback(
   const std::shared_ptr<communication_interfaces::srv::ReadQrCode::Request> request,
   std::shared_ptr<communication_interfaces::srv::ReadQrCode::Response> response)
 {
+  if (_active_service_calls_counter == 0)
+  {
+    toggle_image_subscription(true);
+  }
+
   _visible_qr_codes.clear();
   _active_service_calls_counter++;
 
   bool found_match = false;
   int timeout_counter = 0;
-  int timeout_limit = request->timeout_in_s * 10;
+  const int timeout_limit = request->timeout_in_s * 10;
 
   while (!found_match && timeout_counter < timeout_limit)
   {
@@ -55,13 +66,18 @@ void QrCodeScanner::read_qr_code_service_callback(
 
   _active_service_calls_counter--;
   response->success = found_match;
+
+  if (_active_service_calls_counter == 0)
+  {
+    toggle_image_subscription(false);
+  }
 }
 
 std::vector<std::string> QrCodeScanner::read_qr_codes_from_image(const cv::Mat &image)
 {
   std::vector<std::string> qr_codes;
-  int width = image.cols;
-  int height = image.rows;
+  const int width = image.cols;
+  const int height = image.rows;
 
   quirc_resize(_qr_decoder, width, height);
 
@@ -75,7 +91,7 @@ std::vector<std::string> QrCodeScanner::read_qr_codes_from_image(const cv::Mat &
   quirc_end(_qr_decoder);
 
   // Detect and decode QR codes
-  int num_codes = quirc_count(_qr_decoder);
+  const int num_codes = quirc_count(_qr_decoder);
   for (int i = 0; i < num_codes; i++)
   {
     struct quirc_code code;
